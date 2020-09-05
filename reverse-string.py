8e2d13f0f96869536aabaa2ad0e56042a44b9e70
doyedele1 <demilade.oyedele@gmail.com>
2020-09-05 03:19:01 +0100

    my first commit to this repo

---
+++ reverse-string.py
@@ -0,0 +1,18 @@
+# using recursion method
+def reverse_string(str):
+	if len(str) == 0:
+		return str
+
+	else:
+		return reverse_string(str[1:]) + str[0]
+
+# print(reverse_string("Demilade"))
+
+# using slicing method
+def reverse_string_slice(str):
+	reversed_str = str[::-1]
+	return reversed_str
+
+# print(reverse_string_slice("Cat"))
+# print(reverse_string_slice("The Daily Byte"))
+# print(reverse_string_slice("civic"))
