b48b8c2f4a40b508fb5fc7912d9424aa77e10ce0
doyedele1 <demilade.oyedele@gmail.com>
2020-09-14 00:51:40 +0100

    added another algo challenge

---
+++ robot-position.py
@@ -0,0 +1,29 @@
+# first solution
+def robot_original_position(moves):
+	angle = 0
+	for move in moves:
+		if(move == "L"):
+			angle -= 180
+		elif(move == "R"):
+			angle += 180
+		elif(move == "U"):
+			angle += 90
+		elif(move == "D"):
+			angle -= 90
+
+	if(angle == 0):
+		return True
+	else:
+		return False
+
+# second solution - best solution
+def robot_original_position2(moves):
+	if(moves.count("L") == moves.count("R") and moves.count("U") == moves.count("D")):
+		return True
+	else:
+		return False
+
+# print(robot_original_position2("LR"))
+# print(robot_original_position2("LUDR"))
+# print(robot_original_position2("URURD"))
+# print(robot_original_position2("RUULLDRD"))
