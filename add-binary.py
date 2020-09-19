0ff0a6de56864f0031ddb81765ae5bb9c34a9aef
Demilade Oyedele <demilade.oyedele@gmail.com>
2020-09-20 00:09:41 +0100

    updating algo series

---
+++ add-binary.py
@@ -0,0 +1,9 @@
+# converting first to decimal, then to binary
+def add_two_binary_numbers(num1, num2):
+	sum = int(num1,2) + int(num2,2)
+	binary_sum = bin(sum)
+	return str(binary_sum[2:])
+
+print(add_two_binary_numbers("100", "1"))
+print(add_two_binary_numbers("11", "1"))
+print(add_two_binary_numbers("1", "0"))
