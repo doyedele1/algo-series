10ac323f49894d1ad4583fe66cbb4c9f21fa4577
doyedele1 <demilade.oyedele@gmail.com>
2021-02-19 23:21:40 +0100

    updated the structure and dsa basics folder was added

--- DSA Questions & Solutions/5. Add Binary/solution.py
+++ DSA Questions & Solutions/5. Add Binary/solution.py
@@ -4,6 +4,6 @@ def add_two_binary_numbers(num1, num2):
 	binary_sum = bin(sum)
 	return str(binary_sum[2:])
 
-print(add_two_binary_numbers("100", "1"))
-print(add_two_binary_numbers("11", "1"))
-print(add_two_binary_numbers("1", "0"))
+# print(add_two_binary_numbers("100", "1"))
+# print(add_two_binary_numbers("11", "1"))
+# print(add_two_binary_numbers("1", "0"))
