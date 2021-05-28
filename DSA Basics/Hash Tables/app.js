eb18b37f0c9b8f45aed452e4dc3834d1bcaf07b2
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-05-28 10:06:51 +0100

    Learning DSA Basics

---
+++ DSA Basics/Hash Tables/app.js
@@ -0,0 +1,12 @@
+/* Hash tables or hash maps are used to store key-value pairs.
+
+They are like arrays, but the keys are not ordered.
+
+Unlike arrays, hash tables are fast for all of the following operations:
+    finding values,
+    adding new values,
+    removing values
+
+Python has Dictionaries. JS has Objects and Maps, Java, Go & Scala have Maps, Ruby has Hashes
+
+*/
