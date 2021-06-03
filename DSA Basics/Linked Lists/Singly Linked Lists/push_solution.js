574c79073916cc5684226bfe80270bd63b948ae1
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-06-03 10:24:16 +0100

    Adding new files to DSA Basics

---
+++ DSA Basics/Linked Lists/Singly Linked Lists/push_solution.js
@@ -0,0 +1,47 @@
+/*
+    - No index
+    - Connected via nodes with a next pointer
+    - Random access is not allowed
+    - Arrays (skyscrapers with elevators). Linked lists (skyscrapers with no elevators)
+*/
+
+// piece of data = val
+// reference to next node = next
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(val) {
+        var newNode = new Node(val)
+        if(!this.head) {
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+}
+
+// var first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+
+var list = new SinglyLinkedList()
+list.push("Hello")
+list.push("Goodbye")
