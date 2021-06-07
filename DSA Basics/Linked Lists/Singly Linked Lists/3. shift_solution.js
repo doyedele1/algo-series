843282dc391b8305fab8960d7cd290a18d39bb4a
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-06-07 20:59:51 +0100

    making changes here

--- DSA Basics/Linked Lists/Singly Linked Lists/3. shift_solution.js
+++ DSA Basics/Linked Lists/Singly Linked Lists/3. shift_solution.js
@@ -12,14 +12,14 @@ class SinglyLinkedList {
         this.length = 0
     }
 
-    /*  PSEUDOCODE FOR SHIFT
+    shift() {
+        /*  PSEUDOCODE FOR SHIFT
             - If there are no nodes, return undefined
             - Store the current head property in a variable
             - Set the head property to be the current head's next property
             - Decrement the length by 1
             - Return the value of the node removed
-    */
-    shift() {
+        */
         if(!this.head) return undefined
         var currentHead = this.head
         this.head = currentHead.next
