55721f4927b4fe0b00619b2f3c9bddd890c3cb7b
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-06-14 05:01:37 +0100

    making changes

--- DSA Questions & Solutions/Arrays/Count Unique Values/Remove Duplicates from Sorted Array/solution.py
+++ DSA Questions & Solutions/Arrays/Count Unique Values/Remove Duplicates from Sorted Array/solution.py
@@ -1,13 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
+        # i = 0
         
-        if(len(nums) == 0):
-            return 0
+        # if(len(nums) == 0):
+        #     return 0
     
-        for j in range(1, len(nums)):
-            if(nums[i] != nums[j]):
-                i+=1
-                nums[i] = nums[j]
-        return i + 1
+        # for j in range(1, len(nums)):
+        #     if(nums[i] != nums[j]):
+        #         i+=1
+        #         nums[i] = nums[j]
+        # return i + 1
+
+        index = 0
+        
+        for i in range(0, (len(nums) - 1)):
+            if(nums[i] != nums[i+1]):
+                index += 1
+                nums[index] = nums[i+1]
+        return index + 1
         
