db7ae20d11a51b86fd073a24331a535fcb31fb38
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-06-18 14:16:22 +0100

    adding palindrome linked list question

---
+++ DSA Questions & Solutions/Linked Lists/Palindrome Linked List/solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        # TC - O(n), SC - O(n)
+        nums = []
+        
+        while head:
+            nums.append(head.val)
+            head = head.next
+            
+        start = 0
+        end = len(nums) - 1
+        while(start < end):
+            if(nums[start] != nums[end]):
+                return False
+            start += 1
+            end -= 1
+        return True
