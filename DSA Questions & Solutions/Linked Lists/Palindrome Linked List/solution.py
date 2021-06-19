1044e7928e6c8b03fa5e00fdcb993821dde31f13
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-06-19 04:00:57 +0100

    adding an optimal solution to palindrome linked list

--- DSA Questions & Solutions/Linked Lists/Palindrome Linked List/solution.py
+++ DSA Questions & Solutions/Linked Lists/Palindrome Linked List/solution.py
@@ -4,8 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # TC - O(n), SC - O(n)
     def isPalindrome(self, head: ListNode) -> bool:
-        # TC - O(n), SC - O(n)
+        
         nums = []
         
         while head:
@@ -20,3 +21,39 @@ class Solution:
             start += 1
             end -= 1
         return True
+
+    # TC - O(n), SC - O(1)
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow = head
+        fast = head
+        
+        # 2 --> 3 --> 7 --> 3 --> 2
+       
+        # find middle element using the slow pointer
+        while(fast != None and fast.next != None):
+            slow = slow.next
+            fast = fast.next.next
+        # slow = node of 7
+        # fast = node of second 2
+
+        # 2 --> 3 --> 2 --> 3 -- 7
+        # reversing the second part of the linked list
+        prev = None
+        while(slow != None):
+            nextNode = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nextNode
+        
+        # left = node of first 2
+        # right = node of second 2
+        # check for palindrome
+        left = head
+        right = prev
+        while(right != None):
+            if(left.val != right.val):
+                return False
+            left = left.next
+            right = right.next
+        return True
+            
