93185e4c7a7bdf6be4d5b19db337aa6243c04219
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-06-29 15:29:11 +0100

    adding changes

--- DSA Questions & Solutions/Arrays/Two Sum/solution.py
+++ DSA Questions & Solutions/Arrays/Two Sum/solution.py
@@ -1,4 +1,4 @@
-# Brute force solution
+# Brute force solution - TC - O(n-squared), SC - O(1)
 class Solution:
     def twoSum(self, nums, target):
         i = 0
@@ -9,4 +9,25 @@ class Solution:
                 if(nums[i] + nums[j] == target):
                     return[i,j]
 
-# Optimal solution
+# Optimal solution - TC - O(n), SC - O(n)
+class Solution:
+    def twoSum(self, nums, target):
+        complement_dict = {}
+        
+        for i in range(len(nums)):
+            match = target - nums[i]
+            if(nums[i] in complement_dict):
+                return [complement_dict[nums[i]], i]
+            else:
+                complement_dict[match] = i
+
+# Explanation:
+# [2,7,11,15], target = 9
+# complement_dict = {}
+# - First iteration
+# match = 7
+
+# complement_dict = {7: 0}
+# - Second iteration
+# match = 2
+# nums[i] is in complement_dict, then return the indices
