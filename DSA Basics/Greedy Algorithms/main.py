d332e15d4e7eba5fabf13c54d3c8c57645dc1344
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-09-02 00:46:09 +0100

    made changes

---
+++ DSA Basics/Greedy Algorithms/main.py
@@ -0,0 +1,8 @@
+'''
+    - Greedy algorithm is an algorithm that makes a greedy (optimal) choice at every single step in the solution in the problem.
+    - The greedy choice is defined by some rule.
+    - Greedy Algorithm Properties (when to use a greedy algorithm to solve a problem)
+        - Greedy choice property: A global (or overall) optimal solution can be reached by choosing the optimal choice at every step.
+        - Optimal substructure: A problem has an optimal substructure if an optimal solution to the entire problem contains the optimal solutions to the sub-problems.
+    - Examples: Fractional Knapsack Problem, Dijkstra's Algorithm, Minimum Spanning Tree (MST) Problem, etc.
+'''
