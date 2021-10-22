428211b9a5abce4297c191b616708d2c7091b40d
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-10-22 20:58:42 +0100

    made changes

--- LeetCode Questions/Arrays/Maximum Sum Subarray 2/solution.js
+++ LeetCode Questions/Arrays/Maximum Sum Subarray 2/solution.js
@@ -1,18 +1,25 @@
-// Naive Solution
-let maxSubarraySum = function(nums, n) {
-    let maxSum;
+function maxSubarraySum(arr, n) {
+
+    if (arr.length < n) return null;
+
+    let total = 0;
+    for (let i=0; i<n; i++){
+        total += arr[i];
+    }
+
+    let currentTotal = total;
+    for (let i = n; i < arr.length; i++) {
+        currentTotal += arr[i] - arr[i-n];
+        total = Math.max(total, currentTotal);
+    }
     
+    return total;
 }
 
 
 
-maxSubarraySum([1,2,5,2,8,1,5],2) // 10
-maxSubarraySum([1,2,5,2,8,1,5],4) // 17
-maxSubarraySum([4,2,1,6],1) // 6
-maxSubarraySum([4,2,1,6,2],4) // 13
-maxSubarraySum([],4) // null
-
-
-
-
-// Optimal Solution
+console.log(maxSubarraySum([1,2,5,2,8,1,5],2)) // 10
+console.log(maxSubarraySum([1,2,5,2,8,1,5],4)) // 17
+console.log(maxSubarraySum([4,2,1,6],1)) // 6
+console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
+console.log(maxSubarraySum([],4)) // null
