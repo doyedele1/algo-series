c00d9c05a5567000e17e821f5f0901a807584f46
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-10-27 08:37:19 +0100

    added new questions and changed the folder structure

---
+++ LeetCode Questions/349. Intersection of Two Arrays/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intersection(self, nums1, nums2):
+        seen = {}
+        res = []
+        
+        for num in nums1:
+            if num in seen:
+                seen[num] += 1
+            else:
+                seen[num] = 1
+        print(seen)
+        
+        for num in nums2:
+            if num in seen and seen[num] > 0:
+                res.append(num)
+                seen[num] = 0
+    
+        return res
