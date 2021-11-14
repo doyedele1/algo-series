2016a695fd22b4f66ce8c76c1d699b1d73e63bcd
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-11-14 01:38:04 +0100

    pushed latest changes

--- LeetCode Questions/9. Palindrome Number/solution.py
+++ LeetCode Questions/9. Palindrome Number/solution.py
@@ -1,19 +1,27 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        rev_x = self.reverse(x)
-        if x == rev_x: return True
-        return False
+        s = str(x)
+        reversed_str = ""
         
-    def reverse(self, x):
-        prev = 0
-        res = 0
+        for i in range(len(s) - 1, -1, -1):
+            reversed_str += s[i]
         
-        while x != 0:
-            pop = x % 10
-            x = x // 10
+        return reversed_str == s 
+        
+#         rev_x = self.reverse(x)
+#         if x == rev_x: return True
+#         return False
+        
+#     def reverse(self, x):
+#         prev = 0
+#         res = 0
+        
+#         while x != 0:
+#             pop = x % 10
+#             x = x // 10
             
-            res = res * 10 + pop
-            if (res - pop) / 10 != prev: return 0
-            prev = res
+#             res = res * 10 + pop
+#             if (res - pop) / 10 != prev: return 0
+#             prev = res
             
-        return res
+#         return res
