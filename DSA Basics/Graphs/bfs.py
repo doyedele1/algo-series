abafba986ee2ff15b27a17fad3fc4e6bce568857
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-11-27 02:21:52 +0100

    bfs changes

--- DSA Basics/Graphs/bfs.py
+++ DSA Basics/Graphs/bfs.py
@@ -1,1 +1,19 @@
-TC - 
+# TC - O(V+E)
+# SC - O(V)
+
+def bfs(Node start):
+    queue = LinkedList(start)
+    seen = set()
+
+    queue.add(start)
+
+    while queue:
+        curr = queue.pop()
+
+        if not curr in seen:
+            seen.add(curr)
+            print(curr)
+        
+        for adjacent in adjacents(curr):
+            if not adjacent in seen:
+                queue.add(adjacent)
