a4e155fd561654341c08a108cd03e65303c54f2a
doyedele1 <demilade.oyedele@gmail.com>
2021-12-05 01:48:20 +0100

    Edited problem statement of question 13

--- LeetCode Questions/13. Roman to Integer/solution.py
+++ LeetCode Questions/13. Roman to Integer/solution.py
@@ -14,7 +14,8 @@ class Solution:
         
     '''
     Thought process = 
-        When the lower roman numeral comes before the higher one, we subtract the lower equivalent integer from the higher equivalent           integer to get the result. While, when the higher roman numeral comes before the lower one, we can add the quivalent integer of         the higher roman numeral to the result.
+        When the lower roman numeral comes before the higher one, we subtract the lower equivalent integer from the higher equivalent integer to get the result. 
+        While, when the higher roman numeral comes before the lower one, we can add the quivalent integer of the higher roman numeral to the result.
     Complexity analysis =
         TC - O(n). We only need to iterate through the given string of roman numerals and perform the algorithm explained above.
         SC - O(1). The hashmap created uses a constant space. The length of the conversion from roman numerals to integer doesn't vary.
