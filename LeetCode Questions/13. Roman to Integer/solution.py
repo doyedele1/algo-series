20742b6c8a785db9098035f969e02d4926206d01
doyedele1 <demilade.oyedele@gmail.com>
2021-12-05 01:51:54 +0100

    Resolved question 13

--- LeetCode Questions/13. Roman to Integer/solution.py
+++ LeetCode Questions/13. Roman to Integer/solution.py
@@ -1,16 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
-        rom_to_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         integer = 0
-        n = len(s)
+        dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         
-        for i in range(n):
-            if i + 1 < len(s) and rom_to_int[s[i+1]] > rom_to_int[s[i]]:
-                integer -= rom_to_int[s[i]]
+        for i in range(len(s)):
+            if i + 1 < len(s) and dict[s[i+1]] > dict[s[i]]:
+                integer -= dict[s[i]]
+            
             else:
-                integer += rom_to_int[s[i]]
+                integer += dict[s[i]]
+                
         return integer
+
+
         
     '''
     Thought process = 
