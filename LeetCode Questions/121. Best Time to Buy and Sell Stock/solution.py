1e97ebd53ca1bf6068aa02775c4986a93a38eecc
doyedele1 <demilade.oyedele@gmail.com>
2021-12-10 03:34:43 +0100

    Resolved question 121 on LeetCode

--- LeetCode Questions/121. Best Time to Buy and Sell Stock/solution.py
+++ LeetCode Questions/121. Best Time to Buy and Sell Stock/solution.py
@@ -1,17 +1,26 @@
-from typing import List
-
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float("inf")
         max_profit = 0
+        min_price = float("inf")
         
         for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
+            price = prices[i]
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
         return max_profit
+                    
+    '''
+        TC - O(N) where N is the size of prices list
+        SC - O(1) since there is no extra space used in the computer memory. The two variables do not take              much space in the memory
+        Explanation:
+        [7,1,5,3,6,4]
+        - Initialize max_profit variable as 0
+        - Initialize min_price as infinity which is the largest price there can ever be
         
-#         prices = [7,1,5,3,6,4]
-# Output: 5
+        - Iterations:
+            - Ensure you know what the min_price is at every iteration
+            - And compare the price with previous min_price to get the profit
+            - The iteration with the largest profit should be obtained and returned     
+    '''
