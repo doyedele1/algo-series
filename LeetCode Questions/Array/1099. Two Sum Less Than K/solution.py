94f69f841d05ce21a0219083b48ce685f5f67c4b
doyedele1 <demilade.oyedele@gmail.com>
2021-12-27 14:47:55 +0100

    Resolved question 1099 on LeetCode

--- LeetCode Questions/Array/1099. Two Sum Less Than K/solution.py
+++ LeetCode Questions/Array/1099. Two Sum Less Than K/solution.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
         nums.sort()
@@ -17,3 +16,8 @@ class Solution:
             else:
                 right -= 1
         return res
+    
+    '''
+        TC - O(nlogn)
+        SC - O(n) or O(log n)
+    '''
