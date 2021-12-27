8f0e77f51cab25c56d477de90cd0aaef080f7bab
doyedele1 <demilade.oyedele@gmail.com>
2021-12-27 14:25:41 +0100

    Solved question 14 on LeetCode

---
+++ LeetCode Questions/String/14. Longest Common Prefix/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        
+        '''
+            flower      flow        flight
+            i
+            str
+        '''
+        
+        for i in range(len(strs[0])):
+            for str in strs:
+                if i == len(str) or strs[0][i] != str[i]:
+                    return res
+            res += strs[0][i]
+            
+        return res
