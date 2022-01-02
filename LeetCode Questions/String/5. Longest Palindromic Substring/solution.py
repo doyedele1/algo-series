93158ca0dd76aebf5f28b08c8c11ef50efa217e7
doyedele1 <demilade.oyedele@gmail.com>
2022-01-02 23:57:59 +0100

    Solved question 5 on LC

--- LeetCode Questions/String/5. Longest Palindromic Substring/solution.py
+++ LeetCode Questions/String/5. Longest Palindromic Substring/solution.py
@@ -2,8 +2,8 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         def helper(left, right):
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                left += 1
-                right -= 1
+                left -= 1
+                right += 1
             return s[left+1:right]
         
         res = ""
