56b54e0043aaee89aafb6557537ab2eef356e9ec
doyedele1 <demilade.oyedele@gmail.com>
2022-01-05 00:15:36 +0100

    Solved question 105 on LC

--- LeetCode Questions/Tree/105. Construct Binary Tree from Preorder and Inorder Traversal/solution.py
+++ LeetCode Questions/Tree/105. Construct Binary Tree from Preorder and Inorder Traversal/solution.py
@@ -7,29 +7,36 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         p = collections.deque(preorder)
         n = len(preorder)
         
-        lookup = {v:i for i, v in enumerate(inorder)}
+        # building a hashmap to store value and its index
+        lookup = {}
+        for i, v in enumerate(inorder):
+            lookup[v] = i
+        
         
         def helper(start, end):
+            # if there are no elements to construct the tree
             if start > end:
                 return None
             else:
                 cand = p.popleft()
                 root = TreeNode(cand)
                 middle = lookup[cand]
+                # building left and right subtree excluding the root
                 root.left = helper(start, middle - 1)
                 root.right = helper(middle + 1, end)
+                
                 return root
             
         return helper(0, n-1)
     
     
     '''
-        TC - O(n)
-        SC - O(n) for the lookup
+        TC - O(n) to build the lookup hashmap and the tree
+        SC - O(n) for the memory used by the lookup hashmap and the tree storage
     '''
