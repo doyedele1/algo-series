b62443b8ba8d91a216577662b0811ba6f220a940
doyedele1 <demilade.oyedele@gmail.com>
2022-01-07 00:20:36 +0100

    Solved question 249 on LC

---
+++ LeetCode Questions/String/249. Group Shifted Strings/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        
+        
+        
+        '''
+            - Design a hash function that can identify the pattern of the input string
+                - "acef"  --> "bdfg"
+                - 0245    -->  1356   --> 2467
+        '''
