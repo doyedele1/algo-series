64025261172849faa21cbaa54270c126d1084c83
doyedele1 <demilade.oyedele@gmail.com>
2022-01-16 00:35:06 +0100

    Solved question 249 on LeetCode

--- LeetCode Questions/String/249. Group Shifted Strings/solution.py
+++ LeetCode Questions/String/249. Group Shifted Strings/solution.py
@@ -1,13 +1,21 @@
 from typing import List
 
-
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         
         
         
         '''
-            - Design a hash function that can identify the pattern of the input string
-                - "acef"  --> "bdfg"
-                - 0245    -->  1356   --> 2467
+            Explanation:
+                - Two things to understand
+                    1. Between characters, there is a difference of one
+                    2. There can be rotation in the alphabets (as seen in the case of "zab")
+
+                - We will form a dictionary to help with the grouping.
+                    - Key will be the difference between characters and value will be the strings with the distance same as the difference
+                        - For distance = 1 going in the positive direction, key = "11"
+                        - For distance = 1 going in the reversed direction, key = "-1-1"
+                    - Concept of rotation (abc), c + 2
+                        - (Length of string + index of c + how much are we moving) % length of string
+                        - (3 + 2 + 2) % 3 = 1. Index 1 which is b
         '''
