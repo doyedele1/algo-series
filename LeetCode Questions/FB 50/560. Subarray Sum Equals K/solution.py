4c21856ec17033ffc9a09347f40c5f42706ebab1
doyedele1 <demilade.oyedele@gmail.com>
2022-01-18 00:44:05 +0100

    Solved question 560 on LeetCode

--- LeetCode Questions/FB 50/560. Subarray Sum Equals K/solution.py
+++ LeetCode Questions/FB 50/560. Subarray Sum Equals K/solution.py
@@ -2,23 +2,59 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
-        run_sum = 0
-        dict = {}
-        dict[0] = 1
+        res = 0
+        curr_sum = 0
+        prefix_sum = {0: 1}
         
-        for i in range(len(nums)):
-            run_sum += nums[i]
-            if (run_sum - k) in dict:
-                count += dict[run_sum - k]
-            if not run_sum in dict:
-                dict[run_sum] = 0
-            dict[run_sum] = dict.get(run_sum) + 1
-            
-                    
-        return count
+        for n in nums:
+            curr_sum += n
+            diff = curr_sum - k
+
+            res += prefix_sum.get(diff, 0) # 0 is the custom default value if the key diff is not found
+            prefix_sum[curr_sum] = prefix_sum.get(curr_sum, 0) + 1
+
+        return res
     
     '''
-        T(C) - O(n)
-        S(C) - We use a hashmap that can contain n distinct inputs in the worst case
+        Explanation
+        Brute-force solution:
+            - We have n-squared different subarrays inside of the array
+            - We are doing a ton of repeated work by getting the sum of all the single subarrays
+
+        More optimal solution:
+            - We will use a hashmap where the key is the prefix_sum and the value is the count of how many times that prefix_sum occurs
+            - Can we chop off a prefix to get the required k target number. i.e. for the contiguous array of 4 for [1,1,1,1,1,1], 
+                - sum = 4, sum - k = 1. 1 is present in the array
+                - Does there exist a prefix starting from the beginning that has a value of 1?
+                    - We can, using the count
+                - There is an empty prefix of 0 with a count of 1. For case of [1,1,1,1,1,1] and k = 3 for the contiguous array of first 3
+        
+            [1,-1,1,1,1,1]
+            - 0: 1
+
+            - Sum = 1-3 = -2
+            - Does -2 exist in the prefix map? No, then we continue and add the prefix just computed of 1 and add to the map and increment count by 1
+            - 1: 1
+
+            - Sum = 0-3 = -3
+            - No -3 in the prefix map.
+            - 0: 2
+
+            - Sum = 1-3 = -2
+            - 1: 2
+
+            - Sum = 2-3 = -1
+            - 2: 1
+
+            - Sum = 3-3 = 0
+            - We do have the prefix sum of 0
+            - Count = 2, add to res. Res = 2
+            - 3: 1
+
+            - Sum = 4-3 = 1
+            - We do have the prefix of 1
+            - Count = 2, add to res. Res = 4
+
+        TC - O(n)
+        SC - O(n). We use the hashmap that can contain n distinct inputs in the worst case
     '''
