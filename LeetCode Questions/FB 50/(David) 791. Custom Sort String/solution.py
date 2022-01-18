54084de90dbb1a39ccb5403d0f04a4fa017e36ca
doyedele1 <demilade.oyedele@gmail.com>
2022-01-18 01:55:35 +0100

    Solved question 791 on LeetCode

---
+++ LeetCode Questions/FB 50/(David) 791. Custom Sort String/solution.py
@@ -0,0 +1,52 @@
+import collections
+
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        lookup = collections.defaultdict(int)
+        i = 0
+        
+        for c in order:
+            lookup[c] = i
+            i += 1
+            
+        return "".join(sorted(s, key=lambda x: lookup[x]))
+
+        '''
+            Explanation:
+                - We can use a lookup dict to match these characters to integers
+                - Each time we see a new char, we increase the iterator and sort using the lambda function
+        '''
+        
+
+# COUNT AND WRITE        
+class Solution(object):
+    def customSortString(self, S, T):
+        # count[char] will be the number of occurrences of
+        # 'char' in T.
+        count = collections.Counter(T)
+        ans = []
+
+        # Write all characters that occur in S, in the order of S.
+        for c in S:
+            ans.append(c * count[c])
+            # Set count[c] = 0 to denote that we do not need
+            # to write 'c' to our answer anymore.
+            count[c] = 0
+
+        # Write all remaining characters that don't occur in S.
+        # That information is specified by 'count'.
+        for c in count:
+            ans.append(c * count[c])
+
+        return "".join(ans)
+
+
+        '''
+            Explanation:
+                - Count the elemtns of T
+                - If we have some count[char] = number of occurences of char in T, we can return these elements in any order
+                - The order is S + (characters not in S in any order)
+                
+            TC - O(len(S) + len(T))
+            SC - O(len(T)
+        '''
