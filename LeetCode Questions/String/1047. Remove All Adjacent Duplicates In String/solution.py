72cf721940ae27108fe7cc1fb1445abffd2a3fcd
doyedele1 <demilade.oyedele@gmail.com>
2022-01-25 18:43:40 +0100

    Solved question 1047 on LeetCode

--- LeetCode Questions/String/1047. Remove All Adjacent Duplicates In String/solution.py
+++ LeetCode Questions/String/1047. Remove All Adjacent Duplicates In String/solution.py
@@ -8,6 +8,23 @@ class Solution:
             else: stack.append(char)
         return "".join(stack)
     
-        # "abbaca"
-        
-        # TC - O(n), SC - O(n-d) where d is the total length of all duplicates
+
+    '''
+        Explanation:
+            - We will have a stack to keep track of the characters.
+            - We will iterate through the string and check if the current character is already in the stack.
+                - If it is, we will pop the last character from the stack.
+                - If it is not, we will append the current character to the stack.
+            - We will return the string after joining the stack.
+            
+            - TC - O(n) where n is the length of the string
+            - SC - O(n-d) where d is the total length of all duplicates
+
+            "abbaca"
+            stack = [a]
+            stack = [ab]
+            stack = [a]
+            stack = []
+            stack = [c]
+            stack = [ca]
+    '''
