05bdb426f905f46b9361e16740cc6dfdc241c71a
doyedele1 <demilade.oyedele@gmail.com>
2022-02-17 07:58:48 +0100

    Solved question 12 on LC

--- LeetCode Questions/Math/13. Roman to Integer/solution.py
+++ LeetCode Questions/Math/13. Roman to Integer/solution.py
@@ -24,6 +24,8 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         res = 0
         dict = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
+
+
         
         for i in range(len(s)):
             if i + 1 < len(s) and dict[s[i+1]] > dict[s[i]]:
