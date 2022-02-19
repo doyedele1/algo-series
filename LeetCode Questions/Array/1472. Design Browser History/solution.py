0d0ae56519daad13d0d858cb9bce6b1cb828555d
doyedele1 <demilade.oyedele@gmail.com>
2022-02-19 11:10:21 +0100

    Solved question 1472 on LC

--- LeetCode Questions/Array/1472. Design Browser History/solution.py
+++ LeetCode Questions/Array/1472. Design Browser History/solution.py
@@ -1,40 +1,41 @@
 '''
     Explanation:
         - history:   [leetcode, youtube, facebook, google]
-        - urlPointer:               i
-        visit: pop out all urls after the urlPointer, append the url to visit and move the urlPointer
+        - urlPosition:               i
+        visit: pop out all urls after the urlPosition, append the url to visit and move the urlPosition
         
         - history:   [leetcode, youtube, facebook, google], steps = 2
-        - urlPointer:               i
+        - urlPosition:               i
         back and forward --> you don't want to go out of bound because of the steps
         
         TC - O(n) for all functions
         SC - O(n) for all functions --> to store the browserHistory
 '''
 
+
 class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.browserHistory = [homepage]
-        self.urlPointer = 0
+        self.urlPosition = 0
 
     def visit(self, url: str) -> None:
-        while len(self.browserHistory) - 1 > self.urlPointer:
+        while self.urlPosition < len(self.browserHistory) - 1:
             self.browserHistory.pop()
         self.browserHistory.append(url)
-        self.urlPointer += 1
+        self.urlPosition += 1
 
     def back(self, steps: int) -> str:
-        while self.urlPointer > 0 and steps > 0:
-            self.urlPointer -= 1
+        while self.urlPosition > 0 and steps > 0:
+            self.urlPosition -= 1
             steps -= 1
-        return self.browserHistory[self.urlPointer]
+        return self.browserHistory[self.urlPosition]
 
     def forward(self, steps: int) -> str:
-        while self.urlPointer < len(self.browserHistory) - 1 and steps > 0:
-            self.urlPointer += 1
+        while self.urlPosition < len(self.browserHistory) - 1 and steps > 0:
+            self.urlPosition += 1
             steps -= 1
-        return self.browserHistory[self.urlPointer]
+        return self.browserHistory[self.urlPosition]
         
 
 
