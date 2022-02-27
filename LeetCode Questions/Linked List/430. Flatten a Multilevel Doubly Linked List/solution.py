8bc45c3f885a5c1edd10cb4a72f84b19c3b92aff
doyedele1 <demilade.oyedele@gmail.com>
2022-02-27 22:56:40 +0100

    question-430-on-LC-solved

--- LeetCode Questions/Linked List/430. Flatten a Multilevel Doubly Linked List/solution.py
+++ LeetCode Questions/Linked List/430. Flatten a Multilevel Doubly Linked List/solution.py
@@ -1,6 +1,32 @@
-from typing import Optional
+'''
+    Explanation:
+        - We will have a recursive helper (flatten_helper) function that takes in the head pointer (curr = head, tail = head)
+        - While curr is not null,
+            - Here we can store curr, next and tail on the first node (head)
+            - If no child,
+                current = next and next will point to next of current
+            - If it has child, 
+                We flatten the child. i.e. child_tail will be flatten_helper function called on child. Recursive function here
+                curr --> next = child
+                child_tail --> next = next
+                curr --> child = null
+                child --> prev = curr
+                - If next, 
+                    next --> prev = child_tail
+                - If next is null, 
+                    curr = tail
+                - If curr, tail = curr
+        - return tail
 
-# Definition for a Node
+
+
+        TC - O(n) where n is the number of nodes in the list, we visit each node once from beginning to end
+        SC - O(n), the recursion call stack which can be of the order of n - O(n). In the worst case, the nodes are chained with each other only with the child pointers. In this case, the recursive calls would pile up and take n space in the function call stack
+'''
+
+
+
+# Definition for a Node.
 class Node:
     def __init__(self, val, prev, next, child):
         self.val = val
@@ -9,52 +35,38 @@ class Node:
         self.child = child
 
 
+from typing import Optional
+
+
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if head != None: 
+        if head != None: # if head is there, i.e. the linked list is not empty
             self.flatten_helper(head)
         return head
     
     def flatten_helper(self, head):
         curr, tail = head, head
-        while curr != None:
+        while curr:
             child = curr.child
-            next = curr.next
-            if child != None:
-                _tail = self.flatten_helper(child)
-
-                _tail.next = next
-                if next != None: next.prev = _tail
-
+            next = curr.next # so that we can return the next of child_tail as the next we stored
+            
+            if child: # if there is child
+                child_tail = self.flatten_helper(child)
+                
+                child_tail.next = next
+                if next: next.prev = child_tail
+                    
                 curr.next = child
                 child.prev = curr
-
+                
                 curr.child = None
-
+                
                 curr = tail   
-            else:
+            
+            else: # if there is no child
                 curr = next
-            if curr != None: tail = curr
+                
+            if curr: tail = curr # tail cannot be null because tail will always be there because it is running when we have a child
                 
         # return the tail of the flatten list
         return tail
-        
-        '''
-            Explanation:
-                We will have a recursive (flatten_helper) function that takes in the head pointer and checks for child on current
-                    - If no child,
-                        current = next and next will point to next of current
-                    - If it has child, 
-                        We flatten the child. i.e. tail will be flatten_helper function called on child. Recursive function here
-                        curr --> next = child
-                        tail --> next = next
-                        curr --> child = null
-                        child --> prev = curr
-                    - If next, 
-                        next --> prev = tail
-                    - curr = tail
-                    - return tail
-                    
-                TC - O(n) where n is the number of nodes in the list
-                SC - O(1), except the recursion call stack which can be of the order of n - O(n). In the worst case, the binary tree might be unbalanced. i.e. nodes are chained with each other only with the child pointers. In this case, the recursive calls would pile up and take n space in the function call stack
-        '''
