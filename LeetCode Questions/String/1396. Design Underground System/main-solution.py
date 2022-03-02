38e851e7c84aebb37528e1d29d4107673897a299
doyedele1 <demilade.oyedele@gmail.com>
2022-03-02 12:21:49 +0100

    solved-question-1396-on-LC

--- LeetCode Questions/String/1396. Design Underground System/main-solution.py
+++ LeetCode Questions/String/1396. Design Underground System/main-solution.py
@@ -4,7 +4,7 @@
         - Each customer can only be checked into a station one place at a time
         
         1. Create a map called arrivals. key = id, value = tuple containing stationName and time --> (stationName, t)
-        2. To keep track of all previous travel between any two stations, we create another map called travels. id = stationNames separated by a delimiter, value = tuple of count, total --> [count, total]
+        2. To keep track of all previous travels between any two stations, we create another map called travels. key = stationNames separated by a delimiter, value = count, total --> [count, total]
         
         Example:
         Calls                   Arrivals                Travels
@@ -12,25 +12,25 @@
         checkIn(2,A,10)         2 => (A,10)
         checkOut(1,B,5) --> look up the customer id in the arrivals map, remove the customer from arrivals map and compute the diff between start and end times
         checkOut(1,B,5)                                 A,B => (1,2)
-        getAverageTime(A,B) --> combine the two stations and look it up in the travels map = 2 / 1 = 2
+        getAverageTime(A,B) --> combine the two stations and look it up in the travels map = 2 / 1 = 2.000000
         checkOut(2,B,20)                                A,B ==> (2,12)
         getAverageTime(A,B) --> 12 / 2 = 6
         
         
         
         TC - O(1) for all functions
-        SC - O(n + m) where n is the number of arrivals and m is the number of averages. n grows larger when we have customers that check in but don't check out - we remove those entries from the arrivals map in the checkout function. m increases the more times the customers check out.
+        SC - O(n + m) where n is the number of arrivals and m is the number of travels. n grows larger when we have customers that check in but don't check out - we remove those entries from the arrivals map in the checkout function. m increases the more times the customers check out at various stations.
 '''
-
-from collections import defaultdict
-
 
 class UndergroundSystem:
     def __init__(self):
-        self.arrivals = defaultdict()
-        self.travels = defaultdict()
+        self.arrivals = dict()
+        self.travels = dict()
+        # {} --> O(n)
         
     def checkIn(self, id: int, stationName: str, t: int) -> None:
+        # if id in self.arrivals:
+        #     raise Exception("customer already checked in")
         self.arrivals[id] = (stationName, t)
         
     def checkOut(self, id: int, stationName: str, t: int) -> None:
@@ -44,8 +44,9 @@ class UndergroundSystem:
         
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         count, total = self.travels[startStation + "," + endStation]
+        if count == 0 and total == 0:
+            return "No customer has checked out at this moment"
         return total / count
-    
     
     
     '''
@@ -60,7 +61,7 @@ class UndergroundSystem:
         checkIn(27,L,10)        45 => (L,3)
                                 32 => (P,8)
                                 27 => (L,10)
-        
+                                
         checkOut(45,W,15)       32 => (P,8)
                                 27 => (L,10)                (L,W) => (1,12)
         
@@ -78,5 +79,5 @@ class UndergroundSystem:
         
         checkout(10,W,38)       empty                       (L,W) => (3,36)
         
-        getAverageTime()                                    (L,W) = 36/3 = 12 
+        getAverageTime()                                    (L,W) = 36/3 = 12
     '''
