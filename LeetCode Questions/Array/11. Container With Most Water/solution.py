61fe006c8fe5d4c4e687e2ca20782cd4574e8a75
doyedele1 <demilade.oyedele@gmail.com>
2022-03-06 23:56:36 +0100

    Solving question 11 on LC

---
+++ LeetCode Questions/Array/11. Container With Most Water/solution.py
@@ -0,0 +1,23 @@
+'''
+    Explanation:
+        Naive Solution
+        - Can we consider the possible areas for each pair of heights?
+        - For each pair, we check the area by multiplying the width to the height without spill over? The height is the bottleneck here and we need to consider the minimum height between the pair.
+        
+        
+        Optimal Solution
+
+'''
+
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_amount = 0
+        
+        for i in range(len(height)):
+            for j in range(i + 1, len(height)):
+                area_of_rectangle = (j - i) * (min(height[i], height[j]))
+                max_amount = max(max_amount, area_of_rectangle)
+                
+        return max_amount
