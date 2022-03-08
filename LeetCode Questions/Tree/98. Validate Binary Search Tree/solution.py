825d771fa84c10e909cd094c06aa38b393853b22
doyedele1 <demilade.oyedele@gmail.com>
2022-03-08 20:04:11 +0100

    solved-question-98-on-LC

--- LeetCode Questions/Tree/98. Validate Binary Search Tree/solution.py
+++ LeetCode Questions/Tree/98. Validate Binary Search Tree/solution.py
@@ -1,3 +1,16 @@
+'''
+    Explanation:
+        - On the root node, we can have range of (-infinity, +infinity)
+        - On the left subtree, we can have a range of (-infinity, root.val)
+        - On the right subtree, we can have a range of (root.val, +infinity)
+        - If the node.val is not in the ranges, then it is not a BST
+        - We call the function recursively on (roor.left, lower, root.val) and (root.right, root.val, upper)
+
+        # TC - O(n)
+        # SC - O(n)
+'''
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,18 +22,15 @@
 class Solution:
     def isValidBST(self, root) -> bool:
         return self.validate(root, float("-inf"), float("inf"))
-        
-    def validate(self, node, min_value, max_value):
+
+    def validate(self, node, lower, upper):
         if node == None:
             return True
 
-        if node.val <= min_value or node.val >= max_value:
+        if node.val <= lower or node.val >= upper:
             return False
 
-        return self.validate(node.left, min_value, node.val) and self.validate(node.right, node.val, max_value)
-
-    # TC - O(n)
-    # SC - O(n)
+        return self.validate(node.left, lower, node.val) and self.validate(node.right, node.val, upper)
 
 # Recursive Inorder Traversal
 class Solution:
