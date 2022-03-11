7a5097c84198aa859b22d1c256ca9217cfa7ebf5
doyedele1 <demilade.oyedele@gmail.com>
2022-03-12 00:51:58 +0100

    solved-question-200-on-LC

--- BB's Top 50/(11) 200. Number of Islands/solution.py
+++ BB's Top 50/(11) 200. Number of Islands/solution.py
@@ -1,3 +1,15 @@
+'''
+    Explanation:
+        DFS
+            [
+                [1,1,1,1],
+                [1,1,1,1],
+                [1,1,1,1],
+                [1,1,1,1]
+            ]
+            Space Complexity: O(m*n), recursive call stack calls all the 1s
+'''
+
 class Solution:
     def numIslands(self, grid):
         if not grid:
@@ -24,33 +36,105 @@ class Solution:
                     islands += 1
         return islands
 
+'''
+        BFS
+            [
+                [1,1,1,1],
+                [1,1,1,1],
+                [1,1,1,1],
+                [1,1,1,1]
+            ]
 
-# Iterative solution using bfs
-#         if not grid:
-#             return 0
+            neighbors = [(0,0)]
+            
+            [
+                [0,0,1,1],
+                [0,1,1,1],
+                [1,1,1,1],
+                [1,1,1,1]
+            ]
 
-#         rows, cols = len(grid), len(grid[0])
-#         visited = set()
-#         islands = 0
+            neighbors = [(1,0), (0,1)]
+            
+            [
+                [0,0,1,1],
+                [0,0,1,1],
+                [0,1,1,1],
+                [1,1,1,1]
+            ]
 
-#         for r in range(rows):
-#             for c in range(cols):
-#                 if grid[r][c] == "1" and (r,c) not in visited:
-#                     bfs(r,c)
-#                     islands += 1
-#         return islands
+            neighbors = [(0,1), (2,0), (1,1)]
+            
+            [
+                [0,0,0,1],
+                [0,0,1,1],
+                [0,1,1,1],
+                [1,1,1,1]
+            ]
 
-# def bfs(r,c):
-#     q = collections.deque()
-#     visited.add((r,c))
-#     q.append((r,c))
+            neighbors = [(2,0), (1,1), (0,2)]
+            
+            [
+                [0,0,0,1],
+                [0,0,1,1],
+                [0,0,1,1],
+                [0,1,1,1]
+            ]
 
-#     while q:
-#         row, col = q.popleft()
-#         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-#         for dr, dc in directions:
-#             r, c = row+dr, col+dc
-#             if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visited):
-#                 q.append((r,c))
-#                 visited.add((r,c))
+            neighbors = [(1,1), (0,2), (2,1), (3,0)]
+            
+            [
+                [0,0,0,1],
+                [0,0,0,1],
+                [0,0,1,1],
+                [0,1,1,1]
+            ]
+
+            neighbors = [(0,2), (2,1), (3,0), (1,2)]
+            
+            [
+                [0,0,0,0],
+                [0,0,0,1],
+                [0,0,1,1],
+                [0,1,1,1]
+            ]
+
+            neighbors = [(2,1), (3,0), (1,2), (0,3)]
+            
+            Space Complexity: O(min(nr, nc))
+'''
+import collections
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid: return 0
+        rows = len(grid)
+        cols = len(grid[0])
+        islands = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    neighbors = collections.deque([(r,c)])
+                    grid[r][c] = "2"
+                    
+                    while neighbors:
+                        row, col = neighbors.popleft()
+                        if row - 1 >= 0 and grid[row-1][col] == "1":
+                            neighbors.append((row-1, col))
+                            grid[row-1][col] = "2"
+                        if row + 1 < rows and grid[row+1][col] == "1":
+                            neighbors.append((row+1, col))
+                            grid[row+1][col] = "2"
+                        if col - 1 >= 0 and grid[row][col-1] == "1":
+                            neighbors.append((row, col-1))
+                            grid[row][col-1] = "2"
+                        if col + 1 < cols and grid[row][col+1] == "1":
+                            neighbors.append((row, col+1))
+                            grid[row][col+1] = "2"
+                            
+                    islands += 1
+                        
+        return islands
 
