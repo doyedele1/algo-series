68e7f7fa1c60372a7416b2621534212cf6b60eba
doyedele1 <demilade.oyedele@gmail.com>
2022-03-11 16:12:06 +0100

    solved-question-1169-on-LC

---
+++ BB's Top 50/(6) 1169. Invalid Transactions/solution.py
@@ -0,0 +1,84 @@
+'''
+    Explanation I: Brute-force solution
+        - Convert the transactions to individual item of the valid data types and store it in an array. i.e. alice = string, 20 = integer, 800 = integer and mtv = string
+        - Loop through the new array and find where amount > 1000. Append that to result array and convert the transaction to a string splitting each item by comma
+            - Nested loop to check for same name, and if time difference is less than or equal to 60 and if different city,
+                - Append that to the res array and convert the transaction to a string splitting each item by comma
+        
+        - TC - O(n-squared)
+        - SC - O(n)
+'''
+# class Solution:
+#     def invalidTransactions(self, transactions: List[str]) -> List[str]:
+#         res = []
+#         for i, trans1 in enumerate(transactions):
+#             name1, time1, amount1, city1 = trans1.split(',')
+#             if int(amount1) > 1000:
+#                 res.append(trans1)
+#                 continue
+#             for x, trans2 in enumerate(transactions):
+#                 name2, time2, amount2, city2 = trans2.split(',')
+#                 if name1 == name2 and abs(int(time1) - int(time2)) <= 60 and city1 != city2:                
+#                     res.append(trans1)
+#                     # deals with duplicates by only appending t1
+#                     break
+#         return res
+
+
+
+
+
+'''
+    Explanation II: Optimal Solution
+        - Store all transactions done at a particular time in a dictionary. key = time, value = object --> key = person_name, value = location
+        - ['alice,20,800,mtv', 'bob,50,1200,mtv', 'bob,20,100,beijing']
+        - {   
+            20: {'alice': {'mtv'}, 'bob': {'beijing'}}, 
+            50: {'bob': {'mtv'}}
+            }
+        - Check if the amount is invalid, add it to the result
+        - Go through the time (+-60), check if a transaction(the same person, a different city), add it to the result
+        
+        TC - O(n)
+        SC - O(n)
+'''
+import collections
+from typing import List
+
+class Solution:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        res = []
+
+        # Store all transactions done at a particular time in a dictionary
+        transaction = collections.defaultdict(dict)
+        for trans in transactions:
+            name, str_time, amount, city = trans.split(",")
+            time = int(str_time)
+
+            if name not in transaction[time]:
+                transaction[time][name] = {city, }
+            else:
+                transaction[time][name].add(city)
+
+        for trans in transactions:
+            name, str_time, amount, city = trans.split(",")
+            time = int(str_time)
+
+            if int(amount) > 1000:
+                res.append(trans)
+                continue
+
+            for invalid_time in range(time-60, time+61):
+                if invalid_time not in transaction:
+                    continue
+                if name not in transaction[invalid_time]:
+                    continue
+
+                trans_city = transaction[invalid_time][name]
+
+                # If transactions were done in a different city
+                if city not in trans_city or len(trans_city) > 1:
+                    res.append(trans)
+                    break
+
+        return res
--- BB's Top 50/(8) 394. Decode String/solution.py
+++ BB's Top 50/(8) 394. Decode String/solution.py
@@ -1,3 +1,33 @@
+'''
+    Explanation:
+        3[a2[c]]
+        - Number in front of a character shows how many times that character will be multiplied
+        - 3 applies to everything in the bracket
+        - acc * 3 ==> accaccacc
+        
+        
+        - Nested brackets will make the solution tricky/complicated
+        - We will have to solve the inner brackets before the outer brackets --> recursive solution
+        
+        - The stack tells us explicitly once we are done with a subproblem, where to pop back up to
+        
+        - Going char by char in the string and add to the stack
+        54[ab6[cd]]
+        stack = (54,[,a,b,6,[c,d
+                (54,[,a,b,6
+                We pop any integer after what we've popped 6 --> 6 * cd
+                (54,[,a,b,6*cd,
+                (54,
+                Take everything we pop and multiply by int before what we've popped --> 54 and add to the stack
+                Return the stack after everything
+                
+        TC - O(maxK ^ countK * n) where maxK is the maximum value of K and countK is the count of nested k values and n is the maximum length of encoded string. For example:
+        s = 20[a10[bc]], maxK = 20, countK = 2 as there are two nested K values (20 and 10). n = 2 as there are 2 encoded strings (a and bc).
+        
+        SC - O(sum(maxK ^ countK * n))) where maxK is the maximum value of K, countK is the count of nested K values and n is the maximum length of encoded string
+        max stack size = maxK[nmaxK[n]]
+'''
+
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
@@ -18,34 +48,3 @@ class Solution:
                 stack.append(int(k) * substr)
                 
         return "".join(stack)
-        
-        '''
-            Explanation:
-                3[a2[c]]
-                - Number in front of a character shows how many times that character will be multiplied
-                - 3 applies to everything in the bracket
-                - acc * 3 ==> accaccacc
-                
-                
-                - Nested brackets will make the solution tricky/complicated
-                - We will have to solve the inner brackets before the outer brackets --> recursive solution
-                
-                - The stack tells us explicitly once we are done with a subproblem, where to pop back up to
-                
-                - Going char by char in the string and add to the stack
-                54[ab6[cd]]
-                stack = (54,[,a,b,6,[c,d
-                        (54,[,a,b,6
-                        We pop any integer after what we've popped 6 --> 6 * cd
-                        (54,[,a,b,6*cd,
-                        (54,
-                        Take everything we pop and multiply by int before what we've popped --> 54 and add to the stack
-                        Return the stack after everything
-                        
-                TC - O(maxK ^ countK * n) where maxK is the maximum value of K and countK is the count of nested k values and n is the maximum length of encoded string. For example:
-                s = 20[a10[bc]], maxK = 20, countK = 2 as there are two nested K values (20 and 10). n = 2 as there are 2 encoded strings (a and bc).
-                
-                SC - O(sum(maxK ^ countK * n))) where maxK is the maximum value of K, countK is the count of nested K values and n is the maximum length of encoded string
-                max stack size = maxK[nmaxK[n]]
-        '''
-        
