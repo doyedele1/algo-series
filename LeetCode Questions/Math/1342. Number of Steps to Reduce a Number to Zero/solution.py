c6ed2acc66c888a206f47d386c3c7d01cd583efe
doyedele1 <demilade.oyedele@gmail.com>
2022-03-13 22:44:47 +0100

    solved-question-1342-on-LC

--- LeetCode Questions/Math/1342. Number of Steps to Reduce a Number to Zero/solution.py
+++ LeetCode Questions/Math/1342. Number of Steps to Reduce a Number to Zero/solution.py
@@ -1,14 +1,22 @@
-# TC - O(log n), SC - O(1)
+# Naive Solution --> TC - O(log n), SC - O(1)
+# class Solution:
+#     def numberOfSteps(self, num: int) -> int: 
+#         if num == 0:
+#             return 0
+#         steps = 0
+#         while num > 0:
+#             if num % 2 == 0:
+#                 num //= 2
+#             else:
+#                 num -= 1
+#             steps += 1
+            
+#         return steps
+        
+# Counting Bits --> TC - O(log n), SC - O(log n)
 class Solution:
     def numberOfSteps(self, num: int) -> int: 
-        if num == 0:
-            return 0
-        steps = 0
-        while num > 0:
-            if num % 2 == 0:
-                num //= 2
-            else:
-                num -= 1
-            steps += 1
-            
-        return steps
+        binaryEquivalent = bin(num)[2:]
+        ones = binaryEquivalent.count("1")
+        zeroes = len(binaryEquivalent) - ones
+        return (ones * 2) + zeroes - 1
