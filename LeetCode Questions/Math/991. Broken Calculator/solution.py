a1eefc25d87236d03880b7307f5f5f95de562453
doyedele1 <demilade.oyedele@gmail.com>
2022-03-13 21:30:53 +0100

    solved-question-991-on-LC

--- LeetCode Questions/Math/991. Broken Calculator/solution.py
+++ LeetCode Questions/Math/991. Broken Calculator/solution.py
@@ -1,19 +1,48 @@
 '''
     Explanation:
+        two operations: multiply by 2, or subtract 1 from startValue
+        We can instead perform the following:
+            - divide by 2 if target number is even
+            - add 1 to target number if odd
+
+        Hence,
+            - While target is larger than startValue,
+                - If target is odd, add 1 to target
+                - Else, divide target by 2
+            - Then we need to do startValue - target additions to reach startValue
+
+        Example 1: startValue = 2, target = 3
+
+        res = 1
+        target = 4
+
+        res = 2
+        target = 2
+        answer = 2 + 2 - 2 = 2
+
+        Example 3: startValue = 3, target = 10
+        res = 1
+        target = 5
+
+        res = 2
+        target = 6
+
+        res = 3
+        target = 3
+        answer = 3 + 3 - 3 = 3
+
         TC - O(log target)
         SC - O(1)
 '''
-
-
 
 class Solution:
     def brokenCalc(self, startValue: int, target: int) -> int:
         res = 0
         while target > startValue:
             res += 1
-            if target % 2:
+            if target % 2 == 1:
                 target += 1
             else:
-                target /= 2
+                target //= 2
 
-        return int(res + startValue - target)
+        return res + startValue - target
