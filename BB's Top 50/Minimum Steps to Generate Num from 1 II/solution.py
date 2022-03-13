f91e9af4debb40951f817d323b0fd985bdfaf06e
doyedele1 <demilade.oyedele@gmail.com>
2022-03-13 22:26:16 +0100

    solved-Bloomberg-question-on-LC

--- BB's Top 50/Minimum Steps to Generate Num from 1 II/solution.py
+++ BB's Top 50/Minimum Steps to Generate Num from 1 II/solution.py
@@ -1,9 +1,43 @@
-def solution(n):
+# TC - O(2^n)
+def solution1(n):
     if n == 1:
         return 0
 
     elif n % 2 == 0:
-        return 1 + solution(n // 2)
+        return 1 + solution1(n // 2)
     
     else:
-        return 1 + min(solution(n + 1), solution(n - 1))
+        return 1 + min(solution1(n + 1), solution1(n - 1))
+
+print(solution1(15))
+
+'''
+    Initialize steps = 0
+    While number is greater than one, perform the following,
+        count++ for each iteration
+        if num % 2 == 0, perform division
+        else if num % 4 == 3, perform increment
+        else perform decrement (as odd % 4 is either 1 or 3)
+    return count;
+
+    TC - O(log n)
+'''
+def solution2(n):
+    if n == 1:
+        return 0
+
+    steps = 0
+
+    while n > 1:
+        if n % 2 == 0:
+            n //= 2
+        # n==3(special edge case), decrement by 1
+        elif n % 4 == 1 or n == 3:
+            n -= 1
+        else:
+            n += 1
+        steps += 1
+
+    return steps
+print(solution2(7))
+
