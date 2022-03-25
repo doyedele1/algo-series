aab1bcbc8d26745d042dea1b5afc1f423de41b2b
doyedele1 <demilade.oyedele@gmail.com>
2022-03-25 23:01:49 +0100

    solved-GS-OA

--- Goldman Sachs/Grouping Transactions by Items' Names/solution.py
+++ Goldman Sachs/Grouping Transactions by Items' Names/solution.py
@@ -1,9 +1,10 @@
 '''
-    TC: O(n) where n is the number of transactions
-    SC: O(n) to keep track of the frequencies of each item
+    Explanation:
+        TC: O(n) where n is the number of transactions
+        SC: O(n) to keep track of the frequencies of each item
 '''
 
-def solution(transactions):
+def solution1(transactions):
     dict = {}
 
     for i in transactions:
@@ -14,4 +15,31 @@ def solution(transactions):
     res = [' '.join([t[0], str(t[1])]) for t in dict]
     return res
 
-print(solution(['mouse', 'mouse', 'notebook', 'mouse', 'keyboard', 'printer', 'printer', 'printer']))
+print("solution1", solution1(['mouse', 'mouse', 'notebook', 'mouse', 'keyboard', 'printer', 'printer', 'printer']))
+
+
+def solution2(transactions):
+    inputDict = {}
+    for i in transactions:
+        if i in inputDict:
+            inputDict[i] += 1
+        else:
+            inputDict[i] = 1
+    
+    freqCount = {}
+    for i in inputDict:
+        if inputDict[i] in freqCount:
+            freqCount[inputDict[i]].append(i)
+        else:
+            freqCount[inputDict[i]] = [i]
+
+    dict(sorted(freqCount.items()))
+
+    res = []
+    for i in freqCount:
+        sortedFreqCount = sorted(freqCount[i])
+        for j in sortedFreqCount:
+            res.append(j + " " + str(i))
+    return res
+
+print("solution2", solution2(['mouse', 'mouse', 'notebook', 'mouse', 'keyboard', 'printer', 'printer', 'printer']))
