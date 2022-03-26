b257873253e931aed0542bde7ffbef3620d7ddc7
doyedele1 <demilade.oyedele@gmail.com>
2022-03-26 22:23:16 +0100

    solved-GS-OA

---
+++ Goldman Sachs/Encryption Validity/solution.cpp
@@ -0,0 +1,7 @@
+vector<int> encryptionValidity(int instructionCount, int validityPeriod, vector<int> keys) {
+    long long int mdd = 0;
+
+    for(int k: keys)
+        long long int dd = 0;
+        for (long)
+}
