67232872d84d0004c92ec8b9c7880c19f139e927
doyedele1 <demilade.oyedele@gmail.com>
2022-03-27 23:54:30 +0100

    solved-GS-OA

--- Goldman Sachs/Double on Match/solution.py
+++ Goldman Sachs/Double on Match/solution.py
@@ -8,25 +8,25 @@ def solution1(arr, b):
     
     return b
 
+# TC - O(n), SC - O(n)
 def solution2(arr, b):
     nums = set()
 
     for i in range(len(arr)):
         nums.add(arr[i])
+    print(nums)
 
     while b in nums:
         b *= 2
 
     return b
 
+# print(solution1([1,2,4,11,12,8], 2)) # 16
+# print(solution1([1,2,3,1,2], 1)) # 4
+# print(solution1([1,1,1], 1)) # 2
+# print(solution1([2,4,5,6,8], 1)) # 1
 
-
-print(solution1([1,2,4,11,12,8], 2)) # 16
-print(solution1([1,2,3,1,2], 1)) # 4
-print(solution1([1,1,1], 1)) # 2
-print(solution1([2,4,5,6,8], 1)) # 1
-
-print(solution1([1,2,4,11,12,8], 2))
-print(solution1([1,2,3,1,2], 1))
-print(solution1([1,1,1], 1))
-print(solution1([2,4,5,6,8], 1))
+# print(solution2([1,2,4,11,12,8], 2)) # 16
+# print(solution2([1,2,3,1,2], 1)) # 4
+# print(solution2([1,1,1], 1)) # 2
+# print(solution2([2,4,5,6,8], 1)) # 1
