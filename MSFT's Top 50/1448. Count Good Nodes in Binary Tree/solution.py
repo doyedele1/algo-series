07228156694e0d76c65b5d9efaa223339e714ed5
doyedele1 <demilade.oyedele@gmail.com>
2022-04-04 23:52:52 +0100

    solved-MSFT-question

--- MSFT's Top 50/1448. Count Good Nodes in Binary Tree/solution.py
+++ MSFT's Top 50/1448. Count Good Nodes in Binary Tree/solution.py
@@ -1,12 +1,28 @@
 '''
-    Explanation I: PreOrder (DFS) Traversal
-        - From the root node, go to the node, check if it is greater than the maximum value seen so far
+    Explanation I: PreOrder (DFS) Traversal - Recursive
+        - From the root node, check if it is greater than the maximum value seen so far, increment result
         - Update the maximum value seen so far
-        - Go to the left child next, check if it is greater than the maximum value seen so far
-        - Go to the right child next, check if it is greater than the maximum value seen so far
+        - Go to the left child next, check if it is greater than the maximum value seen so far by recursively calling the dfs function on the left subtree and passing the node.left and the maximum value seen so far
+        - Go to the right child next, check if it is greater than the maximum value seen so far by recursively calling the dfs function on the right subtree and passing the node.right and the maximum value seen so far
         
         - TC: O(n) where n is the number of nodes
         - SC: O(n) where n is the height of the tree (worst case- tree with a single path)
+        
+        
+        
+        
+    Explanation II: PreOrder (DFS) Traversal - Iterative
+        - Works the same with the previous approach
+        - stack = pair the nodes with the corresponding maximum value seen so far
+        
+        - Initialize stack and res
+        - Executing DFS, while the stack is not empty, pop from the stack
+        - If node.val is >= maxVal, increment res and push the children to the stack along with the maximum value between maxVal and node.val
+        
+        - TC: O(n) where n is number of nodes
+        - SC: O(n) for the stack. Worst case is when the right child has 2 children and the left child has no children and vice-versa. The stack will contain at most N/2 nodes at maximum depth
+        
+    Explanation III: BFS Traversal
 '''
 
 
@@ -29,4 +45,18 @@ class Solution1:
         
         res = 0   
         dfs(root, root.val)
+        return res
+
+
+class Solution2:
+    def goodNodes(self, root: TreeNode) -> int: 
+        stack = [(root, root.val)]
+        res = 0
+        
+        while stack:
+            node, maxVal = stack.pop()
+            if maxVal <= node.val: res += 1
+            
+            if node.left: stack.append((node.left, max(maxVal, node.val)))
+            if node.right: stack.append((node.right, max(maxVal, node.val)))
         return res
