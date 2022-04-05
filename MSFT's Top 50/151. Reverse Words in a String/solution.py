f012e47ca4ed8b3ff03dd5af648440b5a2b89f7a
doyedele1 <demilade.oyedele@gmail.com>
2022-04-05 23:56:16 +0100

    solved-MSFT-question

---
+++ MSFT's Top 50/151. Reverse Words in a String/solution.py
@@ -0,0 +1,66 @@
+'''
+    Explanation I: Using built-in split and reverse
+        - It is an in-space algorithm in Python
+        - Linear space complexity
+        
+        - TC: O(n) where n is the number of characters in the string
+        - SC: O(n), to store the result of split by spaces
+        
+    Explanation II: "  leetcode is  fun "
+        - Trim the spaces
+            "leetcode is fun"
+        - Convert string to array
+        - Reverse the whole array
+            "nuf si edocteel"
+        - Reverse each word
+            "fun is leetcode"
+        - Join the array to a string
+        
+
+'''
+
+
+
+class Solution2:
+    def reverseWords(self, s: str) -> str:
+        def trimSpaces(s):
+            i, j = 0, len(s) - 1
+            # remove leading spaces
+            while i <= j and s[i] == " ":
+                i += 1
+            
+            # remove trailing spaces
+            while i <= j and s[j] == " ":
+                j -= 1
+                
+            # reduce multiple spaces to a single space
+            res = []
+            while i <= j:
+                if s[i] != " ": res.append(s[i])
+                elif res[-1] != " ": res.append(s[i])
+                i += 1
+            return res
+        
+        def reverseArr(s, i, j):
+            while i < j:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+                
+        def reverseEachWord(s):
+            i = j = 0
+            
+            while i < len(s):
+                # go to the end of the word
+                while j < len(s) and s[j] != " ":
+                    j += 1
+                # reverse the word
+                reverseArr(s, i, j - 1)
+                # move to the next word
+                i = j + 1
+                j += 1
+        
+        l = trimSpaces(s)
+        reverseArr(l, 0, len(l) - 1)
+        reverseEachWord(l)
+        return "".join(l)
