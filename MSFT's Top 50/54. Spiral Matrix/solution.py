1c730afcc3c7f6fd0d3b47052624781b2d8a3d12
doyedele1 <demilade.oyedele@gmail.com>
2022-04-10 23:06:46 +0100

    solved-MSFT-question

---
+++ MSFT's Top 50/54. Spiral Matrix/solution.py
@@ -0,0 +1,52 @@
+'''
+    Explanation I: (Top, right, bottom and left) boundaries update
+        - Initialize the four boundaries and an output array
+        - Traverse the items in spiral order and add each item to the output array
+            - left --> right
+            - up --> down
+            - before moving from right to left, we need to ensure we are not on a row already visited
+            - before moving from down to up, we need to ensure we are not on a column already visited
+            - Update the boundaries as expected
+        - Return result
+        
+        - TC: O(m*n). where m = number of rows and n = number of columns
+        
+'''
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        up, left = 0
+        right, down = n - 1, m - 1
+        res = []
+        
+        while len(res) < m * n:
+            # left --> right
+            for col in range(left, right + 1):
+                res.append(matrix[up][col])
+            
+            # up --> down
+            for row in range(up, down + 1):
+                res.append(matrix[row][right])
+            
+            # to ensure we are not on a row we've visited or we are now on a different row
+            if up != down:
+                # right --> left
+                for col in range(right - 1, left - 1, -1):
+                    res.append(matrix[down][col])
+            
+            # to ensure we are not on a column we've visited or we are now on a different column
+            if left != right:
+                # down --> up
+                for row in range(down - 1, up, -1):
+                    res.append(matrix[row][left])
+            
+            # update boundaries
+            left += 1
+            right -= 1
+            up += 1
+            down -= 1
+        
+        return res
+                
