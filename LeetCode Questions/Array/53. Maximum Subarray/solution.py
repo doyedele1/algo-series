e04b1c591755c4605c4c1fac15732b9694456489
doyedele1 <demilade.oyedele@gmail.com>
2022-04-13 14:16:58 +0100

    solved-LC-question

---
+++ LeetCode Questions/Array/53. Maximum Subarray/solution.py
@@ -0,0 +1,43 @@
+'''
+    Explanation I: Brute-force solution
+        - We sum every subarray elements and find the maximum sum
+        - TC: O(n^2), SC: O(n)
+
+    Explanation II: Kadane's algorithm
+        - We keep track of the maximum sum at every index
+        - TC: O(n), SC: O(1)
+    [-2,1,-3,4,-1,2,1,-5,4]
+    maxSum = -2, maxAtEveryIndex = -2
+    maxSum = 1, maxAtEveryIndex = 1
+    maxSum = 1, maxAtEveryIndex = -2
+    maxSum = 4, maxAtEveryIndex = 4
+    maxSum = 4, maxAtEveryIndex = 3
+    maxSum = 5, maxAtEveryIndex = 5
+    maxSum = 6, maxAtEveryIndex = 6
+    maxSum = 6, maxAtEveryIndex = 1
+    maxSum = 6, maxAtEveryIndex = 5
+'''
+
+def maxSubArray1(nums):
+    maxSum = 0
+
+    for i in range(0, len(nums)):
+        sum = 0
+        for j in range(i, len(nums)):
+            sum += nums[j]
+            maxSum = max(maxSum, sum)
+    return maxSum
+
+
+def maxSubArray2(nums):
+    maxSum = maxAtEveryIndex = nums[0]
+
+    for i in range(1, len(nums)):
+        maxAtEveryIndex = max(nums[i], maxAtEveryIndex + nums[i])
+        maxSum = max(maxSum, maxAtEveryIndex)
+    return maxSum
+
+
+print(maxSubArray2([-2,1,-3,4,-1,2,1,-5,4]))
+# print(maxSubArray2([1]))
+# print(maxSubArray2([5,4,-1,7,8]))
