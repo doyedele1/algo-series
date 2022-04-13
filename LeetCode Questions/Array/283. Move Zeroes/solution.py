4657a55df895478a63ee8ebc45eefec4ed219c7f
doyedele1 <demilade.oyedele@gmail.com>
2022-04-13 13:54:37 +0100

    changed-folder-structure

--- LeetCode Questions/Array/283. Move Zeroes/solution.py
+++ LeetCode Questions/Array/283. Move Zeroes/solution.py
@@ -24,15 +24,10 @@
         Result ==> [1, 3, 12, 0, 0]
 '''
 
-
 from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
         j = 0
         for i in range(len(nums)):
             if nums[i] != 0:
