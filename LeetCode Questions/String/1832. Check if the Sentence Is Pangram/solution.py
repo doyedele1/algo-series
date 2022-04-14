3584aad7bb9361e33d0e8675edd7ed2b5df92fef
doyedele1 <demilade.oyedele@gmail.com>
2022-04-14 02:44:49 +0100

    solved-LC-question

--- LeetCode Questions/String/1832. Check if the Sentence Is Pangram/solution.py
+++ LeetCode Questions/String/1832. Check if the Sentence Is Pangram/solution.py
@@ -1,3 +1,19 @@
+'''
+    Explanation I: 
+        - The idea is to use a hashmap to store the characters in the sentence.
+        - If the sentence is a pangram, then all the characters in the alphabet (26) should be in the hashmap.
+        - TC: O(n), SC: O(n)
+    
+    Explanation II:
+        - The idea is to use a fixed size array to store the characters in the sentence. Size = 26
+        - Loop through the sentence and increment the count of each character in the array.
+        - Loop through the array and check the count of any character
+            - If the count is less than 1, return False
+        - Return True
+        - TC: O(n), SC: O(1)
+'''
+
+
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         # First solution
