0c8befeb61bb4932b7de5c7de6858b937b0f4eb2
doyedele1 <demilade.oyedele@gmail.com>
2022-04-18 00:11:56 +0100

    solved-LC-questions

--- LeetCode Questions/Array/735. Asteroid Collision/solution.py
+++ LeetCode Questions/Array/735. Asteroid Collision/solution.py
@@ -1,24 +1,36 @@
 '''
     Explanation:
-        - TC: O(n) where n is the number of asteriods. Stack append and pop is O(1)
-        - SC: O(n) where n is the number of asteroids left in the stack
+        [-1, 3, 2, -3]
+        stack = [-1], asteroid = -1
+        stack = [-1, 3], asteroid = 3
+        stack = [-1, 3, 2], asteroid = 2
+        stack = [-1], asteroid = 0
+        TC: O(n) where n is the number of asteriods. Stack append and pop is O(1)
+        SC: O(n) where n is the number of asteroids left in the stack
 '''
-
-from typing import List
 
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
         
         for asteroid in asteroids:
+            
+            # we can only start the collision when these conditions are met
             while stack and asteroid < 0 and stack[-1] > 0:
                 diff = asteroid + stack[-1]
-                if diff < 0: stack.pop()
-                elif diff > 0: asteroid = 0
+                # asteroid is going to win here, so remove the top element in the stack
+                if diff < 0:
+                    stack.pop()
+                # the top of the stack is going to win and asteroid is to be destroyed
+                elif diff > 0:
+                    asteroid = 0
+                # when both will be destroyed
                 else:
                     asteroid = 0
                     stack.pop()
+            
+            # only add to the stack if asteroid is positive or negative
             if asteroid:
                 stack.append(asteroid)
-        
+                
         return stack
