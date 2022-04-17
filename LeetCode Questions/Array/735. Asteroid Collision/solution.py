c51dc8bd1f35f42d9e3cc01d1b058858593af830
doyedele1 <demilade.oyedele@gmail.com>
2022-04-17 01:03:23 +0100

    solved-LC-question

---
+++ LeetCode Questions/Array/735. Asteroid Collision/solution.py
@@ -0,0 +1,26 @@
+'''
+    Explanation:
+        - TC: O(n) where n is the number of asteriods. Stack append and pop is O(1)
+        - SC: O(n) where n is the number of asteroids left in the stack
+'''
+
+from typing import List
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        i = 0
+        
+        while i < len(asteroids):
+            if asteroids[i] > 0:
+                stack.append(asteroids[i])
+            else:
+                while len(stack) and stack[-1] > 0 and stack[-1] < abs(asteroids[i]):
+                    stack.pop()
+                if len(stack) == 0 or stack[-1] < 0:
+                    stack.append(asteroids[i])
+                elif stack[-1] == abs(asteroids[i]):
+                    stack.pop()
+            i += 1
+        
+        return stack
