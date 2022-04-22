cd31e826e5c4037ba18ca1a7bf8fab35bd9f3aa3
doyedele1 <demilade.oyedele@gmail.com>
2022-04-22 23:58:19 +0100

    solved-Amazon-OA-question

--- Amazon OA/Minimize Memory/solution.py
+++ Amazon OA/Minimize Memory/solution.py
@@ -1,16 +1,16 @@
 def solution(processes, m):
-    window_start = 0
-    total_sum = sum(processes)
-    pref_sum = 0
-    max_sum = 0
+    windowStart = 0
+    totalSum = sum(processes)
+    prefSum = 0
+    maxSum = 0
 
     for i in range(len(processes)):
-        win_size = (i-window_start)+1
-        pref_sum+= processes[i]
-        if win_size==m:
-            max_sum= max(max_sum, pref_sum)
-            pref_sum-=processes[window_start]
-            window_start+=1
+        windowSize = (i - windowStart) + 1
+        prefSum += processes[i]
+        if windowSize == m:
+            maxSum= max(maxSum, prefSum)
+            prefSum -= processes[windowStart]
+            windowStart += 1
 
-    output = total_sum- max_sum
-    return output
+    res = totalSum - maxSum
+    return res
