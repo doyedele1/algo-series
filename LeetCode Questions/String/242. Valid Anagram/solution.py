32144958e07ee99bf1aebf1cab949561337c5adc
doyedele1 <demilade.oyedele@gmail.com>
2022-04-22 19:19:34 +0100

    solved-LC-question

--- LeetCode Questions/String/242. Valid Anagram/solution.py
+++ LeetCode Questions/String/242. Valid Anagram/solution.py
@@ -1,26 +1,25 @@
-# def valid_anagram(s, t):
-#     if len(s) != len(t):
-#             return False
+def valid_anagram(s, t):
+    if len(s) != len(t):
+            return False
         
-#     dict_s = {}
+    dict_s = {}
     
-#     for i in s:
-#         if i in dict_s:
-#             dict_s[i] += 1
-#         else: 
-#             dict_s[i] = 1
+    for i in s:
+        if i in dict_s:
+            dict_s[i] += 1
+        else: 
+            dict_s[i] = 1
             
-#     for x in t:
-#         if x in dict_s:
-#             dict_s[x] -= 1
-#         else: 
-#             return False
+    for x in t:
+        if x in dict_s:
+            dict_s[x] -= 1
+        else: 
+            return False
         
-#     for c in dict_s:
-#         if dict_s[c] != 0:
-#             return False
-#     return True
-        
+    for c in dict_s:
+        if dict_s[c] != 0:
+            return False
+    return True
 
 # print(valid_anagram("cat", "tac")) # return True
 # print(valid_anagram("listen", "silent")) # return True
