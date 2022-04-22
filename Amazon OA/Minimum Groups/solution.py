45e23e4ea2816eb0c0a12d62c957d2e6072059a5
doyedele1 <demilade.oyedele@gmail.com>
2022-04-22 19:19:10 +0100

    solved-Amazon-OA

--- Amazon OA/Minimum Groups/solution.py
+++ Amazon OA/Minimum Groups/solution.py
@@ -1,13 +1,27 @@
+'''
+    [1,13,6,8,9,3,5]
+    
+    [1,3,5,6,8,9,13]
+
+    First iteration: count = 1, start = 0
+    Second iteration: count = 1, start = 0
+    Third iteration: count = 1, start = 0
+    Fourth iteration: count = 2, start = 3
+    Fifth iteration: count = 2, start = 3
+    Sixth iteration: count = 2, start = 3
+    Seventh iteration: count = 3, start = 6
+'''
+
 def minimumGroups(arr, k):
     arr.sort()
     start = 0
-    if(len(arr) == 0): return 0
+    if len(arr) == 0: return 0
     
-    count = 1;
+    count = 1
     for i in range(len(arr)):
-        if(arr[i] - arr[start] > k):
+        if (arr[i] - arr[start]) > k:
             count += 1
             start = i
-    return count;
+    return count
 
 print(minimumGroups([1,13,6,8,9,3,5], 4))
