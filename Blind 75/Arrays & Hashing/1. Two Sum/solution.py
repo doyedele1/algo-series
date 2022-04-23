c3d781183207baca19432fae53c2631b53e43457
doyedele1 <demilade.oyedele@gmail.com>
2022-04-23 21:56:35 +0100

    solved-Blind-questions

--- Blind 75/Arrays & Hashing/1. Two Sum/solution.py
+++ Blind 75/Arrays & Hashing/1. Two Sum/solution.py
@@ -1,42 +1,49 @@
+from typing import List
+
 # Brute force solution - TC - O(n-squared), SC - O(1)
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         i = 0
         j = len(nums) - 1
         
         for i in range(0, len(nums)):
             for j in range(i+1, len(nums)):
-                if(nums[i] + nums[j] == target):
+                if (nums[i] + nums[j]) == target:
                     return[i,j]
 
-# Another Optimal solution - TC - O(n), SC - O(n)
-# Explanation:
-    # [2,7,11,15], target = 9
-    # complement = {}
-    # - First iteration
-    # match = 7
-    # complement = {7: 0}
 
-    # - Second iteration
-    # match = 2
-    # nums[i] is in complement, then return the indices
+
+# Most Optimal solution - TC - O(n), SC - O(n)
+'''
+    Explanation:
+    - Use a hashmap and map the value to its index
+        # [2,7,11,15], target = 9
+        # numsMap = {}
+        # - First iteration
+        # diff = 7
+        # numsMap = {2: 0}
+
+        # - Second iteration
+        # diff = 2
+        # diff is in numsMap, then return the indices
+'''
     
 class Solution:
-    def twoSum(self, nums, target):
-        complement = {}
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsMap = {}
         
-        for i in range(len(nums)):
-            match = target - nums[i]
-            if nums[i] in complement:
-                return [complement[nums[i]], i]
-            else:
-                complement[match] = i
+        for index, num in enumerate(nums):
+            diff = target - num
+            if diff in numsMap:
+                return [numsMap[diff], index]
+            numsMap[num] = index
 
-# Optimal solution - TC - O(n), SC - O(1)
+
+
+# Another optimal solution - TC - O(n-squared worst case), SC - O(1)
 class Solution:
-    def twoSum(self, nums, target):
-        for i in range(0,len(nums)):
-            diff = target - nums[i]
-                
-            if diff in nums and nums.index(diff) != i:
-                return [i,nums.index(diff)]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for index, num in enumerate(nums):
+            diff = target - num
+            if diff in nums and nums.index(diff) != index:
+                return [index, nums.index(diff)]
