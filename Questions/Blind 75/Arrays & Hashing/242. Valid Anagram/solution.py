f2549ac89febaace2b9db68965efd67d6ec0f1aa
doyedele1 <demilade.oyedele@gmail.com>
2022-04-25 17:44:09 +0100

    added-OA-questions

---
+++ Questions/Blind 75/Arrays & Hashing/242. Valid Anagram/solution.py
@@ -0,0 +1,32 @@
+'''
+    Explanation: 
+        TC - O(s + t) where s and t are the lengths of the strings s and t
+'''
+
+
+def valid_anagram(s, t):
+    if len(s) != len(t):
+            return False
+        
+    dict_s = {}
+    
+    for i in s:
+        if i in dict_s:
+            dict_s[i] += 1
+        else: 
+            dict_s[i] = 1
+            
+    for x in t:
+        if x in dict_s:
+            dict_s[x] -= 1
+        else: 
+            return False
+        
+    for c in dict_s:
+        if dict_s[c] != 0:
+            return False
+    return True
+
+# print(valid_anagram("cat", "tac")) # return True
+# print(valid_anagram("listen", "silent")) # return True
+# print(valid_anagram("program", "function")) # return False
