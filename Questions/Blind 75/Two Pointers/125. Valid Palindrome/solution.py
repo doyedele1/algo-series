e32023cefbf3ec360d59326f929fcbd9608c5874
doyedele1 <demilade.oyedele@gmail.com>
2022-04-27 00:17:11 +0100

    solved-Blind-question

--- Questions/Blind 75/Two Pointers/125. Valid Palindrome/solution.py
+++ Questions/Blind 75/Two Pointers/125. Valid Palindrome/solution.py
@@ -1,56 +1,30 @@
 # alphanumeric characters - A to Z, a to z, 0 to 9
 
-# TC - O(n), SC - O(n) [newStr and the reveresed newStr space]
-class Solution:
+# TC: O(n), SC: O(n) [newStr and the reveresed newStr space]
+class Solution1:
     def checkAlphanumeric(self, char):
-        if (ord(char) >= 97 and ord(char) <= 122) or (ord(char) >= 65 and ord(char) <= 90) or (ord(char) >= 48 and ord(char) <= 57): return True
-        return False
+        return (ord('A') <= ord(char) <= ord('Z') or ord('a') <= ord(char) <= ord('z') or ord('0') <= ord(char) <= ord('9'))
     
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
         newStr = ""
-        for i in s:
-            if self.checkAlphanumeric(i):
-                newStr += i
+        for char in s:
+            if self.checkAlphanumeric(char):
+                newStr += char
         return newStr == newStr[::-1]
 
-# Another solution
-def isPalindrome(s):
-    s = s.lower()
-    new_str = ''
-    for i in s:
-        if i.isalnum():
-            new_str += i
-    
-    i = 0
-    j = len(new_str) - 1
-
-    while (i < j):
-        if(new_str[i] != new_str[j]):
-            return False
-        i += 1
-        j -= 1
-
-    return True
-
-# TC: O(n) where n is the length of the string, SC - O(1)
-class Solution3:
+# TC: O(n) where n is the length of the input string, SC: O(1)
+class Solution2:
     def isPalindrome(self, s: str) -> bool:
-        i = 0
-        j = len(s) - 1
+        left = 0
+        right = len(s) - 1
         
-        while i < j:
-            while not self.checkAlphanumeric(s[i]) and i < j:
-                i += 1
-            while not self.checkAlphanumeric(s[j]) and i < j:
-                j -= 1
-            if s[i] == s[j] or s[i].lower() == s[j].lower():
-                i += 1
-                j -= 1
-            else: return False
+        while left < right:
+            while not self.checkAlphanumeric(s[left]) and left < right: # left < right - the left pointer doesn't pass the right pointer and it doesn't go out of bounds
+                left += 1
+            while not self.checkAlphanumeric(s[right]) and left < right:
+                right -= 1
+            if s[left].lower() != s[right].lower(): return False
+            left += 1
+            right -= 1
         return True
-
-# print(valid_palindrome("level"))
-# print(valid_palindrome("algorithm"))
-# print(valid_palindrome("A man, a plan, a canal: Panama."))
-# print(valid_palindrome("0P"))
