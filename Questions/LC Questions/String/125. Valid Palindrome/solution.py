0942c5ff76c8812e4d8789d0780ca6e2d36bf18a
doyedele1 <demilade.oyedele@gmail.com>
2022-04-27 00:17:35 +0100

    solved-Blind-question

--- Questions/LC Questions/String/125. Valid Palindrome/solution.py
+++ Questions/LC Questions/String/125. Valid Palindrome/solution.py
@@ -1,65 +1,30 @@
-# Naive solution: TC - O(2n), SC - O(n)
-def isPalindrome1(s):
-    s = s.lower()
-    new_str = ''
-    for i in s:
-        if i.isalnum():
-            new_str += i
-    reversed_str = new_str[::-1]
+# alphanumeric characters - A to Z, a to z, 0 to 9
 
-    if (new_str == reversed_str):
-        return True
-    return False
-
-# Another solution
-def isPalindrome(s):
-    s = s.lower()
-    new_str = ''
-    for i in s:
-        if i.isalnum():
-            new_str += i
-    
-    i = 0
-    j = len(new_str) - 1
-
-    while (i < j):
-        if(new_str[i] != new_str[j]):
-            return False
-        i += 1
-        j -= 1
-
-    return True
-
-# Another solution
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        i = 0
-        j = len(s) - 1
-        
-        while i < j:
-            while not self.checkAlphanumeric(s[i]) and i < j:
-                i += 1
-            while not self.checkAlphanumeric(s[j]) and i < j:
-                j -= 1
-            if s[i] == s[j] or s[i].lower() == s[j].lower():
-                i += 1
-                j -= 1
-            else: return False
-        return True
-        
-        
+# TC: O(n), SC: O(n) [newStr and the reveresed newStr space]
+class Solution1:
     def checkAlphanumeric(self, char):
-        if (ord(char) >= 97 and ord(char) <= 122) or (ord(char) >= 65 and ord(char) <= 90) or (ord(char) >= 48 and ord(char) <= 57):
-            return True
-        return False
+        return (ord('A') <= ord(char) <= ord('Z') or ord('a') <= ord(char) <= ord('z') or ord('0') <= ord(char) <= ord('9'))
     
-    
-    '''
-        TC - O(n) where n is the length of the string
-        SC - O(1)
-    '''
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        newStr = ""
+        for char in s:
+            if self.checkAlphanumeric(char):
+                newStr += char
+        return newStr == newStr[::-1]
 
-# print(valid_palindrome("level"))
-# print(valid_palindrome("algorithm"))
-# print(valid_palindrome("A man, a plan, a canal: Panama."))
-# print(valid_palindrome("0P"))
+# TC: O(n) where n is the length of the input string, SC: O(1)
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            while not self.checkAlphanumeric(s[left]) and left < right: # left < right - the left pointer doesn't pass the right pointer and it doesn't go out of bounds
+                left += 1
+            while not self.checkAlphanumeric(s[right]) and left < right:
+                right -= 1
+            if s[left].lower() != s[right].lower(): return False
+            left += 1
+            right -= 1
+        return True
