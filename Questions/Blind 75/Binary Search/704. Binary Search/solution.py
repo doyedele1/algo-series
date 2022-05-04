0db15327c6bae6a8b89fd8836b59f88d5d045274
doyedele1 <demilade.oyedele@gmail.com>
2022-05-05 00:42:50 +0100

    solved-Blind-question

--- Questions/Blind 75/Binary Search/704. Binary Search/solution.py
+++ Questions/Blind 75/Binary Search/704. Binary Search/solution.py
@@ -1,14 +1,18 @@
-class Solution:
-    def search(self, nums, target):
-        i = 0
-        j = len(nums) - 1
+'''
+    - TC: O(log n)
+    - SC: O(1)
+'''
 
-        while(i <= j):
-            middle = (i + j) // 2
-            if(nums[middle] == target):
-                return middle
-            elif(nums[middle] < target):
-                i = middle + 1
-            else:
-                j = middle - 1
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        i, j = 0, len(nums) - 1
+
+        while i <= j:
+            middle = i + ((j - i) // 2)
+            if target == nums[middle]: return middle
+            elif target < nums[middle]: j = middle - 1
+            else: i = middle + 1
+        
         return -1
