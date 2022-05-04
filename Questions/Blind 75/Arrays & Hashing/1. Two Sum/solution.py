7e61cb92e2062c0c81cd37d1d6040db74d94d4c5
doyedele1 <demilade.oyedele@gmail.com>
2022-05-04 23:53:38 +0100

    solved-LC-question

--- Questions/Blind 75/Arrays & Hashing/1. Two Sum/solution.py
+++ Questions/Blind 75/Arrays & Hashing/1. Two Sum/solution.py
@@ -12,20 +12,20 @@ class Solution:
                     return[i,j]
 
 
-
-# Most Optimal solution - TC - O(n), SC - O(n)
 '''
     Explanation:
-    - Use a hashmap and map the value to its index
-        # [2,7,11,15], target = 9
-        # numsMap = {}
-        # - First iteration
-        # diff = 7
-        # numsMap = {2: 0}
+        - Use a hashmap and map the value to its index
+            [2,7,11,15], target = 9
+            numsMap = {}
+            - First iteration
+                diff = 7
+                numsMap = {2: 0}
 
-        # - Second iteration
-        # diff = 2
-        # diff is in numsMap, then return the indices
+            - Second iteration
+                diff = 2
+                diff is in numsMap, then return the indices
+
+        - Most Optimal solution - TC - O(n), SC - O(n)
 '''
     
 class Solution:
@@ -37,7 +37,6 @@ class Solution:
             if diff in numsMap:
                 return [numsMap[diff], index]
             numsMap[num] = index
-
 
 
 # Another optimal solution - TC - O(n-squared worst case), SC - O(1)
---
+++ Questions/Blind 75/Linked List/206. Reverse Linked List/solution.py
@@ -0,0 +1,43 @@
+'''
+    Explanation I: Iterative solution
+        - 1 -> 2 -> 3 -> 4 -> 5
+        - Use two pointers: current that is set to head and previous that is set to null
+        - Reverse the node by making current.next to be the previous node
+        - Move both pointers. current = next, previous = current
+        - Save we broke the current.next link, we save the link before we end up breaking it
+        - We stop the iteration when current is null
+        
+        - TC: O(n), SC: O(1)
+'''
+
+
+# Definition for singly-linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution1:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous, current = None, head
+        
+        while current:
+            temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+
+        return previous
+
+class Solution2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        
+        reversed_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return reversed_head
