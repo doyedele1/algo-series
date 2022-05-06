81fb72b903d36107dd7b01305aba896c48772260
doyedele1 <demilade.oyedele@gmail.com>
2022-05-06 21:48:37 +0100

    solved-LC-question

--- Questions/LC Questions/Array/2260. Minimum Consecutive Cards to Pick Up/solution.py
+++ Questions/LC Questions/Array/2260. Minimum Consecutive Cards to Pick Up/solution.py
@@ -3,7 +3,23 @@
         [3,4,2,3,4,7]
         - Use a hashmap
             key - the card value
-            value - 
+            value - index occurence of the card value
+        
+        hashamp = {}, ans = Infinity
+        
+        hashmap = {3: 0}
+        
+        hashmap = {3: 0, 4: 1}
+        
+        hashmap = {3: 0, 4: 1, 2: 2}
+        
+        3 is in the hashmap, ans = min(Infinity and 3-0+1) = 4
+        hashmap = {3: 3, 4: 1, 2: 2}
+
+        4 is in the hashmap, ans = min(4, 4-1+1) = 4
+        hashmap = {3: 3, 4: 4, 2: 2}
+
+        hashmap = {3: 3, 4: 4, 2: 2, 7: 5}
             
         - TC: O(n), SC: O(1)
 '''
