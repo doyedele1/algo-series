64fc89bbfd395ac1e6860fdfa31a32fb6be35ac1
doyedele1 <demilade.oyedele@gmail.com>
2022-05-07 00:04:59 +0100

    solved-Blind--question

---
+++ Questions/Blind 75 / NC 150/Linked List/21. Merge Two Sorted Lists/solution.py
@@ -0,0 +1,36 @@
+'''
+    Explanation:
+        # Initialize a dummy new node
+        # And save your new node in a temporary node
+        # Loop through the two lists until both have null values
+            # if l1 < l2, add l1 to the temp node and move l1 pointer
+            # else add l2 and move l2 pointer
+            # move your temp node pointer 
+        # If l2 is not null, add l2 to the temp node
+        # If l1 is not null, add l1 to the temp node
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        newNode = ListNode(-1)
+        temp = newNode
+        
+        while l1 and l2: # while both l1 and l2 are not null
+            if(l1.val < l2.val):
+                temp.next = l1
+                l1 = l1.next
+            else: 
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        
+        if(l2 != None): temp.next = l2
+        else: temp.next = l1
+        
+        return newNode.next
