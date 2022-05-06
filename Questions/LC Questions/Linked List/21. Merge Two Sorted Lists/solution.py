1720543b0b344a724922d46f4d0033c16affdcc1
doyedele1 <demilade.oyedele@gmail.com>
2022-05-07 00:14:42 +0100

    solved-Blind-question

--- Questions/LC Questions/Linked List/21. Merge Two Sorted Lists/solution.py
+++ Questions/LC Questions/Linked List/21. Merge Two Sorted Lists/solution.py
@@ -1,21 +1,13 @@
 '''
     Explanation:
-        - Initialize a dummy new node
-        - Save the new node in a temp node
-        - Loop through the two lists until both have null values
+        # Initialize a dummy new node
+        # And save your new node in a temporary node
+        # Loop through the two lists until both have null values
             # if l1 < l2, add l1 to the temp node and move l1 pointer
             # else add l2 and move l2 pointer
-            # move the temp node pointer 
-        - If l2 is not null, add l2 to the temp node
-        - If l1 is not null, add l1 to the temp node
-
-        Time complexity : O(n + m)O(n+m)
-
-Because exactly one of l1 and l2 is incremented on each loop iteration, the while loop runs for a number of iterations equal to the sum of the lengths of the two lists. All other work is constant, so the overall complexity is linear.
-
-Space complexity : O(1)O(1)
-
-The iterative approach only allocates a few pointers, so it has a constant overall memory footprint.
+            # move your temp node pointer 
+        # If l2 is not null, add l2 to the temp node
+        # If l1 is not null, add l1 to the temp node
 '''
 
 # Definition for singly-linked list.
@@ -30,7 +22,7 @@ class Solution:
         temp = newNode
         
         while l1 and l2: # while both l1 and l2 are not null
-            if l1.val < l2.val:
+            if(l1.val < l2.val):
                 temp.next = l1
                 l1 = l1.next
             else: 
@@ -38,7 +30,7 @@ class Solution:
                 l2 = l2.next
             temp = temp.next
         
-        if l2: temp.next = l2
+        if(l2 != None): temp.next = l2
         else: temp.next = l1
         
         return newNode.next
