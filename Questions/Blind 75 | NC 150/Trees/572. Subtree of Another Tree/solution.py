fe0461f4cfe32bf809dd00338a9d72422868f818
doyedele1 <demilade.oyedele@gmail.com>
2022-05-08 00:28:50 +0100

    solved-Blind-question

--- Questions/Blind 75 | NC 150/Trees/572. Subtree of Another Tree/solution.py
+++ Questions/Blind 75 | NC 150/Trees/572. Subtree of Another Tree/solution.py
@@ -1,3 +1,21 @@
+'''
+    Explanation: Recursive solution with sameTree helper function
+        - If subRoot is empty, we know the empty node (null) is a subtree of root, so we can return True
+        - If root is empty, then the subRoot is not a subtree of an empty node, so we can return False
+        - If the root and subRoot are not empty, and if it's the same tree, then we return True
+        - Else, we recursively return the isSubtree function on either the left subtree and the subRoot or the right subtree and the subRoot
+
+        - Same tree helper function:
+            - If both trees are empty, resturn True
+            - If both trees are non-empty and the values are the same,
+                - Then we can recursively return the sameTree function on the left subtree and right subtree of both trees
+            - Else, return False
+
+        - TC: O(root + subRoot) => O(n)
+        - SC: O(log n) for completely balanced tree, O(n) for completely unbalanced tree)
+'''
+
+
 from typing import Optional
 
 # Definition for a binary tree node.
