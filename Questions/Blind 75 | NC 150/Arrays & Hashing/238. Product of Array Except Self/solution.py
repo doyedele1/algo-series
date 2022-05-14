a5e375c6e420faebd956d165ef363ed07a907db4
doyedele1 <demilade.oyedele@gmail.com>
2022-05-14 10:25:33 +0100

    solved-Blind-question

--- Questions/Blind 75 | NC 150/Arrays & Hashing/238. Product of Array Except Self/solution.py
+++ Questions/Blind 75 | NC 150/Arrays & Hashing/238. Product of Array Except Self/solution.py
@@ -1,1 +1,26 @@
-# def solution():
+'''
+    Explanation:
+        - 
+        
+        - TC: O(n)
+        - SC: O(1), the result array does not count as extra space
+
+'''
+
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        
+        L = 1
+        for i in range(len(nums)):
+            res[i] = L
+            L *= nums[i]
+            
+        R = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= R
+            R *= nums[i]
+        
+        return res
