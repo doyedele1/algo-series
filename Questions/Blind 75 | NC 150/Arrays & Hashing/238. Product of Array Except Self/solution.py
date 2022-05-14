eba8692c543ca818b54755b4150c1fb425797352
doyedele1 <demilade.oyedele@gmail.com>
2022-05-14 10:41:46 +0100

    solved-Blind-question

--- Questions/Blind 75 | NC 150/Arrays & Hashing/238. Product of Array Except Self/solution.py
+++ Questions/Blind 75 | NC 150/Arrays & Hashing/238. Product of Array Except Self/solution.py
@@ -1,26 +1,29 @@
 '''
     Explanation:
-        - 
-        
+        - [1, 2, 3, 4]
+        - We could use left and right arrays - left represents the product of elem to the left and right represents the product of elem to the right
+            - left = [], right = []
+            - After all iterations: left = [1, 1, 2, 6]
+            - After all iterations: right = [24, 12, 4, 1]
+            - res = left[i] * right[i] = [24, 12, 8, 6]
         - TC: O(n)
-        - SC: O(1), the result array does not count as extra space
-
+        - SC: O(n), space occupied by the left and right arrays
 '''
 
+from typing import List
 
-
-class Solution:
+class Solution1:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
+        left, right, res = [1] * len(nums), [1] * len(nums), [1] * len(nums)
         
-        L = 1
+        left[0] = 1
+        for i in range(1, len(nums)):
+            left[i] = nums[i - 1] * left[i - 1]
+        
+        right[len(nums) - 1] = 1
+        for i in range(len(nums) - 2, -1, -1):
+            right[i] = nums[i + 1] * right[i + 1]
+        
         for i in range(len(nums)):
-            res[i] = L
-            L *= nums[i]
-            
-        R = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= R
-            R *= nums[i]
-        
+            res[i] = left[i] * right[i]
         return res
