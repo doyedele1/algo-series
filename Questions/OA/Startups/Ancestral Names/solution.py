edf8413668c5ea2b6cba395c82842c297d05a14b
doyedele1 <demilade.oyedele@gmail.com>
2022-05-15 20:58:18 +0100

    solved-OA-question

--- Questions/OA/Startups/Ancestral Names/solution.py
+++ Questions/OA/Startups/Ancestral Names/solution.py
@@ -3,12 +3,9 @@ def romanToInt(s):
     dict = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
 
     for i in range(len(s)):
-        if i + 1 < len(s) and dict[s[i+1]] > dict[s[i]]:
-            res -= dict[s[i]]
-        
-        else:
-            res += dict[s[i]]
-            
+        if i + 1 < len(s) and dict[s[i+1]] > dict[s[i]]: res -= dict[s[i]]
+        else: res += dict[s[i]]
+
     return res
     
 def sort_roman(names):
