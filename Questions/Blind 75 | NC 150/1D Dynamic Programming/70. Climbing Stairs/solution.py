5811e25265b11275c6252c172eecd4ca02d5913d
doyedele1 <demilade.oyedele@gmail.com>
2022-05-17 15:29:38 +0100

    solved-Blind-question

--- Questions/Blind 75 | NC 150/1D Dynamic Programming/70. Climbing Stairs/solution.py
+++ Questions/Blind 75 | NC 150/1D Dynamic Programming/70. Climbing Stairs/solution.py
@@ -1,4 +1,10 @@
 '''
+                                0
+                1                       2
+        2               3           3           4
+    3       4       4       5    4       5   5       6
+4       5 5     6  5    6  6  7 5   6   6 7 6 7    7  8
+5
     Explanation I: Brute-force solution
         - We take all possible step combinations (1 and 2) at every step
         - climbStairs(i, n) = climbStairs(i + 1, n) + climbStairs(i + 2, n)
@@ -8,9 +14,9 @@
         - recursively call helper(i + 1, n) + helper(i + 2, n)   
         - TC: O(2^n), SC: O(n)
     
-    Explanation II: Recursion with Memoization
+    Explanation II: Recursion with Memoization/Caching
         - In the brute-force approach, we are redundantly calculating the result for every step
-        - Instead, we store the result at each step in memo array and return it when the function is called
+        - Instead, we store the result at each step in memo array and return it when the function is called - we are storing the result of each of the subproblem
         - This way, we are reducing the size of recursion tree up to n
         - TC: O(n), SC: O(n)
         
@@ -22,10 +28,16 @@
         - TC: O(n), SC: O(1)
     
     Explanation IV: Fibonacci Number Idea (Bottom-Up DP Approach)
-                            0
-                1                       2
-        2               3           3           4
-    3       4       4       5   4       5   5       6
+        - From the tree, let's start at the bottom, solve the base case and work our way up to 0
+            0   1   2   3   4   5 - We represent this on an histogram
+        dp =   [                1    1]. This contains the number of different ways we can get to the result n from the position above
+        dp =   [8   5   3   2    1    1]. For position 3, we can get to "5" 1 + 1 different ways.
+                                one   two
+        - The resulting array looks like a Fibonacci sequence
+        - We don't need an extra dp array which would cost O(n) space
+            - If we initialize one and two as 1, we can see that we only need to compute 4 other positions which is n - 1
+        
+        - TC: O(n), SC: O(1)
 '''
 
 class Solution1:
@@ -64,7 +76,7 @@ class Solution3:
 
 class Solution4:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
+        one = two = 1
         
         for i in range(n - 1):
             temp = one
