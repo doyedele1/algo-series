a1734930e328acf1aed1ac9eaf83d2876a9df8e4
doyedele1 <demilade.oyedele@gmail.com>
2022-05-20 23:07:40 +0100

    learning-OOP

---
+++ DSA Basics/OOP/app.py
@@ -0,0 +1,10 @@
+'''
+    Object-Oriented Programming in Python
+        x = "hello"
+        print(type(x)) # class str
+            x is the object of the class string
+
+        def hello():
+            print("hello")
+        print(type(hello)) # class function
+'''
