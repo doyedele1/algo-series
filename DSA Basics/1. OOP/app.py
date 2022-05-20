e1fef87970399c42e8b218bfba1491270334f881
doyedele1 <demilade.oyedele@gmail.com>
2022-05-20 23:31:59 +0100

    learning-OOP

--- DSA Basics/1. OOP/app.py
+++ DSA Basics/1. OOP/app.py
@@ -22,8 +22,10 @@
         - __init__ is a special method in Python that helps to instantiate the object right when it is created
 '''
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
+        # print(name)
 
     def add_one(self, x):
         return x + 1
@@ -31,7 +33,24 @@ class Dog:
     def bark(self):
         print("bark")
 
-d = Dog("Nymeria") # to understand what __init__ does
+    def get_name(self):
+        return self.name
+    
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+
+d = Dog("Nymeria", 12) # to understand what __init__ does
+print(d.name) # Nymeria
+print(d.get_age()) # 12
+d.set_age(10)
+print(d.get_age(10)) # age is now 10
+
+d2 = Dog("Summer", 5) # to understand what self does in __init__
+print(d2.name) # Summer
 d.bark() # output --> bark
 print(d.add_one(5)) # output --> 6
 print(type(d)) # output --> <class '__main__.Dog'>
