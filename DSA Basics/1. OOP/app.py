be3f3806a7bf781ac6d4111072b39ba8a776fee8
doyedele1 <demilade.oyedele@gmail.com>
2022-05-20 23:25:54 +0100

    learning-OOP

--- DSA Basics/1. OOP/app.py
+++ DSA Basics/1. OOP/app.py
@@ -18,11 +18,20 @@
     Creating our defined (blueprint) classes
         - A method is a function that goes inside a class
         - d is the instance of the class Dog. d is the object of type Dog
+
+        - __init__ is a special method in Python that helps to instantiate the object right when it is created
 '''
 class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def add_one(self, x):
+        return x + 1
+
     def bark(self):
         print("bark")
 
-d = Dog()
+d = Dog("Nymeria") # to understand what __init__ does
 d.bark() # output --> bark
+print(d.add_one(5)) # output --> 6
 print(type(d)) # output --> <class '__main__.Dog'>
