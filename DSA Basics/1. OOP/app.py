340fadcf6b64cd1486c776cb31e7ddd8a2795981
doyedele1 <demilade.oyedele@gmail.com>
2022-05-20 23:22:25 +0100

    learning-OOP

--- DSA Basics/1. OOP/app.py
+++ DSA Basics/1. OOP/app.py
@@ -1,15 +1,28 @@
 '''
-    Object-Oriented Programming in Python
+    Introduction to Object-Oriented Programming in Python
         x = "hello"
-        print(type(x)) # output --> class str
+        print(type(x)) # output --> <class str>
             x is the object of the class string
 
         def hello():
             print("hello")
-        print(type(hello)) # output --> class function
+        print(type(hello)) # output --> <class function>
 
         Methods
         string = "hello"
         print(string.upper())
             upper() is the method for class string
 '''
+
+'''
+    Creating our defined (blueprint) classes
+        - A method is a function that goes inside a class
+        - d is the instance of the class Dog. d is the object of type Dog
+'''
+class Dog:
+    def bark(self):
+        print("bark")
+
+d = Dog()
+d.bark() # output --> bark
+print(type(d)) # output --> <class '__main__.Dog'>
