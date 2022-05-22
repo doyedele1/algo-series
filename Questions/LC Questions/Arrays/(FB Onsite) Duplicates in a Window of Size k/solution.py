7594023b6b80fc22a3e6fb2a607a7637201588cc
doyedele1 <demilade.oyedele@gmail.com>
2022-05-22 23:40:22 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/(FB Onsite) Duplicates in a Window of Size k/solution.py
+++ Questions/LC Questions/Arrays/(FB Onsite) Duplicates in a Window of Size k/solution.py
@@ -1,3 +1,19 @@
+'''
+    Explanation:
+        - Create an empty hashmap
+        - Initialize a window_start pointer to 0
+
+        - Iterate through the array
+            - Get the window_size and create a hashmap of the array
+            - If the window_size is equal to k and the size of the hasmap is not the same as k, then return true
+            - Else, delete the number at the window_start from the hashmap and increment the window_start pointer
+            - Repeat the above steps until the end of the array is reached
+        - Return false
+
+        - TC: O(n)
+        - SC: O(n)
+'''
+
 import collections
 
 def solution(arr, k):
@@ -19,23 +35,5 @@ def solution(arr, k):
     
     return False
 
-
-'''
-    Explanation:
-        - Create an empty hashmap
-        - Initialize a window_start pointer to 0
-
-        - Iterate through the array
-            - Get the window_size and create a hashmap of the array
-            - If the window_size is equal to k and the size of the hasmap is not the same as k, then return true
-            - Else, delete the number at the window_start from the hashmap and increment the window_start pointer
-            - Repeat the above steps until the end of the array is reached
-        - Return false
-
-    Time complexity: O(n)
-    Space complexity: O(n)
-'''
-
-
-print(solution([3, 4, 1, 4, 5, 7, 2], 3))
+# print(solution([3, 4, 1, 4, 5, 7, 2], 3))
 # print(solution([3, 4, 1, 9, 5, 7, 2], 3))
