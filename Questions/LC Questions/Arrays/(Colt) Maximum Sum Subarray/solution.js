757b541a43e62356bab843044c6023fbc9e871ef
doyedele1 <demilade.oyedele@gmail.com>
2022-05-22 23:41:33 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/(Colt) Maximum Sum Subarray/solution.js
+++ Questions/LC Questions/Arrays/(Colt) Maximum Sum Subarray/solution.js
@@ -18,8 +18,8 @@ function maxSubarraySum(arr, n) {
 
 
 
-// console.log(maxSubarraySum([1,2,5,2,8,1,5],2)) // 10
-// console.log(maxSubarraySum([1,2,5,2,8,1,5],4)) // 17
-// console.log(maxSubarraySum([4,2,1,6],1)) // 6
-// console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
-// console.log(maxSubarraySum([],4)) // null
+// console.log(maxSubarraySum([1,2,5,2,8,1,5],2)) // returns 10
+// console.log(maxSubarraySum([1,2,5,2,8,1,5],4)) // returns 17
+// console.log(maxSubarraySum([4,2,1,6],1)) // returns 6
+// console.log(maxSubarraySum([4,2,1,6,2],4)) // returns 13
+// console.log(maxSubarraySum([],4)) // returns null
