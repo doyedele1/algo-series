e34bcec12cd421e25230b83934a3ef228633c6c9
doyedele1 <demilade.oyedele@gmail.com>
2022-05-22 23:42:51 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/(Codility) Tape Equilibrium/solution.py
+++ Questions/LC Questions/Arrays/(Codility) Tape Equilibrium/solution.py
@@ -1,3 +1,38 @@
+'''
+    First iteration: 
+        # first_sum = 3
+        # second_sum = 10
+        # diff = 13
+        # new_diff = 3 - 10 = 7, which is less than diff. Then diff = 7
+
+    Second iteration: 
+        # first_sum = 4
+        # second_sum = 9
+        # diff = 7
+        # new_diff = 4 - 9 = 5, which is less than diff. Then diff = 5
+
+    Third iteration
+        # first_sum = 6
+        # second_sum = 7
+        # diff = 5
+        # new_diff = 6 - 7 = 1 which is less than diff. Then diff = 1
+
+    Fourth iteration:
+        # first_sum = 10
+        # second_sum = 3
+        # diff = 1
+        # new_diff = 10 - 3 which is greater than diff. DO NOTHING!!!
+
+    Fifth iteration:
+        # first_sum = 13
+        # second_sum = 0
+        # diff = 1
+        # new_diff = 13 - 0 = 13 which is greater than diff. DO NOTHING!!!
+
+    TC: O(n-1), i.e. the number right before the end of the array, whose asymptotic notation is O(n) where n is the numbers on the tape
+    SC: O(1) because we are not making use of extra space to store data in our computer memory
+'''
+
 def solution(arr):
     first_sum = 0
     second_sum = 0
@@ -6,7 +41,6 @@ def solution(arr):
         second_sum += arr[i]
     
     diff = abs(first_sum - second_sum)
-    # print(diff)
 
     for part in range(len(arr) - 1):
         first_sum += arr[part]
@@ -18,39 +52,4 @@ def solution(arr):
 
     return diff
 
-    '''
-        First iteration: 
-            # first_sum = 3
-            # second_sum = 10
-            # diff = 13
-            # new_diff = 3 - 10 = 7, which is less than diff. Then diff = 7
-
-        Second iteration: 
-            # first_sum = 4
-            # second_sum = 9
-            # diff = 7
-            # new_diff = 4 - 9 = 5, which is less than diff. Then diff = 5
-
-        Third iteration
-            # first_sum = 6
-            # second_sum = 7
-            # diff = 5
-            # new_diff = 6 - 7 = 1 which is less than diff. Then diff = 1
-
-        Fourth iteration:
-            # first_sum = 10
-            # second_sum = 3
-            # diff = 1
-            # new_diff = 10 - 3 which is greater than diff. DO NOTHING!!!
-
-        Fifth iteration:
-            # first_sum = 13
-            # second_sum = 0
-            # diff = 1
-            # new_diff = 13 - 0 = 13 which is greater than diff. DO NOTHING!!!
-
-        # TC: O(N-1), i.e. the number right before the end of the array, whose asymptotic notation is O(N) where N is the numbers on the tape.
-        # SC: O(1) because we are not making use of extra space to store data in our computer memory
-    '''
-
-print(solution([3,1,2,4,3]))
+# print(solution([3,1,2,4,3]))
