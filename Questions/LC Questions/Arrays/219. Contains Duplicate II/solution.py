f89012e24c2b63fb9ea94b7bf8deaea774063b77
doyedele1 <demilade.oyedele@gmail.com>
2022-05-26 20:21:45 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/219. Contains Duplicate II/solution.py
+++ Questions/LC Questions/Arrays/219. Contains Duplicate II/solution.py
@@ -1,3 +1,15 @@
+'''
+    Explanation:
+        Iterate through the array
+            If element in the hash table, return true
+            Add element to the hash table
+            If the size of the hash table is greater than k, remove the oldest element from the hash table
+        Return false
+        
+        TC - O(n) for search, removal and insertion which with constant TC
+        SC - O(min(k, n)) where n is the size of the list. Only min(k, n) items are stored in the hash table
+'''
+
 from typing import List
 
 class Solution:
@@ -5,22 +17,9 @@ class Solution:
         seen = set()
         
         for i in range(len(nums)):
-            if nums[i] in seen:
-                return True
+            if nums[i] in seen: return True
             seen.add(nums[i])
-            if len(seen) > k:
-                seen.remove(nums[i-k])
+            if len(seen) > k: seen.remove(nums[i-k])
             # print(nums[i], seen)
                 
         return False
-        
-        '''
-            - Iterate through the array
-                - If element in the hash table, return true
-                - Add element to the hash table
-                - If the size of the hash table is greater than k, remove the oldest element from the hash table
-            - Return false
-            
-            TC - O(n) for search, removal and insertion which with constant TC
-            SC - O(min(k, n)) where n is the size of the list. Only min(k, n) items are stored in the hash table
-        '''
