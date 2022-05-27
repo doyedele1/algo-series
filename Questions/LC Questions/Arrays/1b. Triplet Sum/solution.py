6c3247e36c9c260dc8e9639ad424d1fcbac4af2c
doyedele1 <demilade.oyedele@gmail.com>
2022-05-28 00:15:59 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/1b. Triplet Sum/solution.py
+++ Questions/LC Questions/Arrays/1b. Triplet Sum/solution.py
@@ -1,5 +1,9 @@
 '''
     Explanation I: Brute-Force Solution
+        - Find every possible triplet combination of the numbers in the array using three nested loops, sum every combination and check if it equals to the target
+        
+        TC: O(n^3)
+        SC: O(1)
 
     Explanation II: Using sorting
 '''
@@ -8,21 +12,15 @@ from typing import List
 
 class Solution1:
     def tripletSum(nums, target) -> List[int]:
-    
-        # Fix the first element as nums[i]
+        res = []
         for i in range(0, len(nums) - 2):
-    
-            # Fix the second element as nums[j]
             for j in range(i + 1, len(nums) - 1):
-                
-                # Now look for the third number
                 for k in range(j + 1, len(nums)):
                     if nums[i] + nums[j] + nums[k] == target:
-                        print("Triplet is", nums[i], ", ", nums[j], ", ", nums[k])
-                        return True
+                        res.append((nums[i], nums[j], nums[k]))
+        return res
         
-        # If we reach here, then no triplet was found
-        return False
+print(Solution1.tripletSum([0, -1, 2, -3, 1], -2))
 
 class Solution2:
     def tripletSum(nums, target) -> List[int]:
@@ -39,4 +37,4 @@ class Solution2:
                 elif nums[j] + nums[k] < diff: j += 1
                 else: k -= 1
 
-print(Solution2.tripletSum([0, -1, 2, -3, 1], -2))
+# print(Solution2.tripletSum([0, -1, 2, -3, 1], -2))
