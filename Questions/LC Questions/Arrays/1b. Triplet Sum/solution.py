c38b8a7695137125e5fea67957b9d35a85b54b2b
doyedele1 <demilade.oyedele@gmail.com>
2022-05-28 01:15:04 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/1b. Triplet Sum/solution.py
+++ Questions/LC Questions/Arrays/1b. Triplet Sum/solution.py
@@ -5,7 +5,34 @@
         TC: O(n^3)
         SC: O(1)
 
-    Explanation II: Using sorting
+    Explanation II: Sorting Method
+        [12, 3, 4, 1, 6, 9]
+        - Sort the array --> [1, 3, 4, 6, 9, 12]
+        - Take the three pointers. i starts from the first element, j starts from the next element after i, k starts at the end of the array
+            [1, 3, 4, 6, 9, 12]
+            i   j           k
+            - Sum nums[j] + nums[k] except where j = k
+                - If sum is less than target, increment j
+                - If sum is greater than target, decrement k
+                - If sum is equal to target, print the triplet
+        - Move the i pointer
+
+        TC: O(n^2)
+        SC: O(1)
+    
+    Explanation III: Hashing Method
+        - Start pointer i from 0 to len(nums) - 2
+        - Create an empty hash map
+        - Traverse from j = i + 1 to len(nums) - 1
+        - Declare sum = arr[i] + arr[j]
+            - If target - sum is present in the hash map,
+                - Return arr[i], arr[j] and target - sum as the combination
+            - Else, insert arr[j] in the hash map and proceed
+        
+        [12, 3, 4, 1, 6, 9]
+        i    j
+        hashmap = {0: 3, 1: 4, 2: 1, 3: 6, 4: 9, 5: 12}
+
 '''
 
 from typing import List
@@ -20,21 +47,22 @@ class Solution1:
                         res.append((nums[i], nums[j], nums[k]))
         return res
         
-print(Solution1.tripletSum([0, -1, 2, -3, 1], -2))
+# print(Solution1.tripletSum([0, -1, 2, -3, 1], -2))
 
 class Solution2:
     def tripletSum(nums, target) -> List[int]:
         res = []
+        nums.sort()
 
-        for i in range(len(nums) - 2):
+        for i in range(0, len(nums) - 2):
             j = i + 1
             k = len(nums) - 1
-            diff = target - nums[i]
             while j < k:
-                if nums[j] + nums[k] == diff:
-                    res = print("Triplet is", nums[i], ", ", nums[j], ", ", nums[k])
-                    return res
-                elif nums[j] + nums[k] < diff: j += 1
+                if nums[i] + nums[j] + nums[k] == target:
+                    res.append((nums[i], nums[j], nums[k]))
+                    return res # not printing out all combinations
+                elif nums[i] + nums[j] + nums[k] < target: j += 1
                 else: k -= 1
+        return False
 
-# print(Solution2.tripletSum([0, -1, 2, -3, 1], -2))
+print(Solution2.tripletSum([0, -1, 2, -3, 1], -2))
