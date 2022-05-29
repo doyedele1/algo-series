75fe05297babfcccb62d7b9d63fc45203a294677
doyedele1 <demilade.oyedele@gmail.com>
2022-05-30 00:05:12 +0100

    solved-LC-question

--- Questions/LC Questions/Graphs/200. Number of Islands/solution.py
+++ Questions/LC Questions/Graphs/200. Number of Islands/solution.py
@@ -13,32 +13,29 @@
 
 class Solution1:
     def numIslands(self, grid):
-        if not grid:
-            return 0
-        
-        def dfs(grid, x, y):
-            if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] != "1":
-                return
+        def dfs(grid, row, col):            
+            grid[row][col] = "0"
             
-            grid[x][y] = "2"
-            
-            dfs(grid, x, y-1, r, c) # down
-            dfs(grid, x+1, y, r, c) # right
-            dfs(grid, x, y+1, r, c) # top
-            dfs(grid, x-1, y, r, c) # left
+            if row - 1 >= 0 and grid[row-1][col] == "1": dfs(grid, row - 1, col)
+            if row + 1 < len(grid) and grid[row+1][col] == "1": dfs(grid, row + 1, col)
+            if col - 1 >= 0 and grid[row][col-1] == "1": dfs(grid, row, col - 1)
+            if col + 1 < len(grid[0]) and grid[row][col+1] == "1": dfs(grid, row, col + 1)
         
+        if not grid: return 0
+
         rows, cols = len(grid), len(grid[0])
         islands = 0
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    dfs(grid, r, c)
                     islands += 1
+                    dfs(grid, r, c)
+                    
         return islands
 
 '''
-    Explanation II: BFS
+    Explanation III: BFS
         [
             [1,1,1,1],
             [1,1,1,1],
@@ -109,48 +106,44 @@ class Solution1:
 import collections
 from typing import List
 
-class Solution2:
+# Modifying input to a value of 0 for parent nodes and neighbors
+class Solution3:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid: return 0
 
-        rows = len(grid)
-        cols = len(grid[0])
-
+        rows, cols = len(grid), len(grid[0])
         islands = 0
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
                     neighbors = collections.deque([(r,c)])
-                    grid[r][c] = "2"
+                    grid[r][c] = "0"
                     
                     while neighbors:
                         row, col = neighbors.popleft()
                         if row - 1 >= 0 and grid[row-1][col] == "1":
                             neighbors.append((row-1, col))
-                            grid[row-1][col] = "2"
+                            grid[row-1][col] = "0"
                         if row + 1 < rows and grid[row+1][col] == "1":
                             neighbors.append((row+1, col))
-                            grid[row+1][col] = "2"
+                            grid[row+1][col] = "0"
                         if col - 1 >= 0 and grid[row][col-1] == "1":
                             neighbors.append((row, col-1))
-                            grid[row][col-1] = "2"
+                            grid[row][col-1] = "0"
                         if col + 1 < cols and grid[row][col+1] == "1":
                             neighbors.append((row, col+1))
-                            grid[row][col+1] = "2"
-                            
+                            grid[row][col+1] = "0"
                     islands += 1
                         
         return islands
 
-# Using hash set to keep track of visited nodes and neighbors
-class Solution3:
+# Using hash set to keep track of parent nodes and neighbors
+class Solution4:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid: return 0
 
-        rows = len(grid)
-        cols = len(grid[0])
-
+        rows, cols = len(grid), len(grid[0])
         islands = 0
         visited = set()
         
