9b20c0212bf8801f392276b389dfb1017840b2b2
doyedele1 <demilade.oyedele@gmail.com>
2022-05-30 00:37:45 +0100

    solved-LC-question

--- Questions/LC Questions/Graphs/200. Number of Islands/solution.py
+++ Questions/LC Questions/Graphs/200. Number of Islands/solution.py
@@ -1,16 +1,10 @@
 '''
-    Explanation I: DFS
-        [
-            [1,1,1,1],
-            [1,1,1,1],
-            [1,1,1,1],
-            [1,1,1,1]
-        ]
-        
-        TC: O(rows*cols)
-        SC: O(rows*cols), recursive call stack calls all the 1s
+    DFS
+        TC: O(rows * cols)
+        SC: O(rows * cols), recursive call stack calls all the 1s in the worst case
 '''
 
+# Modifying input to a value of 0 for parent nodes and neighbors
 class Solution1:
     def numIslands(self, grid):
         def dfs(grid, row, col):            
@@ -30,12 +24,44 @@ class Solution1:
             for c in range(cols):
                 if grid[r][c] == "1":
                     islands += 1
+                    dfs(grid, r, c)    
+        return islands
+
+# Using hash set to keep track of parent nodes and neighbors
+class Solution2:
+    def numIslands(self, grid):
+        visited = set()
+        
+        def dfs(grid, row, col):
+            visited.add((row,col))
+            
+            if row - 1 >= 0 and grid[row-1][col] == "1" and (row - 1, col) not in visited: 
+                dfs(grid, row - 1, col)
+                visited.add((row - 1, col))
+            if row + 1 < len(grid) and grid[row+1][col] == "1" and (row + 1, col) not in visited:
+                dfs(grid, row + 1, col)
+                visited.add((row + 1, col))
+            if col - 1 >= 0 and grid[row][col-1] == "1" and (row, col - 1) not in visited:
+                dfs(grid, row, col - 1)
+                visited.add((row, col - 1))
+            if col + 1 < len(grid[0]) and grid[row][col+1] == "1" and (row, col + 1) not in visited:
+                dfs(grid, row, col + 1)
+                visited.add((row, col + 1))
+            
+        if not grid: return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    islands += 1
                     dfs(grid, r, c)
-                    
         return islands
 
 '''
-    Explanation III: BFS
+    Explanation: BFS
         [
             [1,1,1,1],
             [1,1,1,1],
