fdc14722ceacbf542a6406ac51b444b99fb0a08a
doyedele1 <demilade.oyedele@gmail.com>
2022-05-31 23:13:52 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/(OA) Group Totals/solution.js
+++ Questions/LC Questions/Strings/(OA) Group Totals/solution.js
@@ -1,26 +1,26 @@
 function groupTotals(strArr) {
 
-    const map = new Map()
+    const map = new Map();
 
     for(const elem of strArr) {
-        let key = elem.slice(0, 1)
-        let value = elem.slice(2)
+        let key = elem.slice(0, 1);
+        let value = elem.slice(2);
         
-        if(!map.has(key)) {
-            map.set(key, 0)
+        if (!map.has(key)) {
+            map.set(key, 0);
         }
-        map.set(key, +map.get(key) + +value)
+        map.set(key, +map.get(key) + +value);
     }
 
-    let answer = ""
+    let answer = "";
     answer = [...map].sort().reduce((acc, elem) => {
-        if(elem[1] != 0) {
-            acc = acc + elem[0] + ":" + elem[1] + ","
-            return acc
+        if (elem[1] != 0) {
+            acc = acc + elem[0] + ":" + elem[1] + ",";
+            return acc;
         }
-        return acc
-    }, "").slice(0, -1)
-    return answer
+        return acc;
+    }, "").slice(0, -1);
+    return answer;
 }
 
-groupTotals(["X:-1", "Y:1", "X:-4", "B:3", "X:5"])
+// console.log(groupTotals(["X:-1", "Y:1", "X:-4", "B:3", "X:5"]));
