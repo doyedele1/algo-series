b588b72cc8261e85920c10b1ffb63993af159b74
doyedele1 <demilade.oyedele@gmail.com>
2022-06-01 04:04:58 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/9. Palindrome Number/solution.py
+++ Questions/LC Questions/Strings/9. Palindrome Number/solution.py
@@ -1,3 +1,28 @@
+'''
+    Explanation III:
+        1221
+        - Reverse the last half of the input number. i.e. from 21 to 12
+            - First iteration:
+                res = 0, res = 1, x = 122
+            - Second iteration:
+                res = 1, res = 12, x = 12
+        - We reverse the half and stop the loop when the original number x is less than or equal to reversed number res
+        - When the length of the number is odd, we get rid of the middle digit by doing res // 10
+            12321
+                - First iteration:
+                    res = 0, res = 1, x = 1232
+                - Second iteration:
+                    res = 1, res = 12, x = 123
+                - Third iteration:
+                    res = 12, res = 123, x = 12
+                - We stop the loop since x < 123. We can then ignore the middle digit "3", by doing 123 // 10 = 12 and it's equal to x = 12
+        - When the length of the number is even, we can always get x == res
+        
+        TC: O(log n) since we divide the input number by 10 for every iteration
+        SC: O(1)
+'''
+
+
 class Solution1:
     def isPalindrome(self, x: int) -> bool:
         s = str(x)
@@ -10,6 +35,7 @@ class Solution1:
 
 class Solution2:
     def isPalindrome(self, x: int) -> bool:
+        # edge cases - negative, x is not equal to 0 and x ends with 0
         if x < 0 or (x != 0 and x % 10 == 0):
             return False
         
@@ -27,3 +53,14 @@ class Solution2:
 
 class Solution3:
     def isPalindrome(self, x: int) -> bool:
+        # edge cases - negative, x is not equal to 0 and x ends with 0
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        res = 0
+
+        while x > res:
+            res = res * 10 + x % 10
+            x //= 10
+            
+        return x == res or x == res // 10
