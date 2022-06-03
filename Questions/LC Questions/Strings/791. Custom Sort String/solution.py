9fa37551abc3948a35bc702d3c4241d83990c807
doyedele1 <demilade.oyedele@gmail.com>
2022-06-03 23:32:05 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/791. Custom Sort String/solution.py
+++ Questions/LC Questions/Strings/791. Custom Sort String/solution.py
@@ -1,6 +1,20 @@
+'''
+    Explanation I:
+        - We can use a lookup dict to match these characters to integers
+        - Each time we see a new char, we increase the iterator and sort using the lambda function
+
+    Explanation II:
+        - Count the elemtns of T
+        - If we have some count[char] = number of occurences of char in T, we can return these elements in any order
+        - The order is S + (characters not in S in any order)
+        
+        TC - O(len(S) + len(T))
+        SC - O(len(T)
+'''
+
 import collections
 
-class Solution:
+class Solution1:
     def customSortString(self, order: str, s: str) -> str:
         lookup = collections.defaultdict(int)
         i = 0
@@ -11,15 +25,8 @@ class Solution:
             
         return "".join(sorted(s, key=lambda x: lookup[x]))
 
-        '''
-            Explanation:
-                - We can use a lookup dict to match these characters to integers
-                - Each time we see a new char, we increase the iterator and sort using the lambda function
-        '''
-        
-
 # COUNT AND WRITE        
-class Solution(object):
+class Solution2(object):
     def customSortString(self, S, T):
         # count[char] will be the number of occurrences of
         # 'char' in T.
@@ -29,24 +36,11 @@ class Solution(object):
         # Write all characters that occur in S, in the order of S.
         for c in S:
             ans.append(c * count[c])
-            # Set count[c] = 0 to denote that we do not need
-            # to write 'c' to our answer anymore.
+            # Set count[c] = 0 to denote that we do not need to write 'c' to our answer anymore.
             count[c] = 0
 
-        # Write all remaining characters that don't occur in S.
-        # That information is specified by 'count'.
+        # Write all remaining characters that don't occur in S. That information is specified by 'count'.
         for c in count:
             ans.append(c * count[c])
 
         return "".join(ans)
-
-
-        '''
-            Explanation:
-                - Count the elemtns of T
-                - If we have some count[char] = number of occurences of char in T, we can return these elements in any order
-                - The order is S + (characters not in S in any order)
-                
-            TC - O(len(S) + len(T))
-            SC - O(len(T)
-        '''
