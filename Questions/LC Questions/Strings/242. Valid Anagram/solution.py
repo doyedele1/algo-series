b5a663705ab5ee236723b5cfd67ebaa3667ff947
doyedele1 <demilade.oyedele@gmail.com>
2022-06-03 23:25:05 +0100

    fixing-errors

--- Questions/LC Questions/Strings/242. Valid Anagram/solution.py
+++ Questions/LC Questions/Strings/242. Valid Anagram/solution.py
@@ -16,8 +16,8 @@ class Solution2:
         dictS, dictT = {}, {}
         
         for i in range(len(s)):
-            dictS[s[i]] = 1 + dictS.get(s[i], 0) # if s[i] doesn't exist, we return value to be 0
-            dictT[t[i]] = 1 + dictT.get(t[i], 0) # if s[i] doesn't exist, we return value to be 0
+            dictS[s[i]] = 1 + dictS.get(s[i], 0) # If s[i] doesn't exist, we return value to be 0
+            dictT[t[i]] = 1 + dictT.get(t[i], 0) # If t[i] doesn't exist, we return value to be 0
         
         for char in dictS:
             if dictS[char] != dictT.get(char, 0):
--- Questions/LC Questions/Strings/249. Group Shifted Strings/solution.py
+++ Questions/LC Questions/Strings/249. Group Shifted Strings/solution.py
@@ -1,3 +1,22 @@
+'''
+    Explanation:
+        - Two things to understand
+            1. Between characters, there is a difference of one
+            2. There can be rotation in the alphabets (as seen in the case of "zab")
+
+        - We will form a dictionary to help with the grouping.
+            - Key will be the difference between characters and value will be the strings with the distance same as the difference
+                - For distance = 1 going in the positive direction, key = "11"
+                - For distance = 1 going in the reversed direction, key = "-1-1"
+            - Concept of rotation (abc), c + 2
+                - (Length of string + index of c + how much are we moving) % length of string
+                - (3 + 2 + 2) % 3 = 1. Index 1 which is b
+
+
+        TC - O(n * k) where n is the length of the input strings and k is the maximum length of a string in strings
+        SC - O(n * k). Maximum number of strings stored in the dictionary and each string takes at most O(k) space
+'''
+
 from collections import defaultdict
 from typing import List
 
@@ -12,23 +31,3 @@ class Solution:
                 key += str(((ord(string[ch]) - ord(string[ch-1])) + ord('a')) % 26)
             result[key].append(string)
         return result.values()
-        
-        
-        '''
-            Explanation:
-                - Two things to understand
-                    1. Between characters, there is a difference of one
-                    2. There can be rotation in the alphabets (as seen in the case of "zab")
-
-                - We will form a dictionary to help with the grouping.
-                    - Key will be the difference between characters and value will be the strings with the distance same as the difference
-                        - For distance = 1 going in the positive direction, key = "11"
-                        - For distance = 1 going in the reversed direction, key = "-1-1"
-                    - Concept of rotation (abc), c + 2
-                        - (Length of string + index of c + how much are we moving) % length of string
-                        - (3 + 2 + 2) % 3 = 1. Index 1 which is b
-
-
-                TC - O(n * k) where n is the length of the input strings and k is the maximum length of a string in strings
-                SC - O(n * k). Maximum number of strings stored in the dictionary and each string takes at most O(k) space
-        '''
