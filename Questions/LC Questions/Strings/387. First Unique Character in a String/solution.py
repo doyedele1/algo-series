a7e0cbf0ad5d6b8bef5199dc63321f68a2ad776e
doyedele1 <demilade.oyedele@gmail.com>
2022-06-03 23:26:29 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/387. First Unique Character in a String/solution.py
+++ Questions/LC Questions/Strings/387. First Unique Character in a String/solution.py
@@ -1,13 +1,13 @@
-# TC - O(n), SC - O(1)
+'''
+    TC - O(n), SC - O(1)
+'''
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         dict = {}
         
         # We could use collections.Counter to create the hashmap
-        # dict = collections.Counter(s)
-        
-        # or use object in Python
+        # dict = collections.Counter(s) or use object in Python
         for char in s:
             if char in dict:
                 dict[char] += 1
