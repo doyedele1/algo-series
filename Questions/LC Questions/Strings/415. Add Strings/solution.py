4bcd3e0a4ab33002cbbdd9d49c6237d170c6cfc7
doyedele1 <demilade.oyedele@gmail.com>
2022-06-03 23:27:53 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/415. Add Strings/solution.py
+++ Questions/LC Questions/Strings/415. Add Strings/solution.py
@@ -1,9 +1,11 @@
-class Solution:
+# Brute-force solution
+class Solution1:
     def addStrings(self, num1: str, num2: str) -> str:
-        # Brute-force solution
         return str(int(num1) + int(num2))
-    
-        # Another solution - TC - O(max(n,m)), SC - O(max(n,m))
+
+# Another solution - TC - O(max(n,m)), SC - O(max(n,m))
+class Solution2:
+    def addStrings(self, num1: str, num2: str) -> str:
         m, n = len(num1) - 1, len(num2) - 1
         carry = 0
         res = ""
@@ -11,7 +13,7 @@ class Solution:
         while m >=0 or n >= 0:
             i, j = 0, 0
             if m >= 0:
-                i = ord(nums1[m]) - ord('0') # 0 --> 48
+                i = ord(num1[m]) - ord('0') # 0 --> 48
                 m -= 1
             if n >= 0:
                 j = ord(num2[n]) - ord('0')
