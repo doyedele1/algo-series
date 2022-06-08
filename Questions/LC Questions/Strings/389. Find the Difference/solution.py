6cd8479f92459d3a2b4528bdf5d6d75914c4e178
doyedele1 <demilade.oyedele@gmail.com>
2022-06-08 23:59:48 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/389. Find the Difference/solution.py
+++ Questions/LC Questions/Strings/389. Find the Difference/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def findTheDifference(s, t):
         xor = 0
-        for char in s: xor ^= ord(c)
-        for char in t: xor ^= ord(c)          
+        for char in s: xor ^= ord(char)
+        for char in t: xor ^= ord(char)          
         return chr(xor)
 
 # print(Solution.findTheDifference('foobar', 'barfoot'))
