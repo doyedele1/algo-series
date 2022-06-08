d5634f883483aa93b52cfe9f56e815c7e7dd7945
doyedele1 <demilade.oyedele@gmail.com>
2022-06-08 23:21:33 +0100

    solved-Amazon-OA

--- Questions/LC Questions/Strings/389. Find the Difference/solution.py
+++ Questions/LC Questions/Strings/389. Find the Difference/solution.py
@@ -1,6 +1,10 @@
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
+    def findTheDifference(s, t):
         xor = 0
         for c in s: xor ^= ord(c)
-        for c in t: xor ^= ord(c)            
+        print(xor)
+        for c in t: xor ^= ord(c)
+        print(xor)           
         return chr(xor)
+
+print(Solution.findTheDifference('foobar', 'barfoot'))
