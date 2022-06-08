5c705ef3385aa75a9b7613049e88a32bb9244958
doyedele1 <demilade.oyedele@gmail.com>
2022-06-09 00:06:30 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/389. Find the Difference/solution.py
+++ Questions/LC Questions/Strings/389. Find the Difference/solution.py
@@ -1,8 +1,15 @@
 '''
     Explanation:
+        - If we do xor of 0101 and 0101, we get 0 because they cancel each other out
+
+        - For string "a", 
+            -The ASCII equivalent is 97. 0 xor 97 = 97
+        - For string "ab", (we do the xor character wise and add to the xor variable)
+            - The ASCII equivalent of the first character is 97. 97 xor 97 = 0.
+            - The second character is 98. 0 xor 98 = 98 which is our difference.
+
         - TC: O(n)
         - SC: O(1)
-
 '''
 
 class Solution:
