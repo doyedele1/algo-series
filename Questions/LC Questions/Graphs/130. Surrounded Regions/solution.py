c3165ba80c8b7ced092a501271d6730d5a56a838
doyedele1 <demilade.oyedele@gmail.com>
2022-06-12 00:09:07 +0100

    solved-LC-question

--- Questions/LC Questions/Graphs/130. Surrounded Regions/solution.py
+++ Questions/LC Questions/Graphs/130. Surrounded Regions/solution.py
@@ -1,5 +1,7 @@
 '''
     Explanation I: 
+        - To capture the surrounded regions, can we capture everything except the unsurrounded regions?
+            - To know unsurrounded regions, we know the region is connected to the border
 
 '''
 
@@ -16,7 +18,7 @@ class Solution:
             if r < 0 or c < 0 or r == rows or c == cols or board[r][c] != 'O':
                 return
             
-            board[r][c] = 'E'
+            board[r][c] = 'U'
             dfs(r + 1, c)
             dfs(r - 1, c)
             dfs(r, c + 1)
@@ -24,6 +26,7 @@ class Solution:
             
         for row in range(rows):
             for col in range(cols):
+                # run a dfs on every unsurrounded region. i.e every region connected to the border
                 if board[row][col] == 'O' and (row in [0, rows - 1] or col in [0, cols - 1]):
                     dfs(row, col)
         
@@ -32,5 +35,5 @@ class Solution:
                 if board[row][col] == 'O':
                     board[row][col] = 'X'
                     
-                if board[row][col] == 'E':
+                if board[row][col] == 'U':
                     board[row][col] = 'O'
