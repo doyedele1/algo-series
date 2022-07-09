1b7f30e72754afb3dde74f3733122f776b4e7d11
doyedele1 <demilade.oyedele@gmail.com>
2022-07-09 16:32:08 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/686. Repeated String Match/solution.py
+++ Questions/LC Questions/Strings/686. Repeated String Match/solution.py
@@ -1,13 +1,22 @@
 from math import ceil
 
 class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:
+    # def repeatedStringMatch(self, a: str, b: str) -> int:
         
-        maxTimes = ceil(len(b)/len(a))
-        tempA = a
+    #     maxTimes = ceil(len(b)/len(a))
+    #     tempA = a
         
-        for times in range(1, maxTimes + 2):
-            if b in tempA: return times
-            tempA += a
+    #     for times in range(1, maxTimes + 2):
+    #         if b in tempA: return times
+    #         tempA += a
         
-        return -1
+    #     return -1
+
+    def kmp(a, b):
+            for i in range(len(a) - len(b) + 1):
+                for j in range(len(b)):
+                    if a[i+j] != b[j]: break
+                if j == len(b): return True
+            return False
+        
+print(Solution.kmp("abcdabcdabcd","cdabcdab"))
