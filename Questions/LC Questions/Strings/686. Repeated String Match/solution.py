73debd4b98c1b3da1f373ab0b4b5283579ab92b6
doyedele1 <demilade.oyedele@gmail.com>
2022-07-09 16:49:42 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/686. Repeated String Match/solution.py
+++ Questions/LC Questions/Strings/686. Repeated String Match/solution.py
@@ -1,22 +1,35 @@
 from math import ceil
 
-class Solution:
-    # def repeatedStringMatch(self, a: str, b: str) -> int:
+class Solution1:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
         
-    #     maxTimes = ceil(len(b)/len(a))
-    #     tempA = a
+        maxTimes = ceil(len(b)/len(a))
+        tempA = a
         
-    #     for times in range(1, maxTimes + 2):
-    #         if b in tempA: return times
-    #         tempA += a
+        for times in range(1, maxTimes + 2):
+            if b in tempA: return times
+            tempA += a
         
-    #     return -1
+        return -1
 
-    def kmp(a, b):
-            for i in range(len(a) - len(b) + 1):
-                for j in range(len(b)):
-                    if a[i+j] != b[j]: break
-                if j == len(b): return True
-            return False
+class Solution2:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        def kmp(a, b):
+                for i in range(len(a) - len(b) + 1):
+                    for j in range(len(b)):
+                        if a[i+j] != b[j]: break
+                    if j == len(b): return True
+                return False
         
-print(Solution.kmp("abcdabcdabcd","cdabcdab"))
+        res = 1
+        tempA = a
+        
+        while len(a) < len(b):
+            a += tempA
+            res += 1
+            
+        if kmp(a, b): return res
+        if kmp(a + tempA, b): return res + 1
+        return -1
+        
+# print(Solution.kmp("abcdabcdabcd","cdabcdab"))
