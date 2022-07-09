c0d70497912d5de2c1a50446079622028ee2e2b0
doyedele1 <demilade.oyedele@gmail.com>
2022-07-09 23:33:40 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/494. Target Sum/solution.py
+++ Questions/LC Questions/Arrays/494. Target Sum/solution.py
@@ -1,3 +1,17 @@
+'''
+    Explanation:
+        - We use a cache that contains (index, sum) at every node of the tree?
+                                            (0, 0)
+                            (1, 1)                            (1, -1)
+                    (2, 2)          (2, 0)
+                               (3, 1)
+                          (4, 2)
+                      (5, 3)
+        - (index, sum). Maximum value for index is len(nums) = n, Possible range for sum of values in nums is -5 to 5 = t. 
+        - TC: O(n*t)
+        - SC: O(n*t)
+'''
+
 from typing import List
 
 class Solution:
@@ -5,12 +19,15 @@ class Solution:
         cache = {}
         
         def helper(index, sum):
-            if index == len(nums):
-                return 1 if sum == target else 0
+            # base case
+            if index == len(nums): return 1 if sum == target else 0
             
+            # base case - if the pair (index, sum) already exists
             if (index, sum) in cache: return cache[(index, sum)]
             
-            cache[(index, sum)] = (helper(index + 1, sum + nums[index]) + helper(index + 1, sum - nums[index]))
+            add = sum + nums[index]
+            subtract = sum - nums[index]
+            cache[(index, sum)] = (helper(index + 1, add) + helper(index + 1, subtract))
             
             return cache[(index, sum)]
         
