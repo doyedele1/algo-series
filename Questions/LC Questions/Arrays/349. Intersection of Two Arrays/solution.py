3ebcb3e8319982cd616ac18627cf942ac8201a33
doyedele1 <demilade.oyedele@gmail.com>
2022-07-12 18:39:14 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
+++ Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
@@ -1,16 +1,30 @@
+'''
+    Explanation:
+        - Clarifying questions to ask: duplicates, negative values, single value lists, 0s, empty lists
+            nums1 = [1,2,2,1], nums2 = [2,2]
+            
+            nums1 = set1 = (1,2)
+            nums2 = set2 = (2)
+        - We then iterate over the smallest set which is nums2. For every nums in set2, if we find them in set1, we add to the result array
+        
+        - TC: O(m + n) where m is the length of nums1 and n is the length of nums2. 
+            - O(m) to convert nums1 to set1
+            - O(n) to convert nums2 to set2
+            - O(1) to look up the set
+        - SC: O(m + n) worst case is when all elements in the arrays are different
+'''
+
+
+from typing import List
+
+
 class Solution:
-    def intersection(self, nums1, nums2):
-        seen = {}
-        res = []
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        def helper(set1, set2):
+            return [nums for nums in set1 if nums in set2]
         
-        for num in nums1:
-            if num in seen: seen[num] += 1
-            else: seen[num] = 1
-        # print(seen)
+        set1, set2 = set(nums1), set(nums2)
         
-        for num in nums2:
-            if num in seen and seen[num] > 0:
-                res.append(num)
-                seen[num] = 0
-    
-        return res
+        if len(set1) < len(set2): return helper(set1, set2)
+        else: return helper(set2, set1)
+                
