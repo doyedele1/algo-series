3478ef7af42bad8c481c71f6663cd9b5bc1f754b
doyedele1 <demilade.oyedele@gmail.com>
2022-07-12 21:42:31 +0100

    solved-LC-question

---
+++ Questions/LC Questions/Arrays/1413. Minimum Value to Get Positive Step by Step Sum/solution.py
@@ -0,0 +1,22 @@
+'''
+    Explanation: Prefix Total
+        [-3, 2, -3, 4, 2]
+        sumNums = 0,  res = 0
+        sumNums = -3, res = -3
+        sumNums = -1, res = -3
+        sumNums = -4, res = -4
+        sumNums = 0, res = -4
+        sumNums = 2, res = -4
+        res = 1 - (-4) = 5
+'''
+
+from typing import List
+
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        sumNums, res = 0, 0
+        for num in nums:
+            sumNums += num
+            res = min(res, sumNums)
+            
+        return 1 - res
