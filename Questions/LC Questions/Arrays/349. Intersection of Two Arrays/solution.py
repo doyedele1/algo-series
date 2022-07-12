d70bbabef89152f904500993226ea42ec43ee3e8
doyedele1 <demilade.oyedele@gmail.com>
2022-07-12 18:49:19 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
+++ Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
@@ -1,10 +1,10 @@
 '''
-    Explanation:
+    Explanation I: Converting arrays to sets
         - Clarifying questions to ask: duplicates, negative values, single value lists, 0s, empty lists
-            nums1 = [1,2,2,1], nums2 = [2,2]
-            
-            nums1 = set1 = (1,2)
-            nums2 = set2 = (2)
+        nums1 = [1,2,2,1], nums2 = [2,2]
+        
+        nums1 = set1 = (1,2)
+        nums2 = set2 = (2)
         - We then iterate over the smallest set which is nums2. For every nums in set2, if we find them in set1, we add to the result array
         
         - TC: O(m + n) where m is the length of nums1 and n is the length of nums2. 
@@ -12,13 +12,27 @@
             - O(n) to convert nums2 to set2
             - O(1) to look up the set
         - SC: O(m + n) worst case is when all elements in the arrays are different
+        
+    Explanation II: Sorting and Two Pointers
+        nums1 = [1,2,2,1], nums2 = [2,2]
+        nums1 = [1, 1, 2, 2]
+        nums2 = [2, 2]
+        
+        - i = 0, j = 0, move i
+        - i = 1, j = 0, move i
+        - i = 2, j = 0, add the number 2 to res array and move both i and j
+        - i = 3, j = 1, add the number 2 to the res array again
+        
+        - return set(res)
+        
+        - TC: O(n logn)
+        - SC: O(1)
 '''
-
 
 from typing import List
 
 
-class Solution:
+class Solution1:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         def helper(set1, set2):
             return [nums for nums in set1 if nums in set2]
@@ -28,3 +42,19 @@ class Solution:
         if len(set1) < len(set2): return helper(set1, set2)
         else: return helper(set2, set1)
                 
+class Solution2:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        res = []
+        i, j = 0, 0
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]: j += 1
+            elif nums1[i] < nums2[j]: i += 1
+            else:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+                
+        return set(res)             
