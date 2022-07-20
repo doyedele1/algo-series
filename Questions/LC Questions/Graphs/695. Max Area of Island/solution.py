3da181f6fa3b63203a239790d068144f7ae21fcb
doyedele1 <demilade.oyedele@gmail.com>
2022-07-20 15:48:06 +0100

    solved-LC-question

--- Questions/LC Questions/Graphs/695. Max Area of Island/solution.py
+++ Questions/LC Questions/Graphs/695. Max Area of Island/solution.py
@@ -3,17 +3,17 @@ from typing import List
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         def dfs(r, c):
-            if not (0 <= r < len(grid) and 0 <= c < len(grid[0]) and (r, c) not in visited and grid[r][c] == 1):
+            if not (r >= 0 and r < len(grid) and c >= 0 and c < len(grid[0]) and (r, c) not in visited and grid[r][c] == 1):
                 return 0
+            
             visited.add((r, c))
             return (1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1))
         
         visited = set()
-        max_area = 0
+        res = 0
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1 and (r, c) not in visited:
-                    max_area = max(max_area, dfs(r, c))
-                    
-        return max_area
+                    res = max(res, dfs(r, c))
+        return res
