dbee23939c82342d1bd3af6578ad390356198f5b
doyedele1 <demilade.oyedele@gmail.com>
2022-07-21 11:54:42 +0100

    solved-LC-question

--- Questions/LC Questions/Math/202. Happy Number/solution.py
+++ Questions/LC Questions/Math/202. Happy Number/solution.py
@@ -7,11 +7,15 @@
         - n = 2 --> 4 --> 16 --> 37 --> 58 --> 89 --> 145 --> 42 --> 20 --> 4
         Since we've seen 4 before, then we know that the number is not happy.
         
-        TC: O(log n) - finding the sum square for a given number has a cost of O(logn) because we are processing each digit in the number
-        SC: O(log n) - depends on what numbers are in the hash set. For n > 243, the most space will be taken by n itself
+        - TC: O(log n) - finding the sum square for a given number has a cost of O(logn) because we are processing each digit in the number
+        - SC: O(log n) - depends on what numbers are in the hash set. For n > 243, the most space will be taken by n itself
+
+    Explanation I: Using linked list cycle idea
+        - TC: O(log n)
+        - SC: O(1)
 '''
 
-class Solution:
+class Solution1:
     def isHappy(self, n: int) -> bool:
         def sumOfSquares(n):
             res = 0
@@ -28,3 +32,22 @@ class Solution:
             n = sumOfSquares(n)
             
         return n == 1
+
+class Solution2:
+    def isHappy(self, n: int) -> bool:
+        def sumOfSquares(n):
+            res = 0
+            while n > 0:
+                digit = n % 10
+                res += digit ** 2
+                n //= 10
+            return res
+        
+        slow = n
+        fast = sumOfSquares(n)
+        
+        while fast != 1 and slow != fast:
+            slow = sumOfSquares(slow)
+            fast = sumOfSquares(sumOfSquares(fast))
+            
+        return fast == 1
