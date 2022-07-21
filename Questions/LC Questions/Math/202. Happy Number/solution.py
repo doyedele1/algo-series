4e4da189b6c0af9665b786b0e39bb1ab37e349ce
doyedele1 <demilade.oyedele@gmail.com>
2022-07-21 11:44:54 +0100

    solved-LC-question

--- Questions/LC Questions/Math/202. Happy Number/solution.py
+++ Questions/LC Questions/Math/202. Happy Number/solution.py
@@ -7,23 +7,24 @@
         - n = 2 --> 4 --> 16 --> 37 --> 58 --> 89 --> 145 --> 42 --> 20 --> 4
         Since we've seen 4 before, then we know that the number is not happy.
         
-        Time Complexity: O(log n) - finding the sum square for a given number has a cost of O(logn) because we are processing each digit in the number, and the number of digits in a number is given by (log n): For n > 243, O(243 * 3 + log n + log log n + ....) = O(log n)
-        Space: O(log n) - depends on what numbers are in the hash set. For n > 243, the most space will be taken by n itself
+        TC: O(log n) - finding the sum square for a given number has a cost of O(logn) because we are processing each digit in the number
+        SC: O(log n) - depends on what numbers are in the hash set. For n > 243, the most space will be taken by n itself
 '''
 
 class Solution:
     def isHappy(self, n: int) -> bool:
-        checkCycle = set()
+        def sumOfSquares(n):
+            res = 0
+            while n > 0:
+                digit = n % 10
+                res += digit ** 2
+                n //= 10
+            return res
         
-        while self.squareSum(n) not in checkCycle and n != 1:
-            checkCycle.add(n)
-            n = self.squareSum(n)
+        seen = set()
+        
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sumOfSquares(n)
+            
         return n == 1
-        
-    def squareSum(self, n):
-        resultNumber = 0
-        while n > 0:
-            lastDigit = n % 10
-            resultNumber = resultNumber + (lastDigit ** 2)
-            n //= 10
-        return resultNumber
