4091a0db9638dae5e5eb111faee608cd75d0001b
doyedele1 <demilade.oyedele@gmail.com>
2022-07-22 21:17:41 +0100

    solved-LC-question

---
+++ Questions/OA/GS/Is Possible/solution.py
@@ -0,0 +1,19 @@
+def isPossible(a, b, c, d):
+        while c >= a and d >= b:
+            if c == d:
+                break
+            elif c > d:
+                if d > b:
+                    c %= d
+                else:
+                    if (c - a) % d == 0: return "Yes"
+                    else: return "No"
+            else:
+                if c > a:
+                    d %= c
+                else:
+                    if (d - b) % c == 0: return "Yes"
+                    else: return "No"
+
+        if c == a and d == b: return "Yes"
+        else: return "No"
