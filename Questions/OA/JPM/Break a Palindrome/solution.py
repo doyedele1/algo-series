7cbbe008c650599100bf6a92c4250243a0b1022d
doyedele1 <demilade.oyedele@gmail.com>
2022-07-24 15:33:22 +0100

    solved-OA-questions

--- Questions/OA/JPM/Break a Palindrome/solution.py
+++ Questions/OA/JPM/Break a Palindrome/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        
         if len(palindrome) == 1: return ""
         
         for i in range(len(palindrome) // 2):
