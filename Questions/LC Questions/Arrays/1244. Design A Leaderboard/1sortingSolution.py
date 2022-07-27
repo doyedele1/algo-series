5d1f4e7a049e38015bbf3b68c95d9f1c7c0dccdb
doyedele1 <demilade.oyedele@gmail.com>
2022-07-27 09:09:36 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/1244. Design A Leaderboard/1sortingSolution.py
+++ Questions/LC Questions/Arrays/1244. Design A Leaderboard/1sortingSolution.py
@@ -1,27 +1,65 @@
 '''
-    Explanation I: Using most_common() method in Collections Counter
-        - TC: 
-            - addScore() - O(1)
-            - top() - O(n log K) where n is the number of players
-            - reset() - O(1)
-        - SC: O(n)
+    Explanation I: Sorting
+        - addScore() - Create a hashmap that stores playerId to score
+            - TC: O(1)
+        - top() - Store the values of the hashmap in a scores array
+                - Sort the scores array
+                - Loop through the scores array and get the first k scores and add the scores to your result
+                - TC: O(n logn)
+        - reset() - Pop the playerId from the hashmap
+            - TC: O(1)
+            
+        - SC: O(n) used by the hashmap and the scores array
+    
+    Explanation II: Using most_common() method in Collections Counter
+        - Counter() returns a dictionary which is unordered
+        - most_common() returns the sorted dictionary based on the count specified
+
+        - addScore() - TC: O(1)
+        - top() - TC: O(n log K) where n is the number of players
+        - reset() - TC: O(1)
+        
+        - SC: O(n) used by the counter
 '''
 
 from collections import Counter, defaultdict
-import heapq
 
 class Leaderboard1:
     def __init__(self):
-        self.counter = Counter()
+        # map: key: playerId, value: score
+        self.playersToScores = defaultdict(int)
         
     def addScore(self, playerId: int, score: int) -> None:
-        self.counter[playerId] += score
+        self.playersToScores[playerId] += score
 
     def top(self, K: int) -> int:
-        return sum(map(lambda x: x[1], self.counter.most_common(K)))
+        scores = []
+        
+        for key in self.playersToScores:
+            scores.append(self.playersToScores[key])
+        scores.sort(reverse=True) # sorts scores in descending order
+        
+        res, i = 0, 0
+        while i < K:
+            res += scores[i]
+            i += 1
+        return res
         
     def reset(self, playerId: int) -> None:
-        self.counter[playerId] = 0
+        self.playersToScores.pop(playerId)
+
+class Leaderboard2:
+    def __init__(self):
+        self.playersToScores = Counter()
+        
+    def addScore(self, playerId: int, score: int) -> None:
+        self.playersToScores[playerId] += score
+
+    def top(self, K: int) -> int:
+        return sum(map(lambda x: x[1], self.playersToScores.most_common(K)))
+        
+    def reset(self, playerId: int) -> None:
+        self.playersToScores.pop(playerId)
         
 # Your Leaderboard object will be instantiated and called as such:
 # obj = Leaderboard()
