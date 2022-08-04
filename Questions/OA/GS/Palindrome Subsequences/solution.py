05eddfae6d32da3e9a73ec8ec95caea920db96a5
doyedele1 <demilade.oyedele@gmail.com>
2022-08-04 22:17:27 +0100

    solved-OA-question

---
+++ Questions/OA/GS/Palindrome Subsequences/solution.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+MODULO = 10 ** 9 + 7
+
+def getPalindromesCount(s):
+    count = Counter([''])
+    freqFives = 0
+
+    for c in s:
+        for item, freq in list(count.items()):
+            item += c
+            if len(item) < 5:
+                count[item] += freq
+            elif item == item[::-1]:
+                freqFives += freq
+    
+    return freqFives % MODULO
