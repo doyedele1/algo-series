1d55ca936172365d6a722bd01d843fc814bcd979
doyedele1 <demilade.oyedele@gmail.com>
2022-08-08 15:12:46 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/56. Merge Intervals/solution.py
+++ Questions/LC Questions/Arrays/56. Merge Intervals/solution.py
@@ -1,35 +1,34 @@
 '''
-    Explanation:
-        Is input in sorted order?
-        If not in sorted order,
-            [[1,3], [8, 10], [15, 18], [2,6]]
-        After drawing the number line, we sort the interval by the start value
-        Iterate through each start value, check does the most recent interval overlap with the previous interval, we merge
-        Else we leave it like that
+    Explanation:    
+        [[1,3], [8, 10], [15, 18], [2,6]]
+        1-----3
+                        8------10
+                                                15--------18
+                    2------6
+        - Sort intervals based on the start times
+        [[1,3], [2,6], [8, 10], [15, 18]]
+        - Add the first interval to the res nested array
+        - Iterate through the intervals starting from the second interval,
+            - Check the most recent added interval to res
+            - If the current start time overlap with the end time of the most recent added interval, we merge
+            - Else, we leave it like that
 
-        TC - O(n logn) where n is the numnber of intervals given
-        SC - O(n). We allocate linear space to store a copy of intervals and sort that
+        TC - O(nlogn)
+        SC - O(1)
 '''
 
 from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # Sorting by the start value in Python is straightforward. i stands for the interval
         intervals.sort(key= lambda i: i[0])
         
-        # Declaring res to append the merged intervals and initializing with the first interval to cover some edge case
         res = [intervals[0]]
         
         for start, end in intervals[1:]:
-            # Get the end value of the most recently added interval
-            last_end = res[-1][1]
-            
-            # This means they are overlapping
-            if start <= last_end:
-                # Get the end of the merged interval and merge - add to the res tuple
-                res[-1][1] = max(last_end, end)
+            recentEnd = res[-1][1]
+            if start <= recentEnd:
+                res[-1][1] = max(recentEnd, end)
             else:
                 res.append([start, end])
-                
         return res
