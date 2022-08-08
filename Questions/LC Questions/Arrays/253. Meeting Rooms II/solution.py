66c0dafbd9ff9ea180f2ce36cb0e22d512e82426
doyedele1 <demilade.oyedele@gmail.com>
2022-08-08 14:14:18 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/253. Meeting Rooms II/solution.py
+++ Questions/LC Questions/Arrays/253. Meeting Rooms II/solution.py
@@ -18,14 +18,36 @@
             - heap = [20, 30]
         - Return length of heap which is 2
         
-        - TC: O(nlogn). nlogn for sorting and nlogn for n add operations to the heap
+        - TC: O(nlogn). nlogn for sorting and another nlogn for n add operations to the heap
         - SC: O(n) for the min heap we constructed
+
+
+    Explanation II: Sorting, then Two Pointers
+    [[0,30],[5,10],[15,20]]
+    - Sort startArr = [0, 5, 15]
+    - Sort endArr = [30, 10, 20]
+    
+    [0, 5, 15]          [30, 10, 20]
+    s                     e
+    res = 0
+    - If startArr[s] >= endArr[e], then it means the room is free and we can move the end pointer and decrement our result
+    - If startArr[s] < endArr[e], then it means the room is not free and we can move the start pointer and increment our result
+    [0, 5, 15]          [30, 10, 20]
+        s                 e
+    res = 1
+    
+    [0, 5, 15]          [30, 10, 20]
+            s             e
+    res = 2
+    
+    - TC: O(nlogn) for sorting startArr and endArr
+    - SC: O(n) for the space taken by the startArr and endArr
 '''
 
 from typing import List
 import heapq
 
-class Solution:
+class Solution1:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         heap = []
         intervals.sort(key= lambda i: i[0])
@@ -38,3 +60,21 @@ class Solution:
             heapq.heappush(heap, i[1])
         
         return len(heap)
+
+
+class Solution2:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        res, start, end = 0, 0, 0
+        
+        startArr = sorted([i[0] for i in intervals])
+        endArr = sorted([i[1] for i in intervals])
+        
+        while start < len(intervals):
+            if startArr[start] >= endArr[end]:
+                res -= 1
+                end += 1
+            
+            res += 1
+            start += 1
+            
+        return res
