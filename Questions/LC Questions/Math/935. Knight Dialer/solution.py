adcb32cb1562012efdf83e262f5cd3027e3e1115
doyedele1 <demilade.oyedele@gmail.com>
2022-08-13 15:09:43 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Math/935. Knight Dialer/solution.py
+++ Questions/LC Questions/Math/935. Knight Dialer/solution.py
@@ -1,28 +1,30 @@
 '''
     Explanation: Dynamic Programming
-        rows - number of steps n
+        rows - n which is the length of the digits we can dial
         columns - 0 to 9 possible digits
         
-        dp[2][0] = dp[1][4] + dp[1][6]
+        dp[2][0] = dp[1][4] + dp[1][6], since we can perform n-1 jumps
         Generally, dp[n][1] = dp[n-1][6] + dp[n-1][8]
+        
+        For n = 2, we can dial 04, 06, 16, 18, 27, 29, 34, 38, 40, 43, 49, 60, 61, 67, 72, 76, 81, 83, 92, 94
         
         The DP table:
             0   1   2   3   4   5   6   7   8   9
         0   0   0   0   0   0   0   0   0   0   0
         1   1   1   1   1   1   1   1   1   1   1
-        2   2   2   2   2   
-        3
+        2   2   2   2   2   3   0   3   2   2   2
         .
         .
         .
         n
         
-
+        We don't need a 2D array. A 1D array will do the job. dp[i] = number of steps, i itself will be the possible digits 0-9.
 '''
 
 class Solution:
+    MODULO = 10**9 + 7
+    
     def knightDialer(self, n: int) -> int:
-        MODULO = 10**9 + 7
         dp = [1] * 10
         
         while n >= 2:
@@ -40,4 +42,4 @@ class Solution:
             dp[9] = temp[2] + temp[4]
             n -= 1
             
-        return sum(dp) % MODULO
+        return sum(dp) % self.MODULO
