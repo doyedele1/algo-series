7fd5e7b49f9913872292058b7eee09a56cf8c1e3
doyedele1 <demilade.oyedele@gmail.com>
2022-08-13 16:38:13 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Strings/1347. Minimum Number of Steps to Make Two Strings Anagram/solution.py
+++ Questions/LC Questions/Strings/1347. Minimum Number of Steps to Make Two Strings Anagram/solution.py
@@ -1,18 +1,47 @@
 '''
-    Explanation: Using a hashmap
+    Explanation:
+        - Store the occurence of characters in s in a hashmap or array
+        - Reduce the occurence of each character seen in s in t by 1
+        - If the frequency of a character is more than 0, i.e. the characters left are stopping the two strings becomes anagram of each other,
+            - Then add the counts together
+            
+        - "bab", "aba"
+            counts = {b: 2, a: 1}
+            - Checking t,
+                First character: counts = {b: 2, a: 0}
+                Second character: counts = {b: 1, a: 0}
+                Third character: counts = {b: 1, a: -1}
+            - count of b is 1, so res += 1 which is finally 1
+            
+        - TC: O(n)
+        - SC: O(n) for the hashmap solution and O(1) for the array solution
 '''
 
-class Solution:
+# Using a fixed array of size 26
+class Solution1:
     def minSteps(self, s: str, t: str) -> int:
         res = 0
-        dictS = dict()
+        counts = [0] * 26
+        
+        for i in range(len(s)):
+            counts[ord(s[i]) - ord('a')] += 1
+            counts[ord(t[i]) - ord('a')] -= 1
+        
+        for count in counts:
+            if count > 0: res += count
+        return res
+
+# Using a hashmap
+class Solution2:
+    def minSteps(self, s: str, t: str) -> int:
+        res = 0
+        counts = dict()
         
         for char in s:
-            if char in dictS: dictS[char] += 1
-            else: dictS[char] = 1
+            if char in counts: counts[char] += 1
+            else: counts[char] = 1
         
         for char in t:
-            if char in dictS and dictS[char] > 0:
-                dictS[char] -= 1
+            if char in counts and counts[char] > 0: counts[char] -= 1
             else: res += 1
         return res
