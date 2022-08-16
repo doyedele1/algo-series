24a73441698143d1419ee14dead04c6fe2e677ea
doyedele1 <demilade.oyedele@gmail.com>
2022-08-16 19:41:05 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Linked List/445. Add Two Numbers II/solution.py
+++ Questions/LC Questions/Linked List/445. Add Two Numbers II/solution.py
@@ -6,7 +6,8 @@ class ListNode:
         self.val = val
         self.next = next
 
-class Solution:
+# Reversing the sum linked list
+class Solution1:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         num1 = 0
         num2 = 0
@@ -39,3 +40,28 @@ class Solution:
             previous = temp
             temp = nxt
         return previous
+
+class Solution2:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1 = 0
+        num2 = 0
+        
+        while l1:
+            num1 = num1 * 10 + l1.val
+            l1 = l1.next
+            
+        while l2:
+            num2 = num2 * 10 + l2.val
+            l2 = l2.next
+            
+        sum = num1 + num2
+        
+        dummy = ListNode(0)
+        temp = dummy
+        
+        if sum == 0: return dummy
+        
+        while sum > 0:
+            temp.next = ListNode(sum % 10, temp.next) 
+            sum //= 10
+        return dummy.next
