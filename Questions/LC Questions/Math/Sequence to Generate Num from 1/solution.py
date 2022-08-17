b6d9ae6eb4da585c623ec6595298f043195d750e
doyedele1 <demilade.oyedele@gmail.com>
2022-08-17 17:51:59 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Math/Sequence to Generate Num from 1/solution.py
+++ Questions/LC Questions/Math/Sequence to Generate Num from 1/solution.py
@@ -1,24 +1,23 @@
 def generateSequence(target):
-    visited = set({1})
+    cache = set({1})
     outputString = ""
     start = 1
 
     while start != target:
         if start < target:
             start *= 2
-            visited.add(start)
+            cache.add(start)
             outputString += "*"
         if start > target:
             potentialStart = start // 3
-            if potentialStart in visited:
+            if potentialStart in cache:
                 start *= 2
-                visited.add(start)
+                cache.add(start)
                 outputString += "*"
             else:
                 start = potentialStart
-                visited.add(start)
+                cache.add(start)
                 outputString += "/"
-
     return outputString
 
-print(generateSequence(12))
+# print(generateSequence(12))
