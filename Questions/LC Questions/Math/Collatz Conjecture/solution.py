c35810f86e5f103739266e21d0c119622bf711a0
doyedele1 <demilade.oyedele@gmail.com>
2022-08-17 16:31:43 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Math/Collatz Conjecture/solution.py
+++ Questions/LC Questions/Math/Collatz Conjecture/solution.py
@@ -1,25 +1,14 @@
-# n = 5
-
-# even => n/2, odd => 3n+1
-#  reach 1
-
-
-# 5, 16, 8, 4, 2, 1 => 5
-# 10, 5 = 5+1 = 6
-
-
-# {1:0, 5:5, 16:4, 8:3, 4:2, 2:1}
+# {1: 0, 5: 5, 16: 4, 8: 3, 4: 2, 2: 1}
 
 class CollatzConjecture:
     def _init_(self):
         self.cache = { 1: 0 } # number : steps
 
     def steps(self, number):
-
         start = number # 4   3   2   1
         steps = [] # [5, 16, 8, 4, 2]
 
-        while (start != 1):
+        while start != 1:
 
             if start in self.cache:
                 break
@@ -29,7 +18,7 @@ class CollatzConjecture:
             if (start % 2 == 0):
                 start = start // 2
             else:
-                start = (3*start)+1
+                start = (3 * start) + 1
         
         totalSteps = self.cache.get(start, 0) + 1
 
