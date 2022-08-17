74339f216a01b38f88589a6ea05234fc1af07d46
doyedele1 <demilade.oyedele@gmail.com>
2022-08-17 17:48:59 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Math/1342. Number of Steps to Reduce a Number to Zero/solution.py
+++ Questions/LC Questions/Math/1342. Number of Steps to Reduce a Number to Zero/solution.py
@@ -1,20 +1,17 @@
-# Naive Solution --> TC - O(log n), SC - O(1)
-# class Solution:
-#     def numberOfSteps(self, num: int) -> int: 
-#         if num == 0:
-#             return 0
-#         steps = 0
-#         while num > 0:
-#             if num % 2 == 0:
-#                 num //= 2
-#             else:
-#                 num -= 1
-#             steps += 1
-            
-#         return steps
+# Brute-Force Solution --> TC - O(log n), SC - O(1)
+class Solution1:
+    def numberOfSteps(self, num: int) -> int: 
+        if num == 0: return 0
+        
+        steps = 0
+        while num > 0:
+            if num % 2 == 0: num //= 2
+            else: num -= 1
+            steps += 1
+        return steps
         
 # Counting Bits --> TC - O(log n), SC - O(log n)
-class Solution:
+class Solution2:
     def numberOfSteps(self, num: int) -> int: 
         binaryEquivalent = bin(num)[2:]
         ones = binaryEquivalent.count("1")
