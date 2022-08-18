6376bf89cfa907dd2722886db0a1a055bf105041
doyedele1 <demilade.oyedele@gmail.com>
2022-08-18 17:43:46 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Arrays/716. Max Stack/2dllHeapSet.py
+++ Questions/LC Questions/Arrays/716. Max Stack/2dllHeapSet.py
@@ -77,3 +77,11 @@ class MaxStack:
         if node.next:
             node.next.pre = node.pre
         return num
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
