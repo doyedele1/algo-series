8cb97abe644c9c68bbc94af4998138c9b4805ebe
doyedele1 <demilade.oyedele@gmail.com>
2022-08-18 17:11:55 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Arrays/716. Max Stack/solution.py
+++ Questions/LC Questions/Arrays/716. Max Stack/solution.py
@@ -1,32 +1,4 @@
-# Using a nested array - not accepted on LC
-class MaxStack:
-    def __init__(self):
-        self.stack = []
-
-    def push(self, x: int) -> None:
-        curmax = max(x, self.stack[-1][1] if self.stack else float("-inf"))
-        self.stack.append([x, curmax])
-
-    def pop(self) -> int:
-        return self.stack.pop()[0]
-
-    def top(self) -> int:
-        return self.stack[-1][0]
-
-    def peekMax(self) -> int:
-        return self.stack[-1][1]
-
-    def popMax(self) -> int:
-        temp = []
-        curmax = self.peekMax()
-        while curmax != self.stack[-1][0]:
-            temp.append(self.stack.pop()[0])
-        self.stack.pop()
-        while temp:
-            x = temp.pop()
-            self.stack.append([x, max(x, self.stack[-1][1] if self.stack else float("-inf"))])
-        return curmax
-
+# Using two stacks - not accepted on LC
 class MaxStack:
     def __init__(self):
         self.stack = []
@@ -62,64 +34,4 @@ class MaxStack:
         self.pop()
         while len(buff) != 0:
             self.push(buff.pop(-1))
-        return val
-
-# Accepted on LC, but sortedcontainers library is only available on LC
-from sortedcontainers import SortedDict
-
-class Node:
-    def __init__(self, val, prev=None, nxt=None):    
-        self.val = val
-        self.prev = prev
-        self.next = nxt
-                
-class DoubleLinkedList:
-    def __init__(self):
-        self.tail = Node(0)
-        self.head = Node(0, None, self.tail)
-        self.tail.prev = self.head
-        
-    def add(self, val):
-        new_node = Node(val, None, self.tail)
-        new_node.prev = self.tail.prev        
-        self.tail.prev.next = new_node        
-        self.tail.prev = new_node
-        return new_node
-    
-    def peek(self): return self.tail.prev.val
-    
-    def unlink(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        return node
-        
-    def pop(self):
-        return self.unlink(self.tail.prev).val
-                                            
-class MaxStack:
-    def __init__(self):
-        self.list = DoubleLinkedList()
-        self.max = SortedDict()
-                
-    def push(self, x: int) -> None: 
-        node = self.list.add(x)        
-        if x not in self.max: self.max[x] = []            
-        self.max[x].append(node)
-    
-    def top(self) -> int: 
-        return self.list.peek()
-        
-    def peekMax(self) -> int:
-        return self.max.peekitem()[0]
-        
-    def pop(self) -> int:
-        val = self.list.pop()
-        self.max[val].pop()
-        if not self.max[val]: del self.max[val]
-        return val
-        
-    def popMax(self) -> int:
-        val, addr = self.max.peekitem()
-        self.list.unlink(addr.pop())        
-        if not addr: del self.max[val]            
         return val
