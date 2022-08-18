b01767c81db7e6bb7eef795b676ae37e65eb79c4
doyedele1 <demilade.oyedele@gmail.com>
2022-08-18 23:20:56 +0100

    solved-Bloomberg-question

---
+++ Questions/LC Questions/Strings/1332. Remove Palindromic Subsequences/solution.py
@@ -0,0 +1,17 @@
+# TC: O(n), SC: O(1)
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        def validPalindrome(s):
+            left = 0
+            right = len(s) - 1
+            
+            while left < right:
+                if s[left] != s[right]: return False
+                
+                left += 1
+                right -= 1
+            return True
+        
+        if len(s) == 0: return 0
+        if validPalindrome(s): return 1
+        return 2
