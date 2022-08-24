4fd422ed41cddcb0f7010faefc4d4cd23bd7b603
doyedele1 <demilade.oyedele@gmail.com>
2022-08-24 09:53:58 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Graphs/399. Evaluate Division II/solution.py
+++ Questions/LC Questions/Graphs/399. Evaluate Division II/solution.py
@@ -2,13 +2,20 @@ from collections import defaultdict
 from collections import deque
 
 def solution(start, end, rates):
-    dict = defaultdict(list)
+    graph = defaultdict(list)
     
     for rate in rates:
-        dict[rate[0]].append([rate[1], rate[2]])
-        dict[rate[1]].append([rate[0], 1.0000/rate[2]])
-        # Dict: {USD: [[JPY, 110], [AUD, 1.45]], (JPY: USD, 1/110), (USD: ), (AUD: USD, 1/1.45), (JPY: GBP, 0.007), (GBP: JPY, 1/0.007}
-        # print(dict)
+        graph[rate[0]].append([rate[1], rate[2]])
+        graph[rate[1]].append([rate[0], 1.0000/rate[2]])
+        '''
+            print(graph) returns
+                graph: {
+                    USD: [[JPY, 110], [AUD, 1.45]], 
+                    JPY: [[USD, 1/110], [GBP, 0.007]], 
+                    AUD: [[USD, 1/1.45]],
+                    GBP: [[JPY, 1/0.007]]
+                }
+        '''
 	
     q = deque([(start, 1.0000)])
     visited = set()
@@ -21,7 +28,7 @@ def solution(start, end, rates):
         if curr == end: return round(cost, 2)
         # if curr in visited: continue
 		
-        for neighbors in dict[curr]:
+        for neighbors in graph[curr]:
             if neighbors[0] not in visited:
                 q.append((neighbors[0], cost * neighbors[1]))
                 visited.add(neighbors[0])
