00b66bb06d0d417a59c90fbc74520cd399a34530
doyedele1 <demilade.oyedele@gmail.com>
2022-08-28 22:01:04 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Graphs/797. All Paths From Source to Target/solution.py
+++ Questions/LC Questions/Graphs/797. All Paths From Source to Target/solution.py
@@ -1,3 +1,18 @@
+'''
+    Explanation:
+        Input: [[1,2],[3],[3],[]]
+        Output: [[0,1,3],[0,2,3]]
+    
+                            f(0, 3)
+                        /             \
+                    f(1,3)          f(2,3)
+                        |              |
+                    f(3,3)           f(3,3)
+                    
+    - TC: O(n * 2^n)
+    - SC: O(n)
+'''
+
 from typing import List
 
 class Solution:
@@ -5,11 +20,12 @@ class Solution:
         target = len(graph) - 1
         res = []
         
-        def dfs(currNode, path):
-            if currNode == target: res.append(path)
+        def dfs(path, currNode):
+            if currNode == target: res.append(path + [currNode])
             
-            for nextNode in graph[currNode]:
-                dfs(nextNode, path + [nextNode])
+            else:
+                for nextNode in graph[currNode]:
+                    dfs(path + [currNode], nextNode)
         
-        dfs(0, [0])
+        dfs([], 0)
         return res
