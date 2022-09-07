ae1a34b69c7e4224e98a0d890b447f93b314b096
doyedele1 <demilade.oyedele@gmail.com>
2022-09-07 21:10:10 +0100

    solved-LC-LL-question

--- Questions/LC Questions/Linked List/622. Design Circular Queue/solution.py
+++ Questions/LC Questions/Linked List/622. Design Circular Queue/solution.py
@@ -1,49 +1,104 @@
 '''
     enqueue: add element to the end of the queue
     dequeue: remove element from the front of the queue
+    Explanation I: Using array
+    Explanation II: using a singly linked list
     Explanation II: Using a doubly-linked list
 '''
 
-class ListNode:
-    def __init__(self, value, nextNode, prevNode):
+class ListNode1:
+    def __init__(self, value):
         self.value = value
-        self.nextNode =  nextNode
-        self.prevNode = prevNode
+        self.next =  None
         
-class MyCircularQueue:
+class MyCircularQueue2:
     def __init__(self, k: int):
-        self.head = ListNode(0, None, None)
-        self.tail = ListNode(0, None, self.head)
-        self.head.nextNode = self.tail
+        self.head = None
+        self.tail = None
+        self.capacity = k
+        self.counter = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull(): return False
+        
+        if self.counter == 0:
+            self.head = ListNode1(value)
+            self.tail = self.head
+        else:
+            newNode = ListNode1(value)
+            self.tail.next = newNode
+            self.tail = newNode
+        self.counter += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty(): return False
+        self.head = self.head.next
+        self.counter -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty(): return -1
+        return self.head.value
+
+    def Rear(self) -> int:
+        if self.isEmpty(): return -1
+        return self.tail.value
+
+    def isEmpty(self) -> bool:
+        return self.counter == 0
+
+    def isFull(self) -> bool:
+        return self.counter == self.capacity
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
+class ListNode2:
+    def __init__(self, value, nxt, previous):
+        self.value = value
+        self.next =  nxt
+        self.previous = previous
+class MyCircularQueue3:
+    def __init__(self, k: int):
+        self.head = ListNode2(0, None, None)
+        self.tail = ListNode2(0, None, self.head)
+        self.head.next = self.tail
         self.space = k
 
     def enQueue(self, value: int) -> bool:
         if self.isFull(): return False
         
-        newNode = ListNode(value, self.tail, self.tail.prevNode)
-        self.tail.prevNode.nextNode = newNode
-        self.tail.prevNode = newNode
+        newNode = ListNode2(value, self.tail, self.tail.previous)
+        self.tail.previous.next = newNode
+        self.tail.previous = newNode
         self.space -= 1
         return True
 
     def deQueue(self) -> bool:
         if self.isEmpty(): return False
         
-        self.head.nextNode = self.head.nextNode.nextNode
-        self.head.nextNode.prevNode = self.head
+        self.head.next = self.head.next.next
+        self.head.next.previous = self.head
         self.space += 1
         return True
 
     def Front(self) -> int:
         if self.isEmpty(): return -1
-        return self.head.nextNode.value
+        return self.head.next.value
 
     def Rear(self) -> int:
         if self.isEmpty(): return -1
-        return self.tail.prevNode.value
+        return self.tail.previous.value
 
     def isEmpty(self) -> bool:
-        return self.head.nextNode == self.tail
+        return self.head.next == self.tail
 
     def isFull(self) -> bool:
         return self.space == 0
