6d1fd3c47c242a6d5833580a0074c18e2c5f7fc6
doyedele1 <demilade.oyedele@gmail.com>
2022-09-14 23:14:03 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Arrays/1169. Invalid Transactions/solution.py
+++ Questions/LC Questions/Arrays/1169. Invalid Transactions/solution.py
@@ -1,6 +1,6 @@
 '''
     Explanation I: Brute-force Solution
-        - Convert the transactions to individual item of the valid data types and store it in an array. i.e. alice = string, 20 = integer, 800 = integer and mtv = string
+        - Convert the transactions to individual item of the valid data types and store it in an array. index.e. alice = string, 20 = integer, 800 = integer and mtv = string
         - Loop through the new array and find where amount > 1000. Append that to result array and convert the transaction to a string splitting each item by comma
             - Nested loop to check for same name, and if time difference is less than or equal to 60 and if different city,
                 - Append that to the res array and convert the transaction to a string splitting each item by comma
@@ -28,12 +28,13 @@ from typing import List
 class Solution1:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         res = []
+
         for i, trans1 in enumerate(transactions):
             name1, time1, amount1, city1 = trans1.split(',')
             if int(amount1) > 1000:
                 res.append(trans1)
                 continue
-            for x, trans2 in enumerate(transactions):
+            for j, trans2 in enumerate(transactions):
                 name2, time2, amount2, city2 = trans2.split(',')
                 if name1 == name2 and abs(int(time1) - int(time2)) <= 60 and city1 != city2:                
                     res.append(trans1) # deals with duplicates by only appending t1
