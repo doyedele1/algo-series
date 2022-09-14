2ef85b3aa4687ef54905a5534bee9bad2f22d440
doyedele1 <demilade.oyedele@gmail.com>
2022-09-14 22:58:11 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Strings/1047. Remove All Adjacent Duplicates In String/solution.py
+++ Questions/LC Questions/Strings/1047. Remove All Adjacent Duplicates In String/solution.py
@@ -3,7 +3,7 @@ class Solution:
         stack = []
         
         for char in s:
-            if len(stack) > 0 and stack[-1] == char:
+            if stack and stack[-1] == char:
                 stack.pop()
             else: stack.append(char)
         return "".join(stack)
