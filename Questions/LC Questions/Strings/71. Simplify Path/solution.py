fe66fd703c0bd8b214456fe0a7560f018bc9bd6c
doyedele1 <demilade.oyedele@gmail.com>
2022-09-15 23:05:23 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Strings/71. Simplify Path/solution.py
+++ Questions/LC Questions/Strings/71. Simplify Path/solution.py
@@ -1,21 +1,4 @@
-class Solution:
-    def simplifyPath(self, path: str) -> str:
-        # Initialize a stack that helps pop when we encounter double dots
-        stack = []
-        print(path.split("/"))
-        
-        for char in path.split("/"):
-            if char == "..":
-                if len(stack) != 0: stack.pop()
-            elif char != "" and char != ".":
-                stack.append(char)
-            else: 
-                char = ""
-
-        return "/" + "/".join(stack)
-        
-        
-        '''
+'''
             Example 1:
                 Input = "/../abc//./def/"
                 Simplified =  "/abc/def"
@@ -27,3 +10,18 @@ class Solution:
                 Time complexity - O(N) - where N is the length of the path
                 Space complexity - O(N) - memory needed to create the simplified list
         '''
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        # Initialize a stack that helps pop when we encounter double dots
+        stack = []
+        
+        for char in path.split("/"):
+            if char == "..":
+                if len(stack) != 0: stack.pop()
+            elif char != "" and char != ".":
+                stack.append(char)
+            else: 
+                char = ""
+
+        return "/" + "/".join(stack)
