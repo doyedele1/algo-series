537cc7b2fc5b1766c762c9a81c2871425f597ee7
doyedele1 <demilade.oyedele@gmail.com>
2022-09-16 23:50:08 +0100

    solved-Bloomberg-question

--- Questions/LC Questions/Arrays/1169. Invalid Transactions/solution.py
+++ Questions/LC Questions/Arrays/1169. Invalid Transactions/solution.py
@@ -29,13 +29,13 @@ class Solution1:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         res = []
 
-        for i, trans1 in enumerate(transactions):
+        for trans1 in transactions:
             name1, time1, amount1, city1 = trans1.split(',')
             if int(amount1) > 1000:
                 res.append(trans1)
                 continue
 
-            for j, trans2 in enumerate(transactions):
+            for trans2 in transactions:
                 name2, time2, amount2, city2 = trans2.split(',')
                 if name1 == name2 and abs(int(time1) - int(time2)) <= 60 and city1 != city2:                
                     res.append(trans1) # deals with duplicates by only appending t1
