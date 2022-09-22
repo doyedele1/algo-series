cc7c5fcfb613e27b6a4a024500c8be7aefd457fc
doyedele1 <demilade.oyedele@gmail.com>
2022-09-22 20:43:22 +0100

    solved-Bloomberg-question

---
+++ Questions/OA/Codepath/Longest Number/solution.py
@@ -0,0 +1,25 @@
+def findLargest(nums):
+    quickSort(nums, 0, len(nums)-1)
+    return str(int("".join(map(str, nums)))) 
+
+def quickSort(nums, l, r):
+    if l >= r:
+        return 
+    pos = partition(nums, l, r)
+    quickSort(nums, l, pos-1)
+    quickSort(nums, pos+1, r)
+
+def compare(num1, num2):
+    return str(num1) + str(num2) > str(num2) + str(num1)
+    
+def partition(nums, l, r):
+    low = l
+    while l < r:
+        if compare(nums[l], nums[r]):
+            nums[l], nums[low] = nums[low], nums[l]
+            low += 1
+        l += 1
+    nums[low], nums[r] = nums[r], nums[low]
+    return low
+
+print(findLargest([10, 2]))
