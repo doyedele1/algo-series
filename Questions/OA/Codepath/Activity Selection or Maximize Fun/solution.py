1562e183dcbe6e86665dcd6ef17c231bfdfd64f1
doyedele1 <demilade.oyedele@gmail.com>
2022-09-24 18:46:10 +0100

    solved-OA-question

--- Questions/OA/Codepath/Activity Selection or Maximize Fun/solution.py
+++ Questions/OA/Codepath/Activity Selection or Maximize Fun/solution.py
@@ -1,4 +1,6 @@
 def activitySelection(activities, start, end):
+    activities.sort()
+
     i = 0
     res = [activities[i]]
 
@@ -6,4 +8,7 @@ def activitySelection(activities, start, end):
         if start[j] >= end[i]:
             res.append(activities[j])
             i = j
+
     return res
+
+print(activitySelection(['a', 'b', 'c', 'd', 'e', 'f', 'g'], [0,1,5,6,8,10,11], [2,4,6,8,11,12,20]))
