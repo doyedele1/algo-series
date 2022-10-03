f7a8d269796eb4f1d0c0ab23962be7cdcc9c5c86
doyedele1 <demilade.oyedele@gmail.com>
2022-10-03 18:38:02 +0100

    solved-Amazon-OA

--- Questions/OA/Amazon/Get Max Freq Deviation/solution.py
+++ Questions/OA/Amazon/Get Max Freq Deviation/solution.py
@@ -1,3 +1,9 @@
-def getMaxFreqDeviation(s):
-    deviation = 0
-    chars = list(set(s))
+# def getMaxFreqDeviation(s):
+#     deviation = 0
+#     chars = list(set(s))
+
+#     for i in range(len(chars)):
+#         for j in range(i + 1, len(chars)):
+#             char1, char2 = chars[i], chars[j]
+#             currDiff, maxDiff, minDiff = 0, 0, 0
+#             lastChar1Diff, lastChar2Diff
--- Questions/OA/Amazon/Minimum Groups/solution.py
+++ Questions/OA/Amazon/Minimum Groups/solution.py
@@ -12,16 +12,17 @@
     Seventh iteration: count = 3, start = 6
 '''
 
-def minimumGroups(arr, k):
-    arr.sort()
+def minimumGroups(awards, k):
+    awards.sort()
     start = 0
-    if len(arr) == 0: return 0
+    if len(awards) == 0: return 0
     
     count = 1
-    for i in range(len(arr)):
-        if (arr[i] - arr[start]) > k:
+    for i in range(len(awards)):
+        if (awards[i] - awards[start]) > k:
             count += 1
             start = i
     return count
 
-print(minimumGroups([1, 13, 6, 8, 9, 3, 5], 4))
+# print(minimumGroups([1, 13, 6, 8, 9, 3, 5], 4))
+# print(minimumGroups([1, 5, 4, 6, 8, 9, 2], 3))
