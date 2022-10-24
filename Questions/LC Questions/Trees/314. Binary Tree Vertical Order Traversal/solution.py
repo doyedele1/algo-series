702af54414d2943b72337b59e88907cefa3e5f0a
doyedele1 <demilade.oyedele@gmail.com>
2022-10-24 20:09:47 +0100

    solved-LC-question

--- Questions/LC Questions/Trees/314. Binary Tree Vertical Order Traversal/solution.py
+++ Questions/LC Questions/Trees/314. Binary Tree Vertical Order Traversal/solution.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from collections import deque, defaultdict
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -6,42 +7,24 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        if root == None: return []
-        cache = {}
-        self.min_col = 0
-        self.max_col = 0
+        if root is None: return []
         
-        def dfs(node, row, col):
-            if node == None: return None
-            if col in cache: cache[col].append([row, node.val])
-            else: cache[col] = [[row, node.val]]
-            self.min_col = min(self.min_col, col)
-            self.max_col = max(self.max_col, col)
-            dfs(node.left, row + 1, col - 1)
-            dfs(node.right, row + 1, col + 1)
-        
-        dfs(root, 0, 0)
+        table = defaultdict(list)
+        minColumn  = maxColumn = 0
+        queue = deque([(root,0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                table[column].append(node.val)
+                minColumn = min(minColumn, column)
+                maxColumn = max(maxColumn, column)
+    
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
             
-        for c in range(self.min_col, self.max_col + 1):
-            col = sorted(cache[c], key = lambda i: (i[0]))
-            col_sorted = []
-            for p in col:
-                col_sorted.append(p[1])
-            res.append(col_sorted)
-        
-        return res
-        
-        
-        '''
-            Explanation:
-                - We will use a dfs preorder traversal to traverse the tree
-                    - dfs(node, row, col)
-                - We will initialize an empty cache that stores the column of the node as key and the row and value of the node as value. Key-value pair
-                    - If the column is not in the cache, we will add it to the cache
-                    - If the column is in the cache, we will append the row and value of the node to the cache of specific key (or column)
-                - After the dfs helper function terminates, we will need to get the min_column and max_column from the cache that was created
-        '''
+        return [table[x] for x in range(minColumn, maxColumn + 1)]
