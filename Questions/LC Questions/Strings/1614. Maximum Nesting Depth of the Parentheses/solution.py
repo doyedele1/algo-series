c41337955111220b4e8fccb1afd9f63fe0e4b651
doyedele1 <demilade.oyedele@gmail.com>
2022-10-24 20:06:00 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/1614. Maximum Nesting Depth of the Parentheses/solution.py
+++ Questions/LC Questions/Strings/1614. Maximum Nesting Depth of the Parentheses/solution.py
@@ -1,12 +1,11 @@
 '''
-    NOTE: The depth of any character in the VPS is the ( number of left brackets before it ) - ( number of right brackets before it )
+    NOTE: The depth of any character in the VPS is the (number of left brackets before it) - (number of right brackets before it)
 '''
 
 # TC: O(n), SC: O(n)
 class Solution:
     def maxDepth(self, s: str) -> int:
-        stack = []
-        res = 0
+        stack, res = [], 0
         
         for char in s:
             if stack and char == ")": stack.pop()
