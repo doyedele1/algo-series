7e049f5d17a92ddc112de361ec741f237784cddf
doyedele1 <demilade.oyedele@gmail.com>
2022-10-31 15:13:43 +0100

    solved-LC-question

--- Questions/LC Questions/Trees/257. Binary Tree Paths/solution.py
+++ Questions/LC Questions/Trees/257. Binary Tree Paths/solution.py
@@ -7,7 +7,8 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
+# Iterative DFS solution
+class Solution1:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         stack = [(root, str(root.val))]
         res = []
@@ -15,11 +16,21 @@ class Solution:
         while stack:
             node, path = stack.pop()
             
-            if not node.left and not node.right:
-                res.append(path)
-            if node.left:
-                stack.append((node.left, path + "->" + str(node.left.val)))
-            if node.right:
-                stack.append((node.right, path + "->" + str(node.right.val)))
+            if not node.left and not node.right: res.append(path)
+            if node.left: stack.append((node.left, path + "->" + str(node.left.val)))
+            if node.right: stack.append((node.right, path + "->" + str(node.right.val)))
         
+        return res
+
+# Recursive DFS solution
+class Solution2:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        res = []
+
+        def dfs(node, path):
+            if not node.left and not node.right: res.append(path)
+            if node.left: dfs(node.left, path + "->" + str(node.left.val))
+            if node.right: dfs(node.right, path + "->" + str(node.right.val))
+        
+        dfs(root, str(root.val))
         return res
