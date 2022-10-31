fe7701d9c8824a73125e32cb20bc350999abb86d
doyedele1 <demilade.oyedele@gmail.com>
2022-10-31 13:24:55 +0100

    solved-Amazon-OA

--- Questions/OA/Amazon/Get Max Freq Deviation/solution.py
+++ Questions/OA/Amazon/Get Max Freq Deviation/solution.py
@@ -1,9 +1,16 @@
-# def getMaxFreqDeviation(s):
-#     deviation = 0
-#     chars = list(set(s))
+def getMaxFreqDeviation(s):
+    res = 0
+    chars = list(set(s))
 
-#     for i in range(len(chars)):
-#         for j in range(i + 1, len(chars)):
-#             char1, char2 = chars[i], chars[j]
-#             currDiff, maxDiff, minDiff = 0, 0, 0
-#             lastChar1Diff, lastChar2Diff
+    for i in range(len(chars)):
+        for j in range(i + 1, len(chars)):
+            char1, char2 = chars[i], chars[j]
+            currDiff, maxDiff, minDiff = 0, 0, 0
+            lastChar1Diff, lastChar2Diff = 0, 0
+            meetChar1, meetChar2 = False, False
+            
+            for char in s:
+                if char == char1:
+                    meetChar1 = True
+                    currDiff += 1
+                    maxDiff = max(lastChar1Diff, maxDiff)
