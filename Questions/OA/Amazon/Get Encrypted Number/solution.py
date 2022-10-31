7986f342126ca0a4b039484c6d99ab47aec0c2b8
doyedele1 <demilade.oyedele@gmail.com>
2022-10-31 13:30:09 +0100

    solved-Amazon-OA

--- Questions/OA/Amazon/Get Encrypted Number/solution.py
+++ Questions/OA/Amazon/Get Encrypted Number/solution.py
@@ -1,18 +1,17 @@
 def getEncryptedNumber(numbers):
-    n = len(arr)
+    n = len(numbers)
     
-    if n < 3:
-        return str(arr)
+    if n < 3: return ''.join(str(i) for i in numbers)
 
     a2 = []
     while n != 2:
         for i in range(0, n-1):
-            d = arr[i] + arr[i + 1]
+            d = numbers[i] + numbers[i + 1]
             d = d % 10
             a2.append(d)
-        arr = a2
-        n = len(arr)
+        numbers = a2
+        n = len(numbers)
         a2 = []
-    return ''.join(str(i) for i in arr)
+    return ''.join(str(i) for i in numbers)
 
-# print(getEncryptedNumber([4, 5, 6, 7]))
+# print(getEncryptedNumber([4, 5]))
