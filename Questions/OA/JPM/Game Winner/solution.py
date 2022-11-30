7ff365eaaba92200a45a139040615282fcb8be93
Demilade Oyedele <95083684+demiladeoyedele@users.noreply.github.com>
2022-11-30 19:52:24 +0100

    Update solution.py

--- Questions/OA/JPM/Game Winner/solution.py
+++ Questions/OA/JPM/Game Winner/solution.py
@@ -2,8 +2,8 @@ def findWinner(s):
     wendyMoves, bobMoves = 0, 0
 
     for i in range(len(s) - 2):
-        if s[i:i+3] == 'www': wendyMoves += 1
-        if s[i:i+3] == 'bbb': bobMoves += 1
+        if s[i:i+3] == "www": wendyMoves += 1
+        if s[i:i+3] == "bbb": bobMoves += 1
 
-    if bobMoves <= wendyMoves: return('wendy')
-    else: return('bob')
+    if wendyMoves > bobMoves: return("wendy")
+    else: return("bob")
