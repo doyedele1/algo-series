e2e9ea1884e9e13fbb6a3f7d3a6f5c5dc9e4a351
Demilade Oyedele <demilade.oyedele@gmail.com>
2022-11-30 20:03:20 +0100

    Update solution.py

--- Questions/OA/JPM/Game Winner/solution.py
+++ Questions/OA/JPM/Game Winner/solution.py
@@ -1,7 +1,7 @@
 def gameWinner(colors):
     wendyMoves, bobMoves = 0, 0
 
-    for i in range(len(s) - 2):
+    for i in range(len(colors) - 2):
         if colors[i:i+3] == "www": wendyMoves += 1
         if colors[i:i+3] == "bbb": bobMoves += 1
 
