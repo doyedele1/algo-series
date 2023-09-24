a7a14c257ee02ca080a88bf8e85db59530853be1
doyedele1 <demilade.oyedele@gmail.com>
2023-09-24 20:08:44 +0100

    solved LC questions

--- Questions/LC Questions/Arrays/381. Insert Delete GetRandom O(1) - Duplicates allowed/solution.py
+++ Questions/LC Questions/Arrays/381. Insert Delete GetRandom O(1) - Duplicates allowed/solution.py
@@ -1,27 +1,52 @@
 '''
     Explanation:
         - We can have two data structures. An array and a hashmap that maps to a set of values
-        - With the array, we can easily choose a random number from it
-        - With the hashmap, we can achieve an insert and removal of O(1)
+            - With the array, we can easily choose a random number from it
+            - With the hashmap, we can achieve an insertion and removal of O(1)
 
-        hashmap --> key = value, value = set of indices
-        array --> contains only the value
+            hashmap --> key = value, value = set of indices
+            array --> contains only the value
 
         Insert operation
-            - Add values-index pair to the hashmap
+            - Add value-indices pair to the hashmap
             - Append value to the end of the array
 
-            - If length of that set in the hashmap is equal to 1, then it means the value was not present in the hashmap before, so return true
+        Remove operation
+            - Use the hashmap to find the index of the element to be removed & remove that index from the set
+            - Use the array to find the last item
 
-        TC - GetRandom function = O(1). 
-            Insert and Delete functions = 
-                O(1) on average
-                O(n) in the worst-case scenario when the operation exceeds the capacity of currently allocated array/hashmap and invokes space reallocation.
+            - In the array, set the value of the removed index to be the last item
+            - In the hashmap, add the removed index to the set of the last item
+
+            - Remove the added removed index from the set
+
+        insert(1)
+            hashMap = {1: {0}}
+            arrayList = [1]
+        
+        insert(1)
+            hashMap = {1: {0, 1}}
+            arrayList = [1, 1]
+
+        insert(2)
+            hashMap = {1: {0,1}, 2: {2}}
+            arrayList = [1, 1, 2]
+        
+        remove(1)
+            index = 0, lastItem = 2
+
+            arrayList = [2, 1, 2]
+            hashMap = {1: {1}, 2: {0,2}}
+
+            hashMap = {1: {1}, 2: {0}}
+            arrayList = [2, 1]
+
+        TC - all functions: O(1)
         SC - O(n) to store the items
 '''
 
 from collections import defaultdict
-from random import randint, choice
+from random import randint
 
 class RandomizedCollection:
     def __init__(self):
@@ -37,7 +62,6 @@ class RandomizedCollection:
     def remove(self, val: int) -> bool:
         if not self.hashMap[val]: return False
         
-        # Get the index of the value to be removed from the hashmap and the last item in the array list
         index, lastItem = self.hashMap[val].pop(), self.arrayList[-1]
         
         self.arrayList[index] = lastItem
@@ -50,7 +74,6 @@ class RandomizedCollection:
         return True
 
     def getRandom(self) -> int:
-        # return choice(self.arrayList)
         randomIndex = randint(0, len(self.arrayList) - 1)
         return self.arrayList[randomIndex]
 
