1b276601a979d224d6cce431c549486548b5e3c9
doyedele1 <demilade.oyedele@gmail.com>
2023-09-27 02:18:34 +0100

    solved LC questions

--- Questions/LC Questions/Arrays/198. House Robber/solution.py
+++ Questions/LC Questions/Arrays/198. House Robber/solution.py
@@ -8,16 +8,25 @@
            3   1           1
         The recurrence relation: rob = max(nums[0] + rob[2:n], rob[1:n])
 
-        1   2   3   1
-        # [house_one, house_two, n, n+1, ...]
-        If we rob house n, we have two choices:
-            Choice 1: n + house_one
-            Choice 2: maximum until house_two
-        
-        If we rob house n+1, we have two choices:
-            Choice 1: (n+1) + maximum until house_two
-            Choice 2: maximum until house n
+        [house_one, house_two, n, n+1, ...]
 
+        Example 1: [1, 2, 3, 1]
+            house_one = 1, house_two = max(1,2) = 2
+            If we rob house n, we have two choices:
+                Choice 1: n + house_one = 3 + 1 = 4
+                Choice 2: maximum until house_two = 2
+            
+            If we rob house n+1, we have two choices:
+                Choice 1: (n+1) + maximum until house_two = 1 + 2 = 3
+                Choice 2: maximum until house n = 4
+
+        Example 2: [2, 7, 9, 3, 1]
+            house_one = 2
+            house_two = max(2,7) = 7
+            house_n = (9+2) or 7 = 11
+            house_n+1 = (3+7) or (11) = 11
+            house_n+2 = (1+11) or (11) = 12
+        
     TC: O(n)
     SC: O(1)
 '''
