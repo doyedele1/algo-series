df442a19eefe434dbc95d6f508f160497e678a58
doyedele1 <demilade.oyedele@gmail.com>
2023-10-22 18:46:36 +0100

    solving LC question

---
+++ Questions/LC Questions/Arrays/740. Delete and Earn/solution.py
@@ -0,0 +1,22 @@
+'''
+    Explanation:
+    
+
+'''
+
+
+from typing import List
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        arrNums = [0] * (max(nums) + 1)
+        
+        for num in nums:
+            arrNums[num] += num
+
+        e1, e2 = 0, 0
+        for num in arrNums:
+            currEarn = max(e2, num + e1)
+            e1 = e2
+            e2 = currEarn
+        return e2
