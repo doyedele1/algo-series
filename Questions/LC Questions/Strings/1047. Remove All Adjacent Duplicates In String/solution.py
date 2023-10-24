2b546af7369066f9c5a8b11d1e9612c2a8a28559
doyedele1 <demilade.oyedele@gmail.com>
2023-10-24 13:06:01 +0100

    solved LC question

--- Questions/LC Questions/Strings/1047. Remove All Adjacent Duplicates In String/solution.py
+++ Questions/LC Questions/Strings/1047. Remove All Adjacent Duplicates In String/solution.py
@@ -1,3 +1,23 @@
+'''
+    Explanation:
+        - We will have a stack to keep track of the characters.
+        - We will iterate through the string and check if the current character is already in the stack.
+            - If it is, we will pop the last character from the stack.
+            - If it is not, we will append the current character to the stack.
+        - We will return the string after joining the stack.
+
+        "abbaca"
+        stack = [a]
+        stack = [ab]
+        stack = [a]
+        stack = []
+        stack = [c]
+        stack = [ca]
+
+        TC - O(n) where n is the length of the string
+        SC - O(n-d) where d is the total length of all duplicates
+'''
+
 class Solution:
     def removeDuplicates(self, s: str) -> str:
         stack = []
@@ -6,24 +26,3 @@ class Solution:
             if stack and stack[-1] == char: stack.pop()
             else: stack.append(char)
         return "".join(stack)
-    
-
-    '''
-        Explanation:
-            - We will have a stack to keep track of the characters.
-            - We will iterate through the string and check if the current character is already in the stack.
-                - If it is, we will pop the last character from the stack.
-                - If it is not, we will append the current character to the stack.
-            - We will return the string after joining the stack.
-            
-            - TC - O(n) where n is the length of the string
-            - SC - O(n-d) where d is the total length of all duplicates
-
-            "abbaca"
-            stack = [a]
-            stack = [ab]
-            stack = [a]
-            stack = []
-            stack = [c]
-            stack = [ca]
-    '''
