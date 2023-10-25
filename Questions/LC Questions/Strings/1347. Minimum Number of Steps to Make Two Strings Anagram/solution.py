ad8b8e31b39a2e0e345d48787ab3146cbb2e3780
doyedele1 <demilade.oyedele@gmail.com>
2023-10-25 01:36:58 +0100

    solved LC question

--- Questions/LC Questions/Strings/1347. Minimum Number of Steps to Make Two Strings Anagram/solution.py
+++ Questions/LC Questions/Strings/1347. Minimum Number of Steps to Make Two Strings Anagram/solution.py
@@ -1,47 +1,40 @@
 '''
     Explanation:
-        - Store the occurence of characters in s in a hashmap or array
-        - Reduce the occurence of each character seen in s in t by 1
-        - If the frequency of a character is more than 0, i.e. the characters left are stopping the two strings becomes anagram of each other,
-            - Then add the counts together
-            
         - "bab", "aba"
-            counts = {b: 2, a: 1}
-            - Checking t,
-                First character: counts = {b: 2, a: 0}
-                Second character: counts = {b: 1, a: 0}
-                Third character: counts = {b: 1, a: -1}
-            - count of b is 1, so res += 1 which is finally 1
+            sFreq = {b:2, a:1}
+            tFreq = {a:2, b:1}
+            - Checking sFreq.items() which is [('b', 2), ('a', 1)],
+                First iteration - b:
+                    Is b in tFreq and count of b in sFreq is greater than the count of b in tFreq? Yes, then add the difference to res
+                    Else if b is not in tFreq, add count of b in sFreq to res
+
+                    count = 2 - 1 = 1
+                    res = 1
+
+                Second iteration - a:
+                    a is in tFreq but the count of a in sFreq is less than the count of a in tFreq
+                    We don't run the loop since the conditions fail, hence res is still 1
             
-        - TC: O(n)
-        - SC: O(n) for the hashmap solution and O(1) for the array solution
+        TC: O(n)
+        SC: O(m+n)
 '''
 
-# Using a fixed array of size 26
-class Solution1:
-    def minSteps(self, s: str, t: str) -> int:
-        res = 0
-        counts = [0] * 26
-        
-        for i in range(len(s)):
-            counts[ord(s[i]) - ord('a')] += 1
-            counts[ord(t[i]) - ord('a')] -= 1
-        
-        for count in counts:
-            if count > 0: res += count
-        return res
+from collections import defaultdict
 
-# Using a hashmap
-class Solution2:
+class Solution:
     def minSteps(self, s: str, t: str) -> int:
+        sFreq, tFreq = defaultdict(int), defaultdict(int)
+        
         res = 0
-        counts = dict()
-        
         for char in s:
-            if char in counts: counts[char] += 1
-            else: counts[char] = 1
-        
+            sFreq[char] += 1
         for char in t:
-            if char in counts and counts[char] > 0: counts[char] -= 1
-            else: res += 1
+            tFreq[char] += 1
+
+        for char, count in sFreq.items():
+            if char not in tFreq:
+                res += count
+            elif char in tFreq and count > tFreq[char]:
+                res += (count - tFreq[char])
+
         return res
