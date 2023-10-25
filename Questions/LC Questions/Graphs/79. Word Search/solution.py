af591288d4836c717799c62c671197f35f8e8d66
doyedele1 <demilade.oyedele@gmail.com>
2023-10-25 23:55:47 +0100

    solved LC question

--- Questions/LC Questions/Graphs/79. Word Search/solution.py
+++ Questions/LC Questions/Graphs/79. Word Search/solution.py
@@ -13,34 +13,38 @@
 
 from typing import List
 
-# Modifying visited character to a value of None - This is very fast on LC
+# Modifying visited character to a value of # - This is very fast on LC
 class Solution1:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
         
         def dfs(row, col, index):
             # If the index is at the length of word, it means we've found a word
-            if index == len(word): return True
+            if index == len(word): 
+                return True
             
-            # We are not out of bounds and we see a character we are looking for
-            elif 0 <= row < rows and 0 <= col < cols and board[row][col] == word[index]:
-                temp = board[row][col]
-                board[row][col] = None
+            # If we go out of bounds, or if we found the wrong character
+            if (row < 0 or row >= rows or col < 0 or col >= cols or word[index] != board[row][col]):
+                return False
+            
+            temp = board[row][col] 
+            board[row][col] = '#'
                 
-                # If any of the directions return true, we return true as our result
-                if dfs(row + 1, col, index + 1) or dfs(row - 1, col, index + 1) or dfs(row, col + 1, index + 1) or dfs(row, col - 1, index + 1):
-                    return True
-
-                board[row][col] = temp
+            up = dfs(row - 1, col, index + 1)
+            down = dfs(row + 1, col, index + 1)
+            left = dfs(row, col - 1, index + 1)
+            right = dfs(row, col + 1, index + 1)
+                
+            board[row][col] = temp
             
-            return False
+            return up or down or left or right
             
         for r in range(rows):
             for c in range(cols):
                 if dfs(r, c, 0): return True
         return False
 
-# Using hash set to keep track of visited characters - Doesn't submit sometimes on LC
+# Using hash set to keep track of visited characters
 class Solution2:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
@@ -48,19 +52,26 @@ class Solution2:
         
         def dfs(row, col, index):
             # If the index is at the length of word, it means we've found a word
-            if index == len(word): return True
+            if index == len(word):
+                return True
             
             # If we go out of bounds, or if we found the wrong character, or if the (row, col) position we are at is in the hash set- which means we are visiting the position twice
-            if (row < 0 or row >= rows or col < 0 or col >= cols or word[index] != board[row][col] or (row, col) in visited): return False
+            if (row < 0 or row >= rows or col < 0 or col >= cols or word[index] != board[row][col] or (row, col) in visited):
+                return False
             
             visited.add((row, col))
             # run the recursive function in all 4 adjacent positions
-            res = dfs(row - 1, col, index + 1) or dfs(row + 1, col, index + 1) or dfs(row, col - 1, index + 1) or dfs(row, col + 1, index + 1)
+            up = dfs(row - 1, col, index + 1)
+            down = dfs(row + 1, col, index + 1)
+            left = dfs(row, col - 1, index + 1)
+            right = dfs(row, col + 1, index + 1)
+            res = up or down or left or right
             # since we are no longer visiting that position, we don't visit that position again, so we can remove it
             visited.remove((row, col))
             return res
         
         for r in range(rows):
             for c in range(cols):
-                if dfs(r, c, 0): return True
+                if dfs(r, c, 0):
+                    return True
         return False
