e62c6806bb5b63a21dee68685b4eab1f90c6bb13
doyedele1 <demilade.oyedele@gmail.com>
2023-10-25 01:11:39 +0100

    solved LC question

--- Questions/LC Questions/Arrays/1029. Two City Scheduling/solution.py
+++ Questions/LC Questions/Arrays/1029. Two City Scheduling/solution.py
@@ -3,15 +3,13 @@
         - We have to pick equal number of cities A and B
         Initially,
         - Can we see if we can send all candidates to city A?
-        - Now, which of the candidates can we send to B? We will pick the candidate for which we gained the maximum (the most negative number)
-        
-        --> (A-B) = by sending person to city A, the company would lose A-B
+        - Now, which of the candidates can we send to B?
 
         [[10,20],[30,200],[400,50],[30,20]]
         -10, -170, 350, 10
-        Sorting returns (-170, -10, 10, 350)
+        Sorting this, we get (-170, -10, 10, 350)
+        costs after sorting = [[30, 200], [10, 20], [30, 20], [400, 50]]
 
-        
         TC - O(nlogn) because of sorting the input array
         SC - O(n). Timsort algorithm is used in Python; which is a combination of Merge Sort and Insertion Sort
 '''
@@ -21,12 +19,10 @@ from typing import List
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         costs = sorted(costs, key = lambda x: x[0] - x[1])
-        # costs after sorting = [[30, 200], [10, 20], [30, 20], [400, 50]]
         
         res = 0
         n = len(costs) // 2
         
         for i in range(n):
-            res += costs[i][0] + costs[i + n][1]
-            
+            res += (costs[i][0] + costs[i + n][1])
         return res
