765b84293fd288daa48f6165d82e4ce0b0e7ed01
doyedele1 <demilade.oyedele@gmail.com>
2024-01-16 22:05:36 +0000

    solved-LC-question

--- Questions/LC Questions/Arrays/217. Contains Duplicate/solution.py
+++ Questions/LC Questions/Arrays/217. Contains Duplicate/solution.py
@@ -1,14 +1,15 @@
 '''
-    Solution 1: Nested loops - TC: O(n-squared), SC: O(1)
-
-    Solution 2: Sort, then check for adjacent values - TC: O(n logn), SC: O(1)
+    Explanation I:
+        Sort, then check for adjacent values
+        TC: O(n logn), SC: O(1)
     
-    Solution 3: Use hashset and check if a certain value exists
-        [1, 2, 3, 1]
-        Is the first 1 in our hashset, then we add to the hashset. Hashset: {1}
-        Is 2 in our hashset? Hashset = {1, 2}
-        Is 3 in our hashset? Hashset = {1, 2, 3}
-        Is 1 in our hashset, yes, then we return True
+    Explanation II: 
+        Use hashset and check if a certain value exists
+            [1, 2, 3, 1]
+            Is 1 in our hashset, no, then we add to the hashset. numsSet = {1}
+            Is 2 in our hashset, no, then we add to the hashset? numsSet = {1, 2}
+            Is 3 in our hashset, no, then we add to the hashset? numsSet = {1, 2, 3}
+            Is 1 in our hashset, yes, then we return True
 
         TC: O(n), SC: O(n)
 '''
@@ -17,21 +18,9 @@ from typing import List
 
 class Solution3:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashSet = set()
+        numsSet = set()
         
         for num in nums:
-            if num in hashSet: return True
-            hashSet.add(num)
-        return False
-
-class Solution4:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        freqCounter = {}
-        
-        for i in range(len(nums)):
-            freqCounter[nums[i]] = 1 + freqCounter.get(nums[i], 0)
-        
-        for num in freqCounter:
-            if freqCounter[num] > 1: return True
-            
+            if num in numsSet: return True
+            numsSet.add(num)
         return False
