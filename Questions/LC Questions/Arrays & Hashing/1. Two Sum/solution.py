1efd4f2240bdae4484a93ea2597ecb3abfb43363
doyedele1 <demilade.oyedele@gmail.com>
2024-01-18 22:50:39 +0000

    solved-LC-question

--- Questions/LC Questions/Arrays & Hashing/1. Two Sum/solution.py
+++ Questions/LC Questions/Arrays & Hashing/1. Two Sum/solution.py
@@ -1,11 +1,13 @@
 '''
+    Explanation I:
+        TC: O(n-squared), SC: O(1)
+
     Explanation II:
         Use a hashmap and map the value to its index
         [2,7,11,15], target = 9
         nums_map = {}
         
         First iteration, diff = 7, nums_map = {2:0}
-
         Second iteration, diff = 2, diff is in nums_map, then return the indices
 
         TC: O(n), SC: O(n)
@@ -13,11 +15,9 @@
 
 from typing import List
 
-# TC: O(n-squared), SC: O(1)
 class Solution1:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(nums) - 1
+        i, j = 0, len(nums) - 1
         
         for i in range(0, len(nums)):
             for j in range(i+1, len(nums)):
