aee2a6cc6a146e8350048b04db223d864c18de33
doyedele1 <demilade.oyedele@gmail.com>
2024-01-19 23:40:51 +0000

    code-cleanup

--- Questions/LC Questions/Arrays/238. Product of Array Except Self/solution.py
+++ Questions/LC Questions/Arrays/238. Product of Array Except Self/solution.py
@@ -2,25 +2,32 @@
     Explanation I:
         [1, 2, 3, 4]
         - We could use left and right arrays
-            - left rep the product of elem to the left and right represents the product of elem to the right
-            - left = [], right = []
-            - After all iterations: left = [1, 1, 2, 6]
-            - After all iterations: right = [24, 12, 4, 1]
-            - res = left[i] * right[i] = [24, 12, 8, 6]
+        - left represents the product of numbers to the left and right represents the product of numbers to the right
+        left = [], right = []
+        - After first loop, left = [1, 1, 2, 6]
+        - After second loop, right = [24, 12, 4, 1]
+        - res = left[i] * right[i] = [24, 12, 8, 6]
         
-        - TC: O(n)
-        - SC: O(n), space occupied by the left and right arrays
+        TC: O(n)
+        SC: O(n), space occupied by the left and right arrays
         
     Explanation II:
-        - We could do the left and right arrays operation inside the output array
-        - [1, 2, 3, 4]
-        - At each iteration,
-            - left = [1, 1, 2, 6] which is res after the left operation
-            - right = 24, 12, 4, 1 gotten from the input array
-            - res = [1, 1, 2, 6] and res after the right operation = [1 * 24, 1 * 12, 2 * 4, 6 * 1] = [24, 12, 8, 6]
+        - We could do the left and right arrays operation inside the res array
+        [1, 2, 3, 4]
+        - At first loop,
+            - res = left = [1, 1, 2, 6]
+        - At second loop,
+            First iteration,
+                res = [1, 1, 2, 6], right = 1 * 4 = 4
+            Second iteration,
+                res = [1, 1, 8, 6], right = 4 * 3 = 12
+            Third iteration,
+                res = [1, 12, 8, 6], right = 12 * 2 = 24
+            Fourth iteration,
+                res = [24, 12, 8, 6], right = 24 * 1 = 24
         
-        - TC: O(n)
-        - SC: O(1) since the res doesn't count as extra space
+        TC: O(n)
+        SC: O(1)
 '''
 
 from typing import List
@@ -30,10 +37,10 @@ class Solution1:
         left, right, res = [1] * len(nums), [1] * len(nums), [1] * len(nums)
         
         for i in range(1, len(nums)):
-            left[i] = nums[i - 1] * left[i - 1]
+            left[i] = left[i - 1] * nums[i - 1]
         
         for i in range(len(nums) - 2, -1, -1):
-            right[i] = nums[i + 1] * right[i + 1]
+            right[i] = right[i + 1] * nums[i + 1]
         
         for i in range(len(nums)):
             res[i] = left[i] * right[i]
@@ -43,10 +50,8 @@ class Solution2:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * len(nums)
         
-        left = 1
-        for i in range(len(nums)):
-            res[i] = left
-            left *= nums[i]
+        for i in range(1, len(nums)):
+            res[i] = res[i - 1] * nums[i - 1]
         
         right = 1
         for i in range(len(nums) - 1, -1, -1):
@@ -59,27 +64,30 @@ class Solution2:
 class Solution3:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # [1, 2, 3, 4, 5]
-        # [120, 60, 40, 30, 24]
         # prefixProduct = [1, 2, 6, 24, 120]
         # suffixProduct = [120, 120, 60, 20, 5]
-        lenNums = len(nums)
-        prefixProduct = [0 for i in range(lenNums)]
-        suffixProduct = [0 for i in range(lenNums)]
-        result = [0 for i in range(lenNums)]
+        # res = [1 * 120, 1 * 60, 2 * 20, 6 * 5, 24 * 1]
+        # res = [120, 60, 40, 30, 24]
 
-        for ind, num in enumerate(nums):
-            if ind == 0:
-                prefixProduct[ind] = num
+        prefixProduct = [0] * len(nums)
+        suffixProduct = [0] * len(nums)
+        result = [0] * len(nums)
+
+        for i, num in enumerate(nums):
+            if i == 0:
+                prefixProduct[i] = num
             else:
-                prefixProduct[ind] = prefixProduct[ind - 1] * num
-        for ind in range(lenNums - 1, -1, -1):
-            if ind == lenNums - 1:
-                suffixProduct[ind] = nums[ind]
+                prefixProduct[i] = prefixProduct[i - 1] * num
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i == len(nums) - 1:
+                suffixProduct[i] = nums[i]
             else:
-                suffixProduct[ind] = nums[ind] * suffixProduct[ind + 1]
-        for ind, num in enumerate(nums):
-            left = prefixProduct[ind - 1] if ind > 0 else 1
-            right = suffixProduct[ind + 1] if ind < lenNums - 1 else 1
-            result[ind] = left * right
+                suffixProduct[i] = suffixProduct[i + 1] * nums[i]
+
+        for i, num in enumerate(nums):
+            left = prefixProduct[i - 1] if i > 0 else 1
+            right = suffixProduct[i + 1] if i < len(nums) - 1 else 1
+            result[i] = left * right
 
         return result
