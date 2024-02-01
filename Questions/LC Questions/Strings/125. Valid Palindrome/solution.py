916ecce46c2ee738af32d3ad8f2356ada533553f
doyedele1 <demilade.oyedele@gmail.com>
2024-02-01 22:37:05 +0000

    solved-LC-question

--- Questions/LC Questions/Strings/125. Valid Palindrome/solution.py
+++ Questions/LC Questions/Strings/125. Valid Palindrome/solution.py
@@ -1,30 +1,41 @@
 # alphanumeric characters - A to Z, a to z, 0 to 9
 
 # TC: O(n), SC: O(n) [newStr and the reveresed newStr space]
-class Solution1:
-    def checkAlphanumeric(self, char):
-        return (ord('A') <= ord(char) <= ord('Z') or ord('a') <= ord(char) <= ord('z') or ord('0') <= ord(char) <= ord('9'))
-    
+class Solution1:   
     def isPalindrome(self, s: str) -> bool:
+        def isAlphaNum(c):
+            return (ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9'))
+        
         s = s.lower()
         newStr = ""
-        for char in s:
-            if self.checkAlphanumeric(char):
-                newStr += char
+
+        for c in s:
+            if isAlphaNum(c):
+                newStr += c
         return newStr == newStr[::-1]
 
-# TC: O(n) where n is the length of the input string, SC: O(1)
+# TC: O(n), SC: O(1)
 class Solution2:
     def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
-        
-        while left < right:
-            while not self.checkAlphanumeric(s[left]) and left < right: # left < right - the left pointer doesn't pass the right pointer and it doesn't go out of bounds
-                left += 1
-            while not self.checkAlphanumeric(s[right]) and left < right:
-                right -= 1
-            if s[left].lower() != s[right].lower(): return False
-            left += 1
-            right -= 1
+        i, j = 0, len(s) - 1
+
+        def isAlphaNum(c):
+            return (ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9'))   
+
+        while i < j:
+            # skip if it is a special character
+            while i < j and not isAlphaNum(s[i]):
+                i += 1
+            
+            # skip if it is a special character
+            while j > i and not isAlphaNum(s[j]):
+                j -= 1
+
+            # if characters are both ends are not the same, just return False
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i += 1
+            j -= 1
+
         return True
