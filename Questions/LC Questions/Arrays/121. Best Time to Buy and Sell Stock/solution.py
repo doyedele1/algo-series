104656eba0e2bb6723ec7ab6a95ee7bc177a154c
doyedele1 <demilade.oyedele@gmail.com>
2024-02-05 22:53:05 +0000

    solved-LC-question

--- Questions/LC Questions/Arrays/121. Best Time to Buy and Sell Stock/solution.py
+++ Questions/LC Questions/Arrays/121. Best Time to Buy and Sell Stock/solution.py
@@ -1,15 +1,11 @@
 '''
-    Note: Buy Low, Sell High
+    BUY LOW, SELL HIGH
 
     Explanation I: TC: O(n), SC: O(1)
         - Use two pointers. Left starts from index 0, right starts from index 1. Left represents buy, right represents sell
-        - Initialize maxProfit as 0
-        - While the right pointer is in bounds,
-            - If buy price < sell price, then we have a potential maximum profit
-            - Else, move the left pointer to where the right pointer is
-            - Also, continually move the right pointer
-        - Return maxProfit
-
+        - Initialize res as 0
+        - 
+        - Return res
 
     Explanation II: TC: O(n), SC: O(1)
         [7,1,5,3,6,4]
@@ -26,26 +22,24 @@ from typing import List
 
 class Solution1:
     def maxProfit(self, prices: List[int]) -> int:
-        left, right = 0, 1
-        maxProfit = 0
-        
-        while right < len(prices):
-            if prices[left] < prices[right]:
-                profit = prices[right] - prices[left]
-                maxProfit = max(maxProfit, profit)
-            else: left = right    
-            right += 1
-        return maxProfit
+        start = 0
+        res = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] < prices[start]:
+                start = i
+            res = max(res, prices[i] - prices[start])
+        return res
 
 class Solution2:
     def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
+        res = 0
         minPrice = float("inf")
         
         for i in range(len(prices)):
             price = prices[i]
             if price < minPrice: # we are at loss here
                 minPrice = price
-            elif price - minPrice > maxProfit: # there is a potential profit
-                maxProfit = price - minPrice
-        return maxProfit
+            elif price - minPrice > res: # there is a potential profit
+                res = price - minPrice
+        return res
--- Questions/LC Questions/Arrays/704. Binary Search/solution.py
+++ Questions/LC Questions/Arrays/704. Binary Search/solution.py
@@ -1,11 +1,16 @@
-class Solution:
-    def search(self, nums, target):
-        i = 0
-        j = len(nums) - 1
+from typing import List
 
-        while i <= j:
-            middle = (i + j) // 2
-            if nums[middle] == target: return middle
-            elif nums[middle] < target: i = middle + 1
-            else: j = middle - 1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            # because 2^31 + 2^31 could cause an overflow
+            mid = l + ((r - l) // 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
         return -1
