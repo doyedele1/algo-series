899b52192123a72d7a1faaa5b1c036148aef80d9
doyedele1 <demilade.oyedele@gmail.com>
2024-02-05 23:20:56 +0000

    solved-LC-question

--- Questions/LC Questions/Arrays/121. Best Time to Buy and Sell Stock/solution.py
+++ Questions/LC Questions/Arrays/121. Best Time to Buy and Sell Stock/solution.py
@@ -1,21 +1,57 @@
 '''
     BUY LOW, SELL HIGH
+    [7,1,5,3,6,4]
 
-    Explanation I: TC: O(n), SC: O(1)
-        - Use two pointers. Left starts from index 0, right starts from index 1. Left represents buy, right represents sell
-        - Initialize res as 0
-        - 
-        - Return res
+    Explanation: TC: O(n), SC: O(1)
+        Visualizing first
+            .
+                            .
+                    .
+                                .
+                        .
 
-    Explanation II: TC: O(n), SC: O(1)
-        [7,1,5,3,6,4]
-        - Initialize max_profit variable as 0
+                .
+
+        First iteration:
+            Is 1 < 7, yes, then start = 1
+            res = 0
+        Second iteration,
+            Is 5 < 1, no
+            res = 4
+        Third iteration,
+            Is 3 < 1, no
+            res = 4
+        Fourth iteration,
+            Is 6 < 1, no
+            res = 5
+        Fifth iteration,
+            Is 4 < 1, no
+            res = 5
+
+    Another possible solution:
+        - Initialize res variable as 0
         - Initialize min_price as infinity which is the largest price there can ever be
+        - Ensure you know what the min_price is at every iteration
+        - And compare the price with previous min_price to get the profit
         
-        - Iterations:
-            - Ensure you know what the min_price is at every iteration
-            - And compare the price with previous min_price to get the profit
-            - The iteration with the largest profit should be obtained and returned
+        [7,1,5,3,6,4]
+
+        First iteration,
+            Is 7 < infinity, yes, then minPrice = 7
+        Second iteration,
+            Is 1 < 7, yes, then minPrice = 1
+        Third iteration,
+            Is 5 < 1, no.
+            Is 5 - 1 > 4, yes, res = 4
+        Fourth iteration,
+            Is 3 < 1, no
+            Is 3 - 1 > 4, no
+        Fifth iteration,
+            Is 6 < 1, no
+            Is 6 - 1 > 4, yes, res = 5
+        Sixth iteration,
+            Is 4 < 1, no
+            Is 4 - 1 > 5, no
 '''
 
 from typing import List
