de9567faa013b4fbbe6a59aafe512173808aca2f
doyedele1 <demilade.oyedele@gmail.com>
2024-02-12 22:03:31 +0000

    solved-LC-question

--- Questions/LC Questions/Linked List/21. Merge Two Sorted Lists/solution.py
+++ Questions/LC Questions/Linked List/21. Merge Two Sorted Lists/solution.py
@@ -1,13 +1,13 @@
 '''
     Explanation:
-        - Initialize a dummy new node
-        - And save your new node in a temporary node
-        - Loop through the two lists until both have null values
-            - If l1 < l2, add l1 to the temp node and move l1 pointer
-            - Else add l2 and move l2 pointer
+        - Initialize a dummy node
+        - And save your dummy node in a temp node
+        - Loop through the two lists when both are not null
+            - If list1 < list2, add list1 to the temp node and move list1 pointer
+            - Else, add list2 and move list2 pointer
             - Move your temp node pointer 
-        - If l2 is not null, add l2 to the temp node
-        - If l1 is not null, add l1 to the temp node
+        - If list2 is not null, add list2 to the temp node
+        - If list1 is not null, add list1 to the temp node
 
         TC: O(n + m) where n + m is the sum of the lengths of the two lists
         SC: O(1). The iterative approach only allocates a few pointers, so it has a constant overall memory footprint. newNode output is returning the head, not the entire list
@@ -21,7 +21,7 @@ class ListNode:
         self.val = val
         self.next = next
 
-class Solution:
+class Solution2:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummyNode = ListNode(-1)
         temp = dummyNode
