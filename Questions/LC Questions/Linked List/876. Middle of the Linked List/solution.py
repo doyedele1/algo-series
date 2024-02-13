a19c2201b2174e9ef7e2ca9c8e737dc41aa0c764
doyedele1 <demilade.oyedele@gmail.com>
2024-02-13 23:22:27 +0000

    solved-LC-question

--- Questions/LC Questions/Linked List/876. Middle of the Linked List/solution.py
+++ Questions/LC Questions/Linked List/876. Middle of the Linked List/solution.py
@@ -1,42 +1,65 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+'''
+    Explanation: If length of linked list is even
+        1 -> 2 -> 3 -> 4 -> 5 -> 6
 
-# Simpler solution
-class Solution:
-    def middleNode(self, head):
+        After first iteration:
+            1 -> 2 -> 3 -> 4 -> 5 -> 6
+                 s
+                      f
         
-        slow = head
-        fast = head
+        After second iteration:
+            1 -> 2 -> 3 -> 4 -> 5 -> 6
+                      s
+                                f
         
-        while(fast != None and fast.next != None):
+        After third iteration:
+            1 -> 2 -> 3 -> 4 -> 5 -> 6
+                           s
+                                            f
+        
+        Return slow pointer node = node of 4
+
+        TC: O(n)
+        SC: O(1)
+'''
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution1:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         return slow
 
-# Another solution  
-# class Solution:
-#     def findLength(self, ll):
-#         length = 0
+# Faster solution on LeetCode
+class Solution2:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def findLength(ll):
+            length = 0
+            
+            while ll:
+                ll = ll.next
+                length += 1
+            return length
+    
+    
+        slow = fast = head
+    
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
         
-#         while(ll != None):
-#             ll = ll.next
-#             length += 1
-#         return length
-    
-    
-#     def middleNode(self, head):
-#         slow = head
-#         fast = head
-    
-#         while(fast.next != None and fast.next.next != None):
-#                 slow = slow.next
-#                 fast = fast.next.next
-        
-#         modulo = self.findLength(head)
-#         modulo %= 2
-#         if(modulo == 1):
-#             return slow
-#         else: return slow.next
+        modulo = findLength(head)
+        modulo %= 2
+        if modulo == 1:
+            return slow
+        else:
+            return slow.next
