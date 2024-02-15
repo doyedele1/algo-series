0580f4f6b2bb2023db54595e95435460e19ddaf1
doyedele1 <demilade.oyedele@gmail.com>
2024-02-15 23:51:54 +0000

    solved-LC-question

--- Questions/LC Questions/Linked List/19. Remove Nth Node from End of List/solution.py
+++ Questions/LC Questions/Linked List/19. Remove Nth Node from End of List/solution.py
@@ -1,3 +1,45 @@
+'''
+    Explanation:
+        1 -> 2 -> 3 -> 4 -> 5, n = 2
+
+        - Let's use two pointers left and right
+            1 -> 2 -> 3 -> 4 -> 5
+        l
+            r
+        
+        - Move the right pointer to allow both points be exactly separated by n nodes apart
+            First iteration
+                1 -> 2 -> 3 -> 4 -> 5
+            l
+                    r
+            n = 1
+
+            Second iteration
+                1 -> 2 -> 3 -> 4 -> 5
+            l
+                          r
+            n = 0
+
+        - Move both left and right pointers while right is not None to allow the left pointer get to the node we want to delete
+            First iteration
+                1 -> 2 -> 3 -> 4 -> 5
+                l
+                               r
+            Second iteration
+                1 -> 2 -> 3 -> 4 -> 5
+                     l
+                                    r
+            Third iteration
+                1 -> 2 -> 3 -> 4 -> 5
+                          l
+                                         r
+        
+        - We have a next pointer which is the nth node from the end that is to be deleted
+            l.next = l.next.next
+            then just return dummy.next
+
+        TC: O(n), SC: O(1)            
+'''
 from typing import Optional
 
 # Definition for singly-linked list.
