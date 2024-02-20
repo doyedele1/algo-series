269717c2ab2358b2d000d7307e1223032e9f743f
doyedele1 <demilade.oyedele@gmail.com>
2024-02-20 23:51:28 +0000

    solved-LC-question

--- Questions/LC Questions/Trees/314. Binary Tree Vertical Order Traversal/solution.py
+++ Questions/LC Questions/Trees/314. Binary Tree Vertical Order Traversal/solution.py
@@ -1,10 +1,12 @@
 '''
-    Explanation:
-        - Hashmap of table- key (column): value(node.value)
-        - Queue- initially: [(root, 0)]
-        
-        - TC: O(n) where n is the number of nodes in the tree; we traversed each nodes once and only once
-        - SC: O(n) where n is the number of nodes in the tree; 
+    Explanation II:
+        - Hashmap of columnTable
+            key = column
+            value = list of node.value
+
+        - Initial queue = [(root, 0)]
+       
+        SC: O(n) where n is the number of nodes in the tree
             Hash table: We used a hash table to group nodes on the same column. The values would consume O(n) memory and the space of the key in the worst is when each node has a unique column index and there are as many keys as the values.
             Queue: In a full binary tree, the maximum number of nodes at a level is (n+1 / 2) and the queue can hold no more two levels. (n+1 / 2) * 2 = O(n+1), amortized is O(n)
             Result: Reordering the hash table and creating an array from it is O(n)
@@ -19,6 +21,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# TC: O(nlogn) when the binary tree is extremely imbalanced- each node has only left child node, SC: O(n)
 class Solution1:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
@@ -38,23 +41,25 @@ class Solution1:
         
         return [columnTable[x] for x in sorted(columnTable.keys())]
 
+#  TC: O(n), SC: O(n)
 class Solution2:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None: return root
+        if not root: 
+            return []
         
-        table = defaultdict(list)
-        minColumn  = maxColumn = 0
+        columnTable = defaultdict(list)
+        minColumn, maxColumn = 0, 0
         q = deque([(root,0)])
 
         while q:
             node, column = q.popleft()
 
-            if node is not None:
-                table[column].append(node.val)
+            if node:
+                columnTable[column].append(node.val)
                 minColumn = min(minColumn, column)
                 maxColumn = max(maxColumn, column)
     
                 q.append((node.left, column - 1))
                 q.append((node.right, column + 1))
             
-        return [table[x] for x in range(minColumn, maxColumn + 1)]
+        return [columnTable[x] for x in range(minColumn, maxColumn + 1)]
