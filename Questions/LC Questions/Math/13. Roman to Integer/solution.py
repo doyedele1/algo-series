dc1a7d03ff7aa3678279aa17087b51537d24419f
doyedele1 <demilade.oyedele@gmail.com>
2024-02-20 08:04:25 +0000

    solved-LC-question

--- Questions/LC Questions/Math/13. Roman to Integer/solution.py
+++ Questions/LC Questions/Math/13. Roman to Integer/solution.py
@@ -1,24 +1,20 @@
 '''
-Explanation:
-    - When the lower roman numeral comes before the higher one, we subtract the lower equivalent integer from the higher equivalent integer to get the result. 
-    - While, when the higher roman numeral comes before the lower one, we can add the equivalent integer of the higher roman numeral to the result.
-    
-    Example:
-    MCMXCIV
-    First iteration: M and C. We add 1000 to the result, res = 1000
-    Second iteration: C and M. We subtract 100 from the result, res = 900
-    Third iteration: M and X. We add 1000 to the result, res = 1900
-    Fourth iteration: X and C. We subtract 10 from the result, res = 1890
-    Fifth iteration: C and I. We add 100 to the result, res = 1990
-    Sixth iteration: I and V. We subtract 1 from the result, res = 1989
-    Seventh iteration: V. We add 5 to the result, res = 1994
+    Explanation:
+        - When the lower roman numeral comes before the higher one, we subtract the lower equivalent from the result
+        - When the higher roman numeral comes before the lower one, we add the higher equivalent to the result
+        
+        MCMXCIV
+        First iteration: M and C. We add 1000 to the result, res = 1000
+        Second iteration: C and M. We subtract 100 from the result, res = 900
+        Third iteration: M and X. We add 1000 to the result, res = 1900
+        Fourth iteration: X and C. We subtract 10 from the result, res = 1890
+        Fifth iteration: C and I. We add 100 to the result, res = 1990
+        Sixth iteration: I and V. We subtract 1 from the result, res = 1989
+        Seventh iteration: V. We add 5 to the result, res = 1994
 
-
-    Complexity analysis:
-    - TC - O(n). We only need to iterate through the given string of roman numerals and perform the algorithm explained above.
-    - SC - O(1). The hashmap created uses a constant space. The length of the conversion from roman numerals to integer doesn't vary.
+        TC - O(n). We only need to iterate through the given string of roman numerals and perform the algorithm explained above.
+        SC - O(1). The hashmap created uses a constant space
 '''
-
 
 class Solution:
     def romanToInt(self, s: str) -> int:
