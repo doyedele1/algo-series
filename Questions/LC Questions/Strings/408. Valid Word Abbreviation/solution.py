e9bd9a4c9940958b801573dea06acd71af17a1cc
doyedele1 <demilade.oyedele@gmail.com>
2024-02-21 00:52:50 +0000

    solved-LC-question

--- Questions/LC Questions/Strings/408. Valid Word Abbreviation/solution.py
+++ Questions/LC Questions/Strings/408. Valid Word Abbreviation/solution.py
@@ -1,7 +1,20 @@
 '''
     Explanation:
+        word = "internationalization", abbr = "i12iz4n"
 
-        TC: O(n)
+        - Use two pointers for word and abbr
+
+        - If abbr[abbrPointer] is number,
+            - Check if abbr[abbrPointer] is zero (0). Return false if it is
+            - Find the steps it would take for 12 characters to be viewed in word, then move the wordPointer by steps time
+
+        - Else: abbr[abbrPointer] is letter,
+            - Then check if both word[wordPointer] and abbr[abbrPointer],
+                return false if they are not equal, else move both pointers once at a time
+
+        - If the wordPointer = len(word) and the abbrPointer = len(abbr), that means we've reached the end of the process and we can return True
+
+        TC: O(n). Worst case -> when the word and abbr are the same. i.e. no substitution has been done
         SC: O(1)
 '''
 
@@ -15,7 +28,6 @@ class Solution:
                     return False
     
                 steps = 0
-
                 while abbrPointer < len(abbr) and abbr[abbrPointer].isdigit():
                     steps = steps * 10 + int(abbr[abbrPointer])
                     abbrPointer += 1
