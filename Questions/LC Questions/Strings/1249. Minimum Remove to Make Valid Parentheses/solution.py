13ab5aff5598141f680723cff01084fee3343a13
doyedele1 <demilade.oyedele@gmail.com>
2024-02-22 10:13:33 +0000

    solved-LC-question

--- Questions/LC Questions/Strings/1249. Minimum Remove to Make Valid Parentheses/solution.py
+++ Questions/LC Questions/Strings/1249. Minimum Remove to Make Valid Parentheses/solution.py
@@ -1,19 +1,25 @@
-
 '''
-    Explanation I: Stack Solution
-        - Initialize an empty stack
-        - Make the input string a list and iterate through it
-            - If character in the string is a (, append its index to the stack
-            - When we encounter ),
-                - We check if the stack is not empty and pop the current matching parenthesis out of the stack
-                - If the stack is empty, we also change its character in the list to an empty string
-        - We can then change the remaining unbalanced parentheses into an empty string
-        - We are then left with the balanced string
+    dem(i(l)a)de)
 
-        TC - O(n) where n is the length of the input string since pushing to a stack and popping from a stack is O(1)
-        SC - O(n) where n is the length of the input string for stack and list_conversion, each of which require n characters
+    Explanation I: Stack Solution
+        - Initialize an empty stack; make the input string a list and iterate through it
+        - If character in the string is a (, append its index to the stack
+        - When we encounter ),
+            - If the stack is not empty, pop the top of the stack
+            - If the stack is empty and we still have extra closing parentheses, change its character in the list to an empty string
+        - In case of extra opening parentheses, change the remaining parentheses into an empty string
+        - Return the string which is balanced
+
+        TC: O(n) where n is the length of the input string since pushing to a stack and popping from a stack is O(1)
+        SC: O(n) where n is the length of the input string for stack and list_conversion, each of which require n characters
 
     Explanation II: Non-Stack Solution
+        Do the same as explanation I, but instead of stack, use two variables open_ and close
+
+        In case of extra opening parentheses "))((", iterate from the back of the string and change the parentheses into an empty string, 
+
+        TC: O(n)
+        SC: O(1)
 '''
 
 class Solution1:
@@ -27,12 +33,13 @@ class Solution1:
             elif char == ")":
                 if stack:
                     stack.pop()
+                # extra closing bracket - "dem(i(l)a)de)""
                 else:
                     s[i] = ""
         
+        # extra opening bracket - "))(("
         while stack:
             s[stack.pop()] = ""
-        
         return "".join(s)
     
 class Solution2:
@@ -46,15 +53,17 @@ class Solution2:
             elif s[i] == ")":
                 if open_:
                     open_ -= 1
-                else: s[i] = ""
+                else: 
+                    s[i] = ""
         
         close = 0
-        for i in reversed(range(len(s))):
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == ")":
                 close += 1
             elif s[i] == "(":
                 if close:
                     close -= 1
-                else: s[i] = ""
+                else: 
+                    s[i] = ""
         
         return "".join(s)
