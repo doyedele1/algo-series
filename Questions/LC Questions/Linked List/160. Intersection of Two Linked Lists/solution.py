64b8359fb546313d08eb807ba589a59c704c4cc2
doyedele1 <demilade.oyedele@gmail.com>
2024-02-29 23:05:57 +0000

    solved-LC-question

--- Questions/LC Questions/Linked List/160. Intersection of Two Linked Lists/solution.py
+++ Questions/LC Questions/Linked List/160. Intersection of Two Linked Lists/solution.py
@@ -6,23 +6,31 @@ class ListNode:
         self.val = x
         self.next = None
 
-# TC: O(n), SC: 0(1)
-class Solution:
+# TC: O(n + m), SC: 0(m)
+class Solution1:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        seen = set()
+        
+        while headB:
+            seen.add(headB)
+            headB = headB.next
+        
+        while headA:
+            if headA in seen:
+                return headA
+            headA = headA.next
+        return None
+
+# TC: O(n + m), SC: 0(1)
+class Solution2:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         if not headA or not headB:
             return None
         
-        dummy_A = headA
-        dummy_B = headB
+        dummyA = headA
+        dummyB = headB
         
-        while dummy_A != dummy_B:
-            if not dummy_A:
-                dummy_A = headB
-            else:
-                dummy_A = dummy_A.next
-            
-            if not dummy_B:
-                dummy_B = headA
-            else:
-                dummy_B = dummy_B.next
-        return dummy_B
+        while dummyA != dummyB:
+            dummyA = headB if dummyA is None else dummyA.next
+            dummyB = headA if dummyB is None else dummyB.next
+        return dummyB
