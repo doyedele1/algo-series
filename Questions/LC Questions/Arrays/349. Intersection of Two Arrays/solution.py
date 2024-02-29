0411c01159c9006a6d6d574819f3fa65fc825c8a
doyedele1 <demilade.oyedele@gmail.com>
2024-02-29 23:34:41 +0000

    solved-LC-question

--- Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
+++ Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
@@ -7,11 +7,11 @@
         nums2 = set2 = (2)
         - We then iterate over the smallest set which is nums2. For every nums in set2, if we find them in set1, we add to the result array
         
-        - TC: O(m + n) where m is the length of nums1 and n is the length of nums2. 
+        TC: O(m + n) where m is the length of nums1 and n is the length of nums2. 
             - O(m) to convert nums1 to set1
             - O(n) to convert nums2 to set2
             - O(1) to look up the set
-        - SC: O(m + n) worst case is when all elements in the arrays are different
+        SC: O(m + n) worst case is when all elements in the arrays are different
         
     Explanation II: Sorting and Two Pointers
         nums1 = [1,2,2,1], nums2 = [2,2]
@@ -25,22 +25,23 @@
         
         - return set(res)
         
-        - TC: O(m logm + n logn)
-        - SC: O(log m + log n) to O(m + n) depending on the sorting algorithm used
+        TC: O(mlogm + nlogn)
+        SC: O(logm + logn) to O(m + n) depending on the sorting algorithm used
 '''
 
 from typing import List
 
-
 class Solution1:
+    def intersectionOfTwoSets(self, set1, set2):
+            return [x for x in set1 if x in set2]
+    
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        def helper(set1, set2):
-            return [nums for nums in set1 if nums in set2]
-        
         set1, set2 = set(nums1), set(nums2)
         
-        if len(set1) < len(set2): return helper(set1, set2)
-        else: return helper(set2, set1)
+        if len(set1) < len(set2):
+            return self.intersectionOfTwoSets(set1, set2)
+        else:
+            return self.intersectionOfTwoSets(set2, set1)
                 
 class Solution2:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
@@ -50,11 +51,13 @@ class Solution2:
         i, j = 0, 0
         
         while i < len(nums1) and j < len(nums2):
-            if nums1[i] > nums2[j]: j += 1
-            elif nums1[i] < nums2[j]: i += 1
+            if nums1[i] > nums2[j]:
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
             else:
                 res.append(nums1[i])
                 i += 1
                 j += 1
                 
-        return set(res)             
+        return set(res)
