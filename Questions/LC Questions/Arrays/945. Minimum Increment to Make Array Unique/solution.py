d8f32ec6e9e2c0a538b354ed8600e0b77bc912ad
doyedele1 <demilade.oyedele@gmail.com>
2024-07-15 00:51:09 +0100

    solved LC question

--- Questions/LC Questions/Arrays/945. Minimum Increment to Make Array Unique/solution.py
+++ Questions/LC Questions/Arrays/945. Minimum Increment to Make Array Unique/solution.py
@@ -1,7 +1,26 @@
 '''
-    Explanation II: Counting Sort
+    Explanation I: Sorting
+        [3,2,1,2,1,7]
+        Sort: [1,1,2,2,3,7]
+        No first iteration as we are starting from index 1
+        Second iteration: res = 1, nums[1] = 2
+        Third iteration: res = 2, nums[2] = 3
+        Fourth iteration: res = 4, nums[3] = 4
+        Fifth iteration: res = 6, nums[4] = 5
 
-    TC: O(n + max(nums)), SC: O(n + max(nums))
+        TC: O(nlogn), SC: O(n)
+
+    Explanation II: Counting Sort
+        num     count
+        1       2
+        2       2
+        3       1
+        7       1
+
+        If count is greater than 1, find the extra
+        Get the next count by incrementing extra to the count
+
+        TC: O(n + max(nums)), SC: O(n + max(nums))
 '''
 
 from collections import Counter
--- Questions/LC Questions/Arrays/950. Reveal Cards In Increasing Order/solution.py
+++ Questions/LC Questions/Arrays/950. Reveal Cards In Increasing Order/solution.py
@@ -1,15 +1,16 @@
 '''
     Explanation: Fill and Skip Approach
-    2      3       5       7       11      13      17
-    
-    0      1       2       3        4       5       6
-    2      s       3       s        5       s       7
-    2      s       3       11       5       s       7
-    2      13      3       11       5       s       7
-    2      13      3       11       5       17      7
+        2      3       5       7       11      13      17
+        
+        0      1       2       3        4       5       6
+        2      s       3       s        5       s       7
+        2      s       3       11       5       s       7
+        2      13      3       11       5       s       7
+        2      13      3       11       5       17      7
 
-    TC: O(nlogn), SC: O(n)
+        TC: O(nlogn), SC: O(n)
 '''
+
 from collections import deque
 from typing import List
 
