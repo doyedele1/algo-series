d3025e40d186a76ec5efb7e082ad5429948735dc
doyedele1 <demilade.oyedele@gmail.com>
2024-09-19 20:27:36 +0100

    solved-LC

--- Questions/LC Questions/Arrays/1. Two Sum/solution.py
+++ Questions/LC Questions/Arrays/1. Two Sum/solution.py
@@ -1,35 +1,27 @@
 '''
-    Explanation I:
-        TC: O(n-squared), SC: O(1)
-
-    Explanation II:
-        Use a hashmap and map the value to its index
+    Explanation:
+        - Use a hashmap and map the value to its index
         [2,7,11,15], target = 9
         nums_map = {}
         
-        First iteration, diff = 7, nums_map = {2:0}
-        Second iteration, diff = 2, diff is in nums_map, then return the indices
+        - First iteration, diff = 7, nums_map = {2:0}
+        - Second iteration, diff = 2, diff is in nums_map, then return the indices
 
         TC: O(n), SC: O(n)
 '''
 
 from typing import List
-
-class Solution1:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i, j = 0, len(nums) - 1
-        
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return[i,j]
     
-class Solution2:
+class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map = {}
-        
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in nums_map:
-                return [nums_map[diff], i]
-            nums_map[num] = i
+        diffMap = {}
+
+        for idx, val in enumerate(nums):
+            diff = target - val
+
+            if diff in diffMap:
+                return [diffMap[diff], idx]
+            else:
+                diffMap[val] = idx
+        # return an empty list if no solution is found
+        return []
