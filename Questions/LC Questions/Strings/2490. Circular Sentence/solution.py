85986b4bef130755cab8e9dc1556d229cae063c1
doyedele1 <demilade.oyedele@gmail.com>
2024-11-05 01:55:44 +0000

    solved-LC-question

---
+++ Questions/LC Questions/Strings/2490. Circular Sentence/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        n = len(sentence)
+
+        for i in range(n):
+            if sentence[i] == " " and sentence[i - 1] != sentence[i + 1]:
+                return False
+            if sentence[0] != sentence[n - 1]:
+                return False
+
+        return True
