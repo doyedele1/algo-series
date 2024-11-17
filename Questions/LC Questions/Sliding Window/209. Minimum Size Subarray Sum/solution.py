c37eaccc93350a5c0484b2cd6c1ec13be07fd4e5
doyedele1 <demilade.oyedele@gmail.com>
2024-11-17 23:45:21 +0000

    solved-LC-question

--- Questions/LC Questions/Sliding Window/209. Minimum Size Subarray Sum/solution.py
+++ Questions/LC Questions/Sliding Window/209. Minimum Size Subarray Sum/solution.py
@@ -3,9 +3,27 @@
         [1, 3, 2, 2, 3], target = 7
          l
          r
+        Observation: Since we have positive integers, we know that the sum will keep increasing
+        
+        We do two things:
+            - Keep moving r until sum >= target
+            - Then, compress the window by moving l until sum < target
+        When you are done with this, then you can find the subarray len by doing (r - l + 2)
 
-         - Since we have positive integers, we know that the sum will keep increasing
-
+        [1, 3, 2, 2, 3], target = 7
+         l
+         r
+        First iteration: sum = 1
+        Second iteration: sum = 4
+        Third iteration: sum = 6
+        
+        Fourth iteration: sum = 8. Now sum is greater than or equal to 7, We compress the window. Sum becomes 7
+        Sum is still greater than or equal to 7. We compress the window. Sum becomes 4
+        After the fourth iteration, r = 4, l = 2
+    
+        Fifth iteration: sum = 7. Now sum is greater than or equal to 7. We compress the window. Sum becomes 5
+        After the fifth iteration, r = 5, l = 3
+    
         TC: O(n)
         SC: O(1)
 '''
@@ -14,15 +32,23 @@ from typing import List
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        left, current_sum = 0, 0
-        res = float("inf") # could also be len(nums) + 1 or even -1
+        n = len(nums)
+        l, r = 0, 0
+        sum = 0
+        res = float("inf")
 
-        for right in range(len(nums)):
-            current_sum += nums[right]
+        # Empty array, then return 0
+        if n == 0:
+            return 0
 
-            while current_sum >= target:
-                res = min(res, right - left + 1)
-                current_sum -= nums[left]
-                left += 1
+        while r < n:
+            sum += nums[r]
+
+            if sum >= target:
+                while sum >= target:
+                    sum -= nums[l]
+                    l += 1
+                res = min(res, r - l + 2)
+            r += 1
 
         return 0 if res == float("inf") else res
