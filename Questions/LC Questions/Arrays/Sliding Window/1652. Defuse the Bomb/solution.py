eb512cb423bba3bfdfc5231e3cf6596c8cb8fec2
doyedele1 <demilade.oyedele@gmail.com>
2024-11-18 23:52:56 +0000

    solved-LC-question

--- Questions/LC Questions/Arrays/Sliding Window/1652. Defuse the Bomb/solution.py
+++ Questions/LC Questions/Arrays/Sliding Window/1652. Defuse the Bomb/solution.py
@@ -1,3 +1,9 @@
+'''
+    [5, 7, 1, 4], k = 3
+
+
+'''
+
 from typing import List
 
 class Solution1:
@@ -15,4 +21,29 @@ class Solution1:
             elif k < 0:
                 for j in range(i - 1, i - 1 - abs(k), -1):
                     res[i] += code[j % n]
+        return res
+    
+class Solution2:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        res = [0] * n
+        
+        if k == 0:
+            return res
+
+        l = 0
+        currSum = 0
+        for r in range(n + abs(k)):
+            currSum += code[r % n]
+
+            if r - l + 1 > abs(k):
+                currSum -= code[l % n]
+                l = (l + 1) % n
+
+            if r - l + 1 == abs(k):
+                if k > 0:
+                    res[(l - 1) % n] = currSum
+                elif k < 0:
+                    res[(r + 1) % n] = currSum
+
         return res
