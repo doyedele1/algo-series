3a5bb4c46b88c19e62bb31e56ff52d3e9adc25c6
doyedele1 <demilade.oyedele@gmail.com>
2024-12-27 19:36:29 +0000

    solved-LC-question

---
+++ Questions/LC Questions/Arrays/1014. Best Sightseeing Pair/solution.py
@@ -0,0 +1,17 @@
+'''
+    Explanation:
+        TC: O(n)
+        SC: O(1)
+'''
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        n = len(values)
+        first = values[0]
+        res = 0
+
+        for j in range(1, n):
+            second = values[j] - j
+            res = max(res, first + second)
+            first = max(first, values[j] + j)
+        return res
