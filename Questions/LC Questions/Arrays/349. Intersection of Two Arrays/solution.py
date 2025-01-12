61083d1085da31b7c319a06d42bf59add477b3c6
doyedele1 <demilade.oyedele@gmail.com>
2025-01-12 12:58:25 +0000

    solved-LC-question

--- Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
+++ Questions/LC Questions/Arrays/349. Intersection of Two Arrays/solution.py
@@ -1,19 +1,5 @@
 '''
-    Explanation I: Hash Set
-        - Clarifying questions to ask: duplicates, negative values, single value lists, 0s, empty lists
-        nums1 = [1,2,2,1], nums2 = [2,2]
-        
-        nums1 = set1 = (1,2)
-        nums2 = set2 = (2)
-        - We then iterate over the smallest set which is nums2. For every nums in set2, if we find them in set1, we add to the result array
-        
-        TC: O(m + n) where m is the length of nums1 and n is the length of nums2. 
-            - O(m) to convert nums1 to set1
-            - O(n) to convert nums2 to set2
-            - O(1) to look up the set
-        SC: O(m + n) worst case is when all elements in the arrays are different
-        
-    Explanation II: Sorting and Two Pointers
+    Explanation I: Sorting and Two Pointers
         nums1 = [1,2,2,1], nums2 = [2,2]
         nums1 = [1, 1, 2, 2]
         nums2 = [2, 2]
@@ -26,11 +12,47 @@
         - return set(res)
         
         TC: O(mlogm + nlogn)
-        SC: O(logm + logn) to O(m + n) depending on the sorting algorithm used
+        SC: O(m + n) depending on the sorting algorithm used
+
+    Explanation II: Hash Set
+        - Clarifying questions to ask: duplicates, negative values, single value lists, 0s, empty lists
+        nums1 = [1,2,2,1], nums2 = [2,2]
+        
+        nums1 = set1 = (1,2)
+        nums2 = set2 = (2)
+        - We then iterate over the smallest set which is nums2. For every nums in set2, if we find them in set1, we add to the result array
+        
+        TC: O(m + n) where m is the length of nums1 and n is the length of nums2. 
+            - O(m) to convert nums1 to set1
+            - O(n) to convert nums2 to set2
+            - O(1) to look up the set
+        SC: O(m + n) worst case is when all elements in the arrays are different
 '''
 from typing import List
 
-class Solution1:
+class SortingSolution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+
+        m = len(nums1)
+        n = len(nums2)
+        res = set()
+        i, j = 0, 0
+        
+        while i < m and j < n:
+            if nums1[i] == nums2[j]:
+                res.add(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        
+        return list(res)
+    
+class Solution2:
     def intersectionOfTwoSets(self, set1, set2):
             return [x for x in set1 if x in set2]
     
@@ -41,22 +63,3 @@ class Solution1:
             return self.intersectionOfTwoSets(set1, set2)
         else:
             return self.intersectionOfTwoSets(set2, set1)
-                
-class Solution2:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        nums2.sort()
-        res = []
-        i, j = 0, 0
-        
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] > nums2[j]:
-                j += 1
-            elif nums1[i] < nums2[j]:
-                i += 1
-            else:
-                res.append(nums1[i])
-                i += 1
-                j += 1
-                
-        return set(res)
