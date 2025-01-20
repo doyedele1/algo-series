63359df714a15a634d67e4382281837e79e3c8d5
doyedele1 <demilade.oyedele@gmail.com>
2025-01-20 02:12:34 +0000

    solved-LC-question

--- Questions/LC Questions/Trees/1123. Lowest Common Ancestor of Deepest Leaves/solution.py
+++ Questions/LC Questions/Trees/1123. Lowest Common Ancestor of Deepest Leaves/solution.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def post_order(self, node, depth):
         if not node:
--- Questions/LC Questions/Trees/865. Smallest Subtree with all the Deepest Nodes/solution.py
+++ Questions/LC Questions/Trees/865. Smallest Subtree with all the Deepest Nodes/solution.py
@@ -12,6 +12,7 @@
 from typing import Optional
 from collections import deque
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
