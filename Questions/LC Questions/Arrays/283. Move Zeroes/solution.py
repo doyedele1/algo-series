7e9c33f89800f2c07cb1c41ab4debe3e153f141a
doyedele1 <demilade.oyedele@gmail.com>
2025-05-04 16:05:23 +0100

    solved-LC

--- Questions/LC Questions/Arrays/283. Move Zeroes/solution.py
+++ Questions/LC Questions/Arrays/283. Move Zeroes/solution.py
@@ -1,44 +1,50 @@
 '''
-    Explanation:
-        [0,1,0,3,12]
+    Explanation: Two-pointer approach
+        index i is to iterate through the array
+        index j is the swap index which represents the position to place the next non-zero number
+        
+        Dry run:
+        [0, 1, 0, 3, 12]
+
         First Iteration --> [0, 1, 0, 3, 12]
                              i
                              j
-        Second Iteration -->[1, 1, 0, 3, 12]
+        Second Iteration -->[1, 0, 0, 3, 12]
                                 i
                              j
-        Third Iteration --> [1, 1, 0, 3, 12]
+        Third Iteration --> [1, 0, 0, 3, 12]
                                    i
                                 j
-        Fourth Iteration --> [1, 3, 0, 3, 12]
+        Fourth Iteration --> [1, 3, 0, 0, 12]
                                        i
                                  j
-        Fifth Iteration --> [1, 3, 12, 3, 12]
-                                           i
+        Fifth Iteration --> [1, 3, 12, 0, 0]
+                                          i
                                     j
 
-        Another loop:
-        [1, 3, 12, 3, 12]
-                    j
-                    i               
-        Result ==> [1, 3, 12, 0, 0]
-
-        - TC - O(n)
-        - SC - O(1)
+        TC - O(n)
+        SC - O(1)
 '''
 
 from typing import List
 
-class Solution:
+class Solution1:
     def moveZeroes(self, nums: List[int]) -> None:
+        n = len(nums)
         j = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[j] = nums[i]
-                j += 1
-        
-        for i in range(j, len(nums)):
-            nums[i] = 0
 
-# One-line solution
-# nums[:] = [x for x in nums if x != 0] + [0]*nums.count(0)
+        for i in range(n):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
+            
+# Variant question asked by Meta- Move the zeroes to the beginning instead of the end
+class Solution2:
+    def moveZeroes(self, nums: List[int]) -> None:
+        n = len(nums)
+        j = n - 1
+
+        for i in range(n - 1, -1, -1):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j -= 1
