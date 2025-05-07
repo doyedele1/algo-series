15aceed1d9863b83b9d35ef5539adb2dd67ea203
doyedele1 <demilade.oyedele@gmail.com>
2025-05-07 10:41:18 +0100

    solved-LC

--- Questions/LC Questions/Arrays/1920. Build Array from Permutation/solution.py
+++ Questions/LC Questions/Arrays/1920. Build Array from Permutation/solution.py
@@ -1,7 +1,7 @@
 from typing import List
 
-# TC: O(n), SC: O(1)
-class Solution:
+# TC: O(n), SC: O(n)
+class Solution1:
     def buildArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         res = [0] * n
@@ -9,3 +9,20 @@ class Solution:
         for i in range(n):
             res[i] = nums[nums[i]]
         return res
+    
+# Encoding and Decoding values approach
+# TC: O(n), SC: O(1)
+# stored_value = original_val +  new_value_for_this_slot * CONSTANT
+    
+class Solution2:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        for i in range(n):
+            new_value = nums[nums[i]] % n
+            nums[i] = nums[i] + new_value * n
+
+        for i in range(n):
+            nums[i] = nums[i] // n
+        
+        return nums
