9272bb626a77309e17ebfdc5fea37292e8b7e4db
doyedele1 <demilade.oyedele@gmail.com>
2025-05-19 22:05:54 +0100

    solved-LC-question

---
+++ Questions/LC Questions/Math/3024. Type of Triangle/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# TC: O(1), SC: O(1)
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        nums.sort()
+
+        if nums[0] + nums[1] <= nums[2]:
+            return "none"
+        
+        if nums[0] == nums[2]:
+            return "equilateral"
+        
+        if nums[0] == nums[1] or nums[1] == nums[2]:
+            return "isosceles"
+        
+        return "scalene"
