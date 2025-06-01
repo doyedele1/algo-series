0df5958f74a1c1f9a16d8ec3b4f562669d999b3b
doyedele1 <demilade.oyedele@gmail.com>
2025-06-01 17:26:45 +0100

    solved-LC-question

--- Questions/LC Questions/Strings/791. Custom Sort String/solution.py
+++ Questions/LC Questions/Strings/791. Custom Sort String/solution.py
@@ -1,46 +1,108 @@
 '''
-    Explanation I:
-        - We can use a lookup dict to match these characters to integers
-        - Each time we see a new char, we increase the iterator and sort using the lambda function
+    Explanation:
+        Original Question:
+            s = "babcz", order = "cba"
 
-    Explanation II:
-        - Count the elemtns of T
-        - If we have some count[char] = number of occurences of char in T, we can return these elements in any order
-        - The order is S + (characters not in S in any order)
-        
-        TC - O(len(S) + len(T))
-        SC - O(len(T)
+            Brute force solution: Two for loops
+                Iterate through order (cba).
+                First, we see a c. Iterate through s and look for a c. If it is present, add to the result. res = "c"
+                Second, we see a b. Iterate through s and look for a b. If it is present, add to the result. res = "cbb"
+                Third, we see an a. Iterate through s and look for an a. If it is present, add to the result. res = "cbba"
+
+                After, we iterate through s and check for the remaining characters we have not added to s, and then add to res. res = "cbbaz"
+
+                Note: There could be two edge cases.
+                Case 1: A character in order does not exist in s. order can be "cbad". Since d does not exist in s, we don't add to res
+                Case 2: This is what we have seen already. A character in s does not exist in order. If s = "babcz", z does not exist in order, but we need to add to res in any position creating different permutations
+
+                TC: O(s*order) = O(s-squared)
+                SC: O(s)
+
+            Optimal Solution: Hashmap
+                In previous solution, we are going through our string s to find character we have seen in order. We are doing this multiple times. Why not look for the character once?
+                So, we can use a hashmap that is a frequency counter of s
+
+                s = "babcz", order = "cba"
+                freq = {a: 1, b: 2, c: 1, z: 1}
+
+                Iterate through the order.
+                
+                First iteration: We see a c. We can then check freq for c. If it's there, we add it to our res and have the count as 0. 
+                res = "c", freq = {a: 1, b: 2, c: 0, z: 1}
+                
+                Second iteration: We see a b. We can then check freq for b. If it's there, we add it to our res and have the count as 0. 
+                res = "cbb", freq = {a: 1, b: 0, c: 0, z: 1}
+                
+                Third iteration: We see an a. We can then check freq for a. If it's there, we add it to our res and have the count as 0. 
+                res = "cbba", freq = {a: 0, b: 0, c: 0, z: 1}
+
+                When we are done iterating through order, we then check freq for any char with count greater than 0, and add it to res
+                res = "cbbaz"
+
+                TC: O(s)
+                SC: O(26). O(s) if we count auxiliary space variables like res
+
+        Follow-up Question: Optimize your solution - use an array/list, instead of a hashmap
+            If we use a hashmap, it can suffer from collisions
+
+            TC: O(s)
+            SC: O(1)
+
+        Variant Question: Different Order Parameter
+            order is now an array of characters, rather than a string
+
+            TC: O(s)
+            SC: O(1)
 '''
+from collections import Counter
+from typing import List
 
-import collections
-
-class Solution1:
+class OriginalSolution:
     def customSortString(self, order: str, s: str) -> str:
-        lookup = collections.defaultdict(int)
-        i = 0
+        freq = Counter(s)
+        res = []
+
+        for ch in order:
+            if ch in freq:
+                res.extend([ch] * freq[ch])
+                freq[ch] = 0
         
-        for c in order:
-            lookup[c] = i
-            i += 1
-            
-        return "".join(sorted(s, key=lambda x: lookup[x]))
+        for ch, count in freq.items():
+            if count > 0:
+                res.extend([ch] * count)
+        
+        return "".join(res)
+    
+class FollowUpSolution:
+    def customSortString(self, order: str, s: str) -> str:
+        freq = [0] * 26
+        res = ""
 
-# COUNT AND WRITE        
-class Solution2(object):
-    def customSortString(self, S, T):
-        # count[char] will be the number of occurrences of
-        # 'char' in T.
-        count = collections.Counter(T)
-        ans = []
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
 
-        # Write all characters that occur in S, in the order of S.
-        for c in S:
-            ans.append(c * count[c])
-            # Set count[c] = 0 to denote that we do not need to write 'c' to our answer anymore.
-            count[c] = 0
+        for ch in order:
+            if ch in freq:
+                res += ch * freq[ord(ch) - ord('a')]
+                freq[ord(ch) - ord('a')] = 0
+        
+        for i in range(26):
+            res += chr(i + ord('a')) * freq[i]
+        
+        return res
 
-        # Write all remaining characters that don't occur in S. That information is specified by 'count'.
-        for c in count:
-            ans.append(c * count[c])
+# Same solution as the OriginalSolution
+class VariantSolution:
+    def customSortString(self, order: List[str], s: str) -> str:
+        freq = Counter(s)
+        res = []
 
-        return "".join(ans)
+        for ch in order:
+            if ch in freq:
+                res.append(ch * freq[ch])
+                del freq[ch]
+        
+        for ch, freq in freq.items():
+            res.append(ch * freq)
+        
+        return ''.join(res)
