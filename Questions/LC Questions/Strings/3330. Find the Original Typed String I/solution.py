c79e0d9fb9f6b1924a2c4bbd8acf556b715492ab
doyedele1 <demilade.oyedele@gmail.com>
2025-07-01 07:58:16 +0100

    solved-LC-question

---
+++ Questions/LC Questions/Strings/3330. Find the Original Typed String I/solution.py
@@ -0,0 +1,11 @@
+# TC: O(n), SC: O(1)
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        n = len(word)
+        res = 1
+
+        for i in range(1, n):
+            if word[i - 1] == word[i]:
+                res += 1
+        
+        return res
