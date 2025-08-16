f190f5f768e0fa28f1f6f772102785fec00baa75
doyedele1 <demilade.oyedele@gmail.com>
2025-08-16 19:31:23 +0100

    solved-LC-question

---
+++ Questions/LC Questions/Math/342. Power of Four/solution.py
@@ -0,0 +1,9 @@
+# TLE in LC
+class Solution1:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n < 1:
+            return False
+            
+        while n % 4 == 0:
+            n // 4
+        return n == 1
