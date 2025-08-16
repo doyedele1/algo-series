6c18142995535b2e21eaeb6698005df822855a27
doyedele1 <demilade.oyedele@gmail.com>
2025-08-16 19:32:51 +0100

    solved-LC-question

--- Questions/LC Questions/Math/231. Power of Two/solution.py
+++ Questions/LC Questions/Math/231. Power of Two/solution.py
@@ -1,10 +1,12 @@
-class Solution:
+class Solution1:
     def isPowerOfTwo(self, n: int) -> bool:
-        # if n < 1:
-        #     return False
+        if n < 1:
+            return False
         
-        # while n % 2 == 0:
-        #     n /= 2
-        # return n == 1
+        while n % 2 == 0:
+            n /= 2
+        return n == 1
 
+class Solution2:
+    def isPowerOfTwo(self, n: int) -> bool:
         return n > 0 and n & (n - 1) == 0
