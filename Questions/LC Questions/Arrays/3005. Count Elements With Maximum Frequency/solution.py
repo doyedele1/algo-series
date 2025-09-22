fa3c1ec9405183f308c9f9bd4611f11f569bebee
doyedele1 <demilade.oyedele@gmail.com>
2025-09-22 07:32:10 +0100

    solved-LC-question

--- Questions/LC Questions/Arrays/3005. Count Elements With Maximum Frequency/solution.py
+++ Questions/LC Questions/Arrays/3005. Count Elements With Maximum Frequency/solution.py
@@ -1,7 +1,7 @@
 from typing import List
 
 # One pass - Hashmap. TC: O(n), SC: O(n)
-class Solution:
+class Solution1:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         nums_freq = {}
         max_freq = 0
@@ -15,5 +15,22 @@ class Solution:
                 max_freq = freq
                 total_freq = freq
             elif freq == max_freq:
+                total_freq += freq
+        return total_freq
+
+# Using an array as the frequency counter. TC: O(n), SC: O(1)
+class Solution2:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        nums_freq = [0] * 101
+        for num in nums:
+            nums_freq[num] += 1
+
+        max_freq = 0
+        for freq in nums_freq:
+            max_freq = max(max_freq, freq)
+
+        total_freq = 0
+        for freq in nums_freq:
+            if freq == max_freq:
                 total_freq += freq
         return total_freq
